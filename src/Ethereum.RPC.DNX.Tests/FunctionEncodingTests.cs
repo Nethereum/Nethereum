ae8533d82f1954ee09b5e3f9663113894085edde
Juan Blanco <juanfranblanco@hotmail.com>
2015-12-14 07:36:53 +0000

    Function Encoding Dynamic Types

--- src/Ethereum.RPC.DNX.Tests/FunctionEncodingTests.cs
+++ src/Ethereum.RPC.DNX.Tests/FunctionEncodingTests.cs
@@ -8,9 +8,11 @@ namespace Ethereum.ABI.Tests.DNX
         [Fact]
         public virtual void ShouldEncodeInt()
         {
-            var functionCallEncoder = new FunctionCallEnconder();
-            functionCallEncoder.FunctionSha3Encoded = "c6888fa1";
-            functionCallEncoder.FunctionTypes = new string[] {"int"};
+            var functionCallEncoder = new FunctionCallEnconder
+            {
+                FunctionSha3Encoded = "c6888fa1",
+                InputsParams = new[] {CreateParam("int", "a")}
+            };
             var result = functionCallEncoder.Encode(69);
             Assert.Equal("0xc6888fa10000000000000000000000000000000000000000000000000000000000000045", result);
         }
@@ -18,9 +20,12 @@ namespace Ethereum.ABI.Tests.DNX
         [Fact]
         public virtual void ShouldEncodeAddress()
         {
-            var functionCallEncoder = new FunctionCallEnconder();
-            functionCallEncoder.FunctionSha3Encoded = "c6888fa1";
-            functionCallEncoder.FunctionTypes = new string[] { "address" };
+            var functionCallEncoder = new FunctionCallEnconder
+            {
+                FunctionSha3Encoded = "c6888fa1",
+                InputsParams = new[] {CreateParam("address", "a")}
+            };
+
             var result = functionCallEncoder.Encode("1234567890abcdef1234567890abcdef12345678");
             Assert.Equal("0xc6888fa10000000000000000000000001234567890abcdef1234567890abcdef12345678", result);
         }
@@ -28,9 +33,11 @@ namespace Ethereum.ABI.Tests.DNX
         [Fact]
         public virtual void ShouldEncodeBool()
         {
-            var functionCallEncoder = new FunctionCallEnconder();
-            functionCallEncoder.FunctionSha3Encoded = "c6888fa1";
-            functionCallEncoder.FunctionTypes = new string[] { "bool" };
+            var functionCallEncoder = new FunctionCallEnconder
+            {
+                FunctionSha3Encoded = "c6888fa1",
+                InputsParams = new[] {CreateParam("bool", "a")}
+            };
             var result = functionCallEncoder.Encode(true);
             Assert.Equal("0xc6888fa10000000000000000000000000000000000000000000000000000000000000001", result);
         }
@@ -38,11 +45,60 @@ namespace Ethereum.ABI.Tests.DNX
         [Fact]
         public virtual void ShouldEncodeMultipleTypes()
         {
-            var functionCallEncoder = new FunctionCallEnconder();
-            functionCallEncoder.FunctionSha3Encoded = "c6888fa1";
-            functionCallEncoder.FunctionTypes = new string[] { "address", "int", "int" };
+            var functionCallEncoder = new FunctionCallEnconder
+            {
+                FunctionSha3Encoded = "c6888fa1",
+                InputsParams = new[] {CreateParam("address", "a"), CreateParam("int", "b"), CreateParam("int", "c")}
+            };
             var result = functionCallEncoder.Encode("1234567890abcdef1234567890abcdef12345678", 69, 69);
             Assert.Equal("0xc6888fa10000000000000000000000001234567890abcdef1234567890abcdef1234567800000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000000000000000000000000000000000000045", result);
+        }
+
+       
+        [Fact]
+        public virtual void ShouldEncodeMultipleTypesIncludingDynamiString()
+        {
+             var paramsEncoded =
+            "0000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000004500000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000568656c6c6f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005776f726c64000000000000000000000000000000000000000000000000000000";
+
+            var functionCallEncoder = new FunctionCallEnconder
+            {
+                FunctionSha3Encoded = "c6888fa1",
+                InputsParams = new[] {CreateParam("string", "a"), CreateParam("int", "b"), CreateParam("string", "c")}
+            };
+            var result = functionCallEncoder.Encode("hello", 69, "world");
+            Assert.Equal("0x" + functionCallEncoder.FunctionSha3Encoded + paramsEncoded, result);
+            
+        }
+
+        [Fact]
+        public virtual void ShouldEncodeMultipleTypesIncludingDynamicStringAndIntArray()
+        {
+            var paramsEncoded =
+                "00000000000000000000000000000000000000000000000000000000000002c0000000000000000000000000000000000000000000000000000000000003944700000000000000000000000000000000000000000000000000000000000394480000000000000000000000000000000000000000000000000000000000039449000000000000000000000000000000000000000000000000000000000003944a000000000000000000000000000000000000000000000000000000000003944b000000000000000000000000000000000000000000000000000000000003944c000000000000000000000000000000000000000000000000000000000003944d000000000000000000000000000000000000000000000000000000000003944e000000000000000000000000000000000000000000000000000000000003944f0000000000000000000000000000000000000000000000000000000000039450000000000000000000000000000000000000000000000000000000000003945100000000000000000000000000000000000000000000000000000000000394520000000000000000000000000000000000000000000000000000000000039453000000000000000000000000000000000000000000000000000000000003945400000000000000000000000000000000000000000000000000000000000394550000000000000000000000000000000000000000000000000000000000039456000000000000000000000000000000000000000000000000000000000003945700000000000000000000000000000000000000000000000000000000000394580000000000000000000000000000000000000000000000000000000000039459000000000000000000000000000000000000000000000000000000000003945a0000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000568656c6c6f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005776f726c64000000000000000000000000000000000000000000000000000000";
+
+            var functionCallEncoder = new FunctionCallEnconder
+            {
+                FunctionSha3Encoded = "c6888fa1",
+                InputsParams =
+                    new[] {CreateParam("string", "a"), CreateParam("uint[20]", "b"), CreateParam("string", "c")}
+            };
+
+            var array = new uint[20];
+            for (uint i = 0; i < 20; i++)
+            {
+                array[i] = i + 234567;
+            }
+
+            var result = functionCallEncoder.Encode("hello", array, "world");
+
+            Assert.Equal("0x" + functionCallEncoder.FunctionSha3Encoded + paramsEncoded, result);
+          
+        }
+
+        public Param CreateParam(string type, string name)
+        {
+            return new Param() {Type = ABIType.CreateABIType(type), Name = name};
         }
     }
 }
--- src/Ethereum.RPC/ABI/FunctionCallEnconder.cs
+++ src/Ethereum.RPC/ABI/FunctionCallEnconder.cs
@@ -1,22 +1,24 @@
+using System;
 using Ethereum.RPC.Util;
 
 namespace Ethereum.RPC.ABI
 {
+
+    public class Param
+    {
+        public string Name;
+        public ABIType Type;
+    }
+
     public class FunctionCallEnconder
     {
         public string FunctionSha3Encoded { get; set; }
 
-        public string[] FunctionTypes { get; set; }
+        public Param[] InputsParams { get; set; }
 
-        public string Encode(params object[] parameters)
+        public string Encode(params object[] parametersValues)
         {
-
-            //TODO dynamic types
-            var parametersEncoded = "";
-            for (var i = 0; i < FunctionTypes.Length; i++)
-            {
-                parametersEncoded += ABIType.CreateABIType(FunctionTypes[i]).Encode(parameters[i]).ToHexString();
-            }
+            var parametersEncoded = EncodeParameters(parametersValues).ToHexString();
 
             var prefix = "0x";
 
@@ -26,6 +28,55 @@ namespace Ethereum.RPC.ABI
             }
 
             return prefix + FunctionSha3Encoded + parametersEncoded;
+        }
+
+        public byte[] EncodeParameters(params object[] parametersValues)
+        {
+
+            if (parametersValues.Length > InputsParams.Length)
+            {
+                throw new Exception("Too many arguments: " + parametersValues.Length + " > " + InputsParams.Length);
+            }
+
+            int staticSize = 0;
+            int dynamicCount = 0;
+            // calculating static size and number of dynamic params
+            for (int i = 0; i < parametersValues.Length; i++)
+            {
+                var inputsParameter = InputsParams[i];
+                int inputParameterSize = inputsParameter.Type.FixedSize;
+                if (inputParameterSize < 0)
+                {
+                    dynamicCount++;
+                    staticSize += 32;
+                }
+                else
+                {
+                    staticSize += inputParameterSize;
+                }
+            }
+
+            byte[][] encodedBytes = new byte[parametersValues.Length + dynamicCount][];
+          
+            int currentDynamicPointer = staticSize;
+            int currentDynamicCount = 0;
+            for (int i = 0; i < parametersValues.Length; i++)
+            {
+                if (InputsParams[i].Type.IsDynamic())
+                {
+                    byte[] dynamicValueBytes = InputsParams[i].Type.Encode(parametersValues[i]);
+                    encodedBytes[i] = IntType.EncodeInt(currentDynamicPointer);
+                    encodedBytes[parametersValues.Length + currentDynamicCount] = dynamicValueBytes;
+                    currentDynamicCount++;
+                    currentDynamicPointer += dynamicValueBytes.Length;
+                }
+                else
+                {
+                    encodedBytes[i] = InputsParams[i].Type.Encode(parametersValues[i]);
+                }
+            }
+            return ByteUtil.Merge(encodedBytes);
+
         }
 
     }
