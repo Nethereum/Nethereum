d6b6ffe470d10bfb6f944c5e30d5686e035e2288
Juan Blanco <juanfranblanco@hotmail.com>
2015-12-14 14:48:36 +0000

    Decoding multiple parameter outputs from a function

--- src/Ethereum.RPC.DNX.Tests/FunctionEncodingTests.cs
+++ src/Ethereum.RPC.DNX.Tests/FunctionEncodingTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Ethereum.RPC.ABI;
 using Xunit;
 
@@ -13,7 +14,7 @@ namespace Ethereum.ABI.Tests.DNX
                 FunctionSha3Encoded = "c6888fa1",
                 InputsParams = new[] {CreateParam("int", "a")}
             };
-            var result = functionCallEncoder.Encode(69);
+            var result = functionCallEncoder.EncodeRequest(69);
             Assert.Equal("0xc6888fa10000000000000000000000000000000000000000000000000000000000000045", result);
         }
 
@@ -26,7 +27,7 @@ namespace Ethereum.ABI.Tests.DNX
                 InputsParams = new[] {CreateParam("address", "a")}
             };
 
-            var result = functionCallEncoder.Encode("1234567890abcdef1234567890abcdef12345678");
+            var result = functionCallEncoder.EncodeRequest("1234567890abcdef1234567890abcdef12345678");
             Assert.Equal("0xc6888fa10000000000000000000000001234567890abcdef1234567890abcdef12345678", result);
         }
 
@@ -38,7 +39,7 @@ namespace Ethereum.ABI.Tests.DNX
                 FunctionSha3Encoded = "c6888fa1",
                 InputsParams = new[] {CreateParam("bool", "a")}
             };
-            var result = functionCallEncoder.Encode(true);
+            var result = functionCallEncoder.EncodeRequest(true);
             Assert.Equal("0xc6888fa10000000000000000000000000000000000000000000000000000000000000001", result);
         }
 
@@ -50,7 +51,7 @@ namespace Ethereum.ABI.Tests.DNX
                 FunctionSha3Encoded = "c6888fa1",
                 InputsParams = new[] {CreateParam("address", "a"), CreateParam("int", "b"), CreateParam("int", "c")}
             };
-            var result = functionCallEncoder.Encode("1234567890abcdef1234567890abcdef12345678", 69, 69);
+            var result = functionCallEncoder.EncodeRequest("1234567890abcdef1234567890abcdef12345678", 69, 69);
             Assert.Equal("0xc6888fa10000000000000000000000001234567890abcdef1234567890abcdef1234567800000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000000000000000000000000000000000000045", result);
         }
 
@@ -66,7 +67,7 @@ namespace Ethereum.ABI.Tests.DNX
                 FunctionSha3Encoded = "c6888fa1",
                 InputsParams = new[] {CreateParam("string", "a"), CreateParam("int", "b"), CreateParam("string", "c")}
             };
-            var result = functionCallEncoder.Encode("hello", 69, "world");
+            var result = functionCallEncoder.EncodeRequest("hello", 69, "world");
             Assert.Equal("0x" + functionCallEncoder.FunctionSha3Encoded + paramsEncoded, result);
             
         }
@@ -90,10 +91,35 @@ namespace Ethereum.ABI.Tests.DNX
                 array[i] = i + 234567;
             }
 
-            var result = functionCallEncoder.Encode("hello", array, "world");
+            var result = functionCallEncoder.EncodeRequest("hello", array, "world");
 
             Assert.Equal("0x" + functionCallEncoder.FunctionSha3Encoded + paramsEncoded, result);
           
+        }
+
+
+        [Fact]
+        public virtual void ShouldDecodeMultipleTypesIncludingDynamicStringAndIntArray()
+        {
+          
+            var functionCallEncoder = new FunctionCallEnconder
+            {
+                FunctionSha3Encoded = "c6888fa1",
+                OutputParams = 
+                    new[] { CreateParam("string", "a"), CreateParam("uint[20]", "b"), CreateParam("string", "c") }
+            };
+
+            var array = new uint[20];
+            for (uint i = 0; i < 20; i++)
+            {
+                array[i] = i + 234567;
+            }
+
+            var result = functionCallEncoder.DecodeOutput("0x" + "00000000000000000000000000000000000000000000000000000000000002c0000000000000000000000000000000000000000000000000000000000003944700000000000000000000000000000000000000000000000000000000000394480000000000000000000000000000000000000000000000000000000000039449000000000000000000000000000000000000000000000000000000000003944a000000000000000000000000000000000000000000000000000000000003944b000000000000000000000000000000000000000000000000000000000003944c000000000000000000000000000000000000000000000000000000000003944d000000000000000000000000000000000000000000000000000000000003944e000000000000000000000000000000000000000000000000000000000003944f0000000000000000000000000000000000000000000000000000000000039450000000000000000000000000000000000000000000000000000000000003945100000000000000000000000000000000000000000000000000000000000394520000000000000000000000000000000000000000000000000000000000039453000000000000000000000000000000000000000000000000000000000003945400000000000000000000000000000000000000000000000000000000000394550000000000000000000000000000000000000000000000000000000000039456000000000000000000000000000000000000000000000000000000000003945700000000000000000000000000000000000000000000000000000000000394580000000000000000000000000000000000000000000000000000000000039459000000000000000000000000000000000000000000000000000000000003945a0000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000568656c6c6f0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005776f726c64000000000000000000000000000000000000000000000000000000");
+
+            Assert.Equal("hello", result.First(x => x.ParameterOutput.Name == "a").Result);
+            Assert.Equal("world", result.First(x => x.ParameterOutput.Name == "c").Result);
+
         }
 
         public Param CreateParam(string type, string name)
--- src/Ethereum.RPC/ABI/FunctionCallEnconder.cs
+++ src/Ethereum.RPC/ABI/FunctionCallEnconder.cs
@@ -1,13 +1,25 @@
 using System;
+using System.Collections.Generic;
 using Ethereum.RPC.Util;
+using System.Linq;
+using System.Numerics;
 
 namespace Ethereum.RPC.ABI
 {
 
     public class Param
     {
-        public string Name;
-        public ABIType Type;
+        public string Name { get; set; }
+        public ABIType Type { get; set; }
+    }
+
+    public class ParameterOutputResult
+
+    {
+        public Param ParameterOutput { get; set; }
+        public int DataIndexStart { get; set; }
+
+        public object Result { get; set; }
     }
 
     public class FunctionCallEnconder
@@ -15,8 +27,9 @@ namespace Ethereum.RPC.ABI
         public string FunctionSha3Encoded { get; set; }
 
         public Param[] InputsParams { get; set; }
+        public Param[] OutputParams { get; set; }
 
-        public string Encode(params object[] parametersValues)
+        public string EncodeRequest(params object[] parametersValues)
         {
             var parametersEncoded = EncodeParameters(parametersValues).ToHexString();
 
@@ -57,7 +70,7 @@ namespace Ethereum.RPC.ABI
             }
 
             byte[][] encodedBytes = new byte[parametersValues.Length + dynamicCount][];
-          
+
             int currentDynamicPointer = staticSize;
             int currentDynamicCount = 0;
             for (int i = 0; i < parametersValues.Length; i++)
@@ -79,5 +92,54 @@ namespace Ethereum.RPC.ABI
 
         }
 
+        public List<ParameterOutputResult> DecodeOutput(string output)
+        {
+            var results = new List<ParameterOutputResult>();
+            byte[] outputBytes = output.HexStringToByteArray();
+
+            var currentIndex = 0;
+
+            foreach (var outputParam in OutputParams)
+            {
+                var parameterOutputResult = new ParameterOutputResult() {ParameterOutput = outputParam};
+                results.Add(parameterOutputResult);
+
+                if (outputParam.Type.IsDynamic())
+                {
+                    var bytes = outputBytes.Skip(currentIndex).Take(32).ToArray();
+
+                    var indexObject = (BigInteger) new IntType("int").Decode(bytes);
+                    parameterOutputResult.DataIndexStart = (int) indexObject;
+
+                    currentIndex = currentIndex + 32;
+                }
+                else
+                {
+                    var bytes = outputBytes.Skip(currentIndex).Take(outputParam.Type.FixedSize).ToArray();
+                    parameterOutputResult.Result = outputParam.Type.Decode(bytes);
+
+                    currentIndex = currentIndex + outputParam.Type.FixedSize;
+                }
+            }
+
+            ParameterOutputResult currentDataItem = null;
+            foreach (var nextDataItem in results.Where(outputParam => outputParam.ParameterOutput.Type.IsDynamic()))
+            {
+                if (currentDataItem != null)
+                {
+                    var bytes =
+                        outputBytes.Skip(currentDataItem.DataIndexStart).Take(nextDataItem.DataIndexStart).ToArray();
+                    currentDataItem.Result = currentDataItem.ParameterOutput.Type.Decode(bytes);
+                }
+                currentDataItem = nextDataItem;
+            }
+
+            if (currentDataItem != null)
+            {
+                var bytes = outputBytes.Skip(currentDataItem.DataIndexStart).ToArray();
+                currentDataItem.Result = currentDataItem.ParameterOutput.Type.Decode(bytes);
+            }
+            return results;
+        }
     }
 }
