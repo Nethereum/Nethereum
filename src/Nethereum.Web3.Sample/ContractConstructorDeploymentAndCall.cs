d7d6d4241080170f2423113f2a753182bdf3bf23
Juan Blanco <juanfranblanco@hotmail.com>
2016-02-05 07:16:13 +0000

    Constructor sample

---
+++ src/Nethereum.Web3.Sample/ContractConstructorDeploymentAndCall.cs
@@ -0,0 +1,61 @@
+using System.Threading.Tasks;
+using Nethereum.RPC.Eth.Transactions;
+
+namespace Nethereum.Web3.Sample
+{
+    public class ContractConstructorDeploymentAndCall
+    {
+        public async Task<string> Test()
+        {
+            //The compiled solidity contract to be deployed
+            /*
+               contract test { 
+
+               uint _multiplier;
+
+               function test(uint multiplier){
+                   _multiplier = multiplier;
+               }
+
+               function multiply(uint a) returns(uint d) { return a * _multiplier; }
+
+           }
+           */
+
+            var contractByteCode =
+                "0x606060405260405160208060ae833981016040528080519060200190919050505b806000600050819055505b5060768060386000396000f360606040526000357c010000000000000000000000000000000000000000000000000000000090048063c6888fa1146037576035565b005b604b60048080359060200190919050506061565b6040518082815260200191505060405180910390f35b6000600060005054820290506071565b91905056";
+
+            var abi =
+                @"[{""constant"":false,""inputs"":[{""name"":""a"",""type"":""uint256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""uint256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""uint256""}],""type"":""constructor""}]";
+
+            var addressFrom = "0x12890d2cce102216644c59dae5baed380d84830c";
+
+            var web3 = new Web3();
+
+            //deploy the contract, including abi and a paramter of 7. 
+            var transactionHash = await web3.Eth.DeployContract.SendRequestAsync(abi, contractByteCode, addressFrom, 7);
+
+            //the contract should be mining now
+
+            //get the contract address 
+            EthTransactionReceipt receipt = null;
+            //wait for the contract to be mined to the address
+            while (receipt == null)
+            {
+                await Task.Delay(500);
+                receipt = await web3.Eth.GetTransactionReceipt.SendRequestAsync(transactionHash);
+            }
+
+            var contract = web3.Eth.GetContract(abi, receipt.ContractAddress);
+
+            //get the function by name
+            var multiplyFunction = contract.GetFunction("multiply");
+
+            //do a function call (not transaction) and get the result
+            var result = await multiplyFunction.CallAsync<int>(69);
+            //visual test 
+            return "The result of deploying a contract and calling a function to multiply 7 by 69 is: " + result +
+                   " and should be 483";
+        }
+    }
+}
