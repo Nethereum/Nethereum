e9b8fa6ca6f1ac0733abc1f6fd0c23d3e666fb77
Juan Blanco <juanfranblanco@hotmail.com>
2016-03-09 00:35:58 +0000

    remove submodule checked in

--- Nethereum-XS/JsonRpc.Router/Publish.cmd
+++
@@ -1,8 +0,0 @@
-SET configuration=Release
-SET out=C:/Publish
-
-call dnu pack ./src/JsonRpc.Core --configuration %configuration% --out "%out%/JsonRpc.Core"
-
-call dnu pack ./src/JsonRpc.Router --configuration %configuration% --out "%out%/JsonRpc.Router"
-
-call dnu pack ./src/JsonRpc.Client --configuration %configuration% --out "%out%/JsonRpc.Client"
