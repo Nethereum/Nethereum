63c6c8102847079ebab4c68891e05d57d1488777
Juan Blanco <juanfranblanco@hotmail.com>
2016-05-29 18:19:38 +0100

    Removing Xamarin-XS and adding projects folder, include portable in main solution and project builder for portable

--- projects/BuildProjectFile/Program.cs
+++ projects/BuildProjectFile/Program.cs
@@ -17,8 +17,8 @@ namespace BuildProjectFile
 			stringBuilder.Append(CreateOutputFolder ("..\\src\\Nethereum.JsonRpc.Client", "NethereumJsonRpc"));
 			stringBuilder.Append(CreateOutputFolder ("JsonRpc.Router\\src\\JsonRpc.Core", "JsonRpc.Core"));
 			stringBuilder.Append(CreateOutputFolder ("JsonRpc.Router\\src\\JsonRpc.Client", "JsonRpc.Client"));
-            stringBuilder.Append(CreateOutputFolder("\\Netherum.Maker\\Nethereum.Maker", "Maker"));
-            GenerateFile("Nethereum-XS\\Nethereum - XS.csproj", fileTemplate1, fileTemplate2, stringBuilder.ToString());
+            stringBuilder.Append(CreateOutputFolder("\\Nethereum.Maker\\Nethereum.Maker", "Maker"));
+            //GenerateFile("Nethereum-XS\\Nethereum - XS.csproj", fileTemplate1, fileTemplate2, stringBuilder.ToString());
             GenerateFile("Nethereum.Portable\\Nethereum.Portable.csproj", fileTemplatePortable1, fileTemplatePortable2, stringBuilder.ToString());
         }
 
