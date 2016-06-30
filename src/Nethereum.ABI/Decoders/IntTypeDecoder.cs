6808e065a1abf1604413384e202b716e5a4e0edf
Juan Blanco <juanfranblanco@hotmail.com>
2016-06-30 09:31:09 +0100

    Move address decoder to file

--- src/Nethereum.ABI/Decoders/IntTypeDecoder.cs
+++ src/Nethereum.ABI/Decoders/IntTypeDecoder.cs
@@ -6,28 +6,6 @@ using Nethereum.Hex.HexConvertors.Extensions;
 
 namespace Nethereum.ABI.Decoders
 {
-    public class AddressTypeDecoder : TypeDecoder
-    {
-        private IntTypeDecoder intTypeDecoder;
-
-        public AddressTypeDecoder()
-        {
-            this.intTypeDecoder = new IntTypeDecoder();
-
-        }
-        public override bool IsSupportedType(Type type)
-        {
-            return type == typeof(string);
-        }
-
-        public override object Decode(byte[] encoded, Type type)
-        {
-            if (!IsSupportedType(type)) throw new NotSupportedException(type + " is not supported");
-            return intTypeDecoder.DecodeBigInteger(encoded).ToHex(false);
-        }
-
-    }
-
     public class IntTypeDecoder: TypeDecoder
     {
         public override bool IsSupportedType(Type type)
