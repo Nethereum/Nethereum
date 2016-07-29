aa0fad4efee23f133820b26a223eeb854975389d
Juan Blanco <juanfranblanco@hotmail.com>
2016-07-29 11:27:32 +0100

    Adding "0x"

--- src/Nethereum.Web3.Tests/TransactionSigningTests.cs
+++ src/Nethereum.Web3.Tests/TransactionSigningTests.cs
@@ -11,7 +11,7 @@ namespace SimpleTests
         public async Task<bool> ShouldSignAndSendRawTransaction()
         {
             var privateKey = "b5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
-            var senderAddress = "12890d2cce102216644c59daE5baed380d84830c";
+            var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
             var receiveAddress = "0x13f022d72158410433cbd66f5dd8bf6d2d129924";
             var web3 = new Web3();
             var txCount = await web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(senderAddress);
@@ -25,7 +25,7 @@ namespace SimpleTests
                 Thread.Sleep(1000);
                 receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(txId);
             }
-
+            await web3.Miner.Stop.SendRequestAsync();
             Assert.Equal(txId, receipt.TransactionHash);
             return true;
         }
