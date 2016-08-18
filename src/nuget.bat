b659777ee992ccc9011879e901a8cf27a4379772
Juan Blanco <juanfranblanco@hotmail.com>
2016-08-18 05:16:14 +0100

    Nuget generator / copy

---
+++ src/nuget.bat
@@ -0,0 +1,27 @@
+rem packing web3 and dependencies
+cd Nethereum.Hex
+dotnet pack
+cd ..
+cd Nethereum.ABI
+dotnet pack
+cd ..
+cd Nethereum.JsonRpc.Client
+dotnet pack
+cd ..
+cd Nethereum.RPC
+dotnet pack
+cd ..
+cd Nethereum.Web3
+dotnet pack
+cd ..
+cd Nethereum.StandardToken
+dotnet pack
+cd ..
+cd Nethereum.JsonRpc.IpcClient
+dotnet pack
+cd ..
+setlocal
+set DIR=%~dp0
+set OUTPUTDIR=%~dp0\packages
+for /R %DIR% %%a in (*.nupkg) do xcopy "%%a" "%OUTPUTDIR%"
+xcopy *.nupkg packages /s /y
