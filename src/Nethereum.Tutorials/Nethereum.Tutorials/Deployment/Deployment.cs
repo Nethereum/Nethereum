718a186ebb64bc4c5fa4815a594639d860e20bed
Juan Blanco <juanfranblanco@hotmail.com>
2016-08-23 09:35:45 +0100

    Tutorials solution and deployment sample

---
+++ src/Nethereum.Tutorials/Nethereum.Tutorials/Deployment/Deployment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Nethereum.Hex.HexTypes;
+using Xunit;
+
+namespace Nethereum.Tutorials
+{
+    public class Deployment
+    {
+        [Fact]
+        public async Task ShouldBeAbleToDeployAContract()
+        {
+            var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
+            var password = "password";
+            var abi = @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
+            var byteCode =
+                "0x60606040526040516020806052833950608060405251600081905550602b8060276000396000f3606060405260e060020a60003504631df4f1448114601a575b005b600054600435026060908152602090f3";
+
+            var multiplier = 7;
+
+            var web3 = new Web3.Web3();
+            var unlockAccountResult =
+                await web3.Personal.UnlockAccount.SendRequestAsync(senderAddress, password, new HexBigInteger(120));
+            Assert.True(unlockAccountResult);
+
+            var transactionHash =
+                await web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, multiplier);
+
+            var mineResult = await web3.Miner.Start.SendRequestAsync(6);
+
+            Assert.True(mineResult);
+
+            var receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash);
+
+            while (receipt == null)
+            {
+                Thread.Sleep(5000);
+                receipt = await web3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash);
+            }
+
+            var contractAddress = receipt.ContractAddress;
+
+            var contract = web3.Eth.GetContract(abi, contractAddress);
+
+            var multiplyFunction = contract.GetFunction("multiply");
+
+            var result = await multiplyFunction.CallAsync<int>(7);
+
+            Assert.Equal(49, result);
+
+        }
+
+    }
+}
