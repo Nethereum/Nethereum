43628e6f06de8bb299a5248ff0c1c81066f4ff4b
Juan Blanco <juanfranblanco@hotmail.com>
2016-08-23 09:36:08 +0100

    Private chain geth init

--- testchain/startgeth.bat
+++ testchain/startgeth.bat
@@ -3,4 +3,5 @@ RD /S /Q %~dp0\devChain\dapp
 RD /S /Q %~dp0\devChain\nodes
 del %~dp0\devchain\nodekey
 
-geth --genesis genesis_dev.json --rpc --networkid=39318 --maxpeers=0 --datadir=devChain  --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin" --ipcapi "eth,web3,personal,net,miner,admin" --verbosity 0 console  
+geth  --datadir=devChain init genesis_dev.json
+geth  --rpc --networkid=39318 --maxpeers=0 --datadir=devChain  --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin" --ipcapi "eth,web3,personal,net,miner,admin" --verbosity 0 console  
