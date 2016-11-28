4d863b742415fd8bc51b6678dc2ab70e3a94e32a
Juan Blanco <juanfranblanco@hotmail.com>
2016-11-28 15:25:25 +0000

    Sample update

--- src/Nethereum.KeyStore.Console.Sample/Program.cs
+++ src/Nethereum.KeyStore.Console.Sample/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Nethereum.Core.Signing.Crypto;
 
 namespace Nethereum.KeyStore.Console.Sample
 {
@@ -11,34 +12,57 @@ namespace Nethereum.KeyStore.Console.Sample
     {
         public static void Main(string[] args)
         {
+
+            //Reading an existing file
             var address = "12890d2cce102216644c59dae5baed380d84830c";
             var password = "password";
 
             var file = File.OpenText("UTC--2015-11-25T05-05-03.116905600Z--12890d2cce102216644c59dae5baed380d84830c");
             var json = file.ReadToEnd();
             
+            //using the simple key store service
             var service = new KeyStoreService();
+            //decrypt the private key
             var key = service.DecryptKeyStoreFromJson(password, json);
             
-
-           
+           //Generating a new file using the existing private key (key) 
+           //create new file with the standard UTC file name
             var fileName = service.GenerateUTCFileName(address);
             using (var newfile = File.CreateText(fileName))
             {
+                //generate the encrypted and key store content as json. (The default uses pbkdf2)
                 var newJson = service.EncryptAndGenerateDefaultKeyStoreAsJson(password, key, address);
                 newfile.Write(newJson);
                 newfile.Flush();
             }
-
-
+            
+            //Decrypting the new file created
             file = File.OpenText(fileName);
             json = file.ReadToEnd();
 
             var newkey = service.DecryptKeyStoreFromJson(password, json);
 
+            //Compare the keys
             System.Console.WriteLine("Original key: " + key.ToHex());
             System.Console.WriteLine("New key: " + key.ToHex());
             System.Console.ReadLine();
+
+
+            //We can use EthECKey to generate a new one, this is uses SecureRandom
+            var ecKey = EthECKey.GenerateKey();
+            var privateKey = ecKey.GetPrivateKeyAsBytes();
+            var genAddress = ecKey.GetPublicAddress();
+
+            //instead of the default service we can use either
+            //Scrypt
+            var scryptService = new KeyStoreScryptService();
+            var scryptResult = scryptService.EncryptAndGenerateKeyStoreAsJson(password, privateKey, genAddress);
+            //or pkbdf2
+            var pbkdf2Service = new KeyStorePbkdf2Service();
+            var pkbdf2Result = pbkdf2Service.EncryptAndGenerateKeyStoreAsJson(password, privateKey, genAddress);
+
+            //Both services can be configured with a new IRandomBytesGenerator for the IV or Salt
+            //and when encrypting we can pass custom KdfParameters
         }
     }
 }
