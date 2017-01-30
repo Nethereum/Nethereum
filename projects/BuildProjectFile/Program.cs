3e4ac7842ec4923324b6d06c5b4ec2b952e0a888
Juan Blanco <juanfranblanco@hotmail.com>
2017-01-30 13:54:49 +0000

    1.05

--- projects/BuildProjectFile/Program.cs
+++ projects/BuildProjectFile/Program.cs
@@ -17,6 +17,9 @@ namespace BuildProjectFile
             stringBuilder.Append(CreateOutputFolder("..\\src\\Nethereum.StandardTokenEIP20", "EIP20"));
             stringBuilder.Append(CreateOutputFolder ("..\\src\\Nethereum.JsonRpc.Client", "NethereumJsonRpc"));
             stringBuilder.Append(CreateOutputFolder ("..\\src\\Nethereum.KeyStore", "KeyStore"));
+            stringBuilder.Append(CreateOutputFolder("..\\src\\Nethereum.Quorum", "Quorum"));
+            stringBuilder.Append(CreateOutputFolder("..\\src\\Nethereum.ENS", "ENS"));
+
             //stringBuilder.Append(CreateOutputFolder ("JsonRpc.Router\\src\\JsonRpc.Core", "JsonRpc.Core"));
             //stringBuilder.Append(CreateOutputFolder ("JsonRpc.Router\\src\\JsonRpc.Client", "JsonRpc.Client"));
             //GenerateFile("Nethereum-XS\\Nethereum - XS.csproj", fileTemplate1, fileTemplate2, stringBuilder.ToString());
