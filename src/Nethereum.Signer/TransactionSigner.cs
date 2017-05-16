8e0089382ca4a5af1fe82940613dbef3df2dc61b
Andres G. Aragoneses <knocte@gmail.com>
2017-05-16 13:57:25 +0800

    TransactionSigner: reuse some code between SignTransaction() overloads
    Small refactoring to remove some duplicated code.

--- src/Nethereum.Signer/TransactionSigner.cs
+++ src/Nethereum.Signer/TransactionSigner.cs
@@ -20,29 +20,31 @@ namespace Nethereum.Signer
         public string SignTransaction(string key, string to, BigInteger amount, BigInteger nonce)
         {
             var transaction = new Transaction(to, amount, nonce);
-            transaction.Sign(new EthECKey(key.HexToByteArray(), true));
-            return transaction.GetRLPEncoded().ToHex();
+            return SignTransaction(key, transaction);
         }
 
         public string SignTransaction(string key, string to, BigInteger amount, BigInteger nonce, string data)
         {
             var transaction = new Transaction(to, amount, nonce, data);
-            transaction.Sign(new EthECKey(key.HexToByteArray(), true));
-            return transaction.GetRLPEncoded().ToHex();
+            return SignTransaction(key, transaction);
         }
 
         public string SignTransaction(string key, string to, BigInteger amount, BigInteger nonce, BigInteger gasPrice,
             BigInteger gasLimit)
         {
             var transaction = new Transaction(to, amount, nonce, gasPrice, gasLimit);
-            transaction.Sign(new EthECKey(key.HexToByteArray(), true));
-            return transaction.GetRLPEncoded().ToHex();
+            return SignTransaction(key, transaction);
         }
 
         public string SignTransaction(string key, string to, BigInteger amount, BigInteger nonce, BigInteger gasPrice,
             BigInteger gasLimit, string data)
         {
             var transaction = new Transaction(to, amount, nonce, gasPrice, gasLimit, data);
+            return SignTransaction(key, transaction);
+        }
+
+        private string SignTransaction(string key, Transaction transaction)
+        {
             transaction.Sign(new EthECKey(key.HexToByteArray(), true));
             return transaction.GetRLPEncoded().ToHex();
         }
