e59d0c209df44e85aab03fd02c90b614036c36d8
Juan Blanco <juanfranblanco@hotmail.com>
2017-05-19 13:47:38 +0100

    #126 Adding Account Transfer Tutorial

---
+++ src/Nethereum.Tutorials/Nethereum.Tutorials.Account/AccountTransfer.cs
@@ -0,0 +1,101 @@
+using System.Threading.Tasks;
+using Nethereum.Hex.HexTypes;
+using Nethereum.Web3.Accounts;
+using Nethereum.Web3.Accounts.Managed;
+using Nethereum.Web3.TransactionReceipts;
+using Xunit;
+
+namespace Nethereum.Tutorials
+{
+    public class AccountTransfer
+    {
+        [Fact]
+        public async Task ShouldBeAbleToTransferBetweenAccountsUsingManagedAccount()
+        {
+            var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
+            var addressTo = "0x13f022d72158410433cbd66f5dd8bf6d2d129924";
+            var password = "password";
+
+            //if not using portable or netstandard (^net45) you can use LoadFromKeyStoreFile to load the file from the file system.
+
+            var account = new ManagedAccount(senderAddress, password);
+            var web3 = new Web3.Web3(account);
+
+            var transactionPolling = new TransactionReceiptPollingService(web3);
+
+            var currentBalance = await web3.Eth.GetBalance.SendRequestAsync(addressTo);
+            //assumed client is mining already
+            var transactionReceipt = await transactionPolling.SendRequestAsync(() =>
+                web3.TransactionManager.SendTransactionAsync(account.Address, addressTo, new HexBigInteger(20))
+            );
+
+            var newBalance = await web3.Eth.GetBalance.SendRequestAsync(addressTo);
+
+            Assert.Equal(currentBalance.Value + 20, newBalance.Value);
+        }
+
+        [Fact]
+        public async Task ShouldBeAbleToTransferBetweenAccountsUsingThePrivateKey()
+        {
+            var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
+            var addressTo = "0x13f022d72158410433cbd66f5dd8bf6d2d129924";
+            var password = "password";
+
+            var privateKey = "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
+
+            //if not using portable or netstandard (^net45) you can use LoadFromKeyStoreFile to load the file from the file system.
+            
+            var account = new Account(privateKey);
+            var web3 = new Web3.Web3(account);
+
+            var transactionPolling = new TransactionReceiptPollingService(web3);
+
+            var currentBalance = await web3.Eth.GetBalance.SendRequestAsync(addressTo);
+            //assumed client is mining already
+            var transactionReceipt = await transactionPolling.SendRequestAsync(() =>
+                web3.TransactionManager.SendTransactionAsync(account.Address, addressTo, new HexBigInteger(20))
+            );
+
+            var newBalance = await web3.Eth.GetBalance.SendRequestAsync(addressTo);
+
+            Assert.Equal(currentBalance.Value + 20, newBalance.Value);
+        }
+
+
+        [Fact]
+        public async Task ShouldBeAbleToTransferBetweenAccountsLoadingEncryptedPrivateKey()
+        {
+            var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
+            var addressTo = "0x13f022d72158410433cbd66f5dd8bf6d2d129924";
+            var password = "password";
+            //this is your wallet key file which can be found on
+
+            //Linux: ~/.ethereum/keystore
+            //Mac: /Library/Ethereum/keystore
+            //Windows: %APPDATA%/Ethereum
+
+            var keyStoreEncryptedJson =
+                @"{""crypto"":{""cipher"":""aes-128-ctr"",""ciphertext"":""b4f42e48903879b16239cd5508bc5278e5d3e02307deccbec25b3f5638b85f91"",""cipherparams"":{""iv"":""dc3f37d304047997aa4ef85f044feb45""},""kdf"":""scrypt"",""mac"":""ada930e08702b89c852759bac80533bd71fc4c1ef502291e802232b74bd0081a"",""kdfparams"":{""n"":65536,""r"":1,""p"":8,""dklen"":32,""salt"":""2c39648840b3a59903352b20386f8c41d5146ab88627eaed7c0f2cc8d5d95bd4""}},""id"":""19883438-6d67-4ab8-84b9-76a846ce544b"",""address"":""12890d2cce102216644c59dae5baed380d84830c"",""version"":3}";
+
+            //if not using portable or netstandard (^net45) you can use LoadFromKeyStoreFile to load the file from the file system.
+
+            var keyStoreService = new KeyStore.KeyStoreService();
+            var key = keyStoreService.DecryptKeyStoreFromJson(password, keyStoreEncryptedJson);
+
+            var account = new Account(key);
+            var web3 = new Web3.Web3(account);
+
+            var transactionPolling = new TransactionReceiptPollingService(web3);
+
+            var currentBalance = await web3.Eth.GetBalance.SendRequestAsync(addressTo);
+            //assumed client is mining already
+            var transactionReceipt = await transactionPolling.SendRequestAsync(() =>
+                web3.TransactionManager.SendTransactionAsync(account.Address, addressTo, new HexBigInteger(20))
+            );
+
+            var newBalance = await web3.Eth.GetBalance.SendRequestAsync(addressTo);
+
+            Assert.Equal(currentBalance.Value + 20, newBalance.Value);
+        }
+    }
+}
--- src/Nethereum.Tutorials/Nethereum.Tutorials.Account/Deployment/Deployment.cs
+++ src/Nethereum.Tutorials/Nethereum.Tutorials.Account/Deployment/Deployment.cs
@@ -7,32 +7,36 @@ using System.Threading.Tasks;
 using Nethereum.Hex.HexTypes;
 using Xunit;
 using Nethereum.Web3.Accounts;
-using Nethereum.Web3.Transactions;
+using Nethereum.Web3.Accounts.Managed;
+using Nethereum.Web3.TransactionReceipts;
 
 namespace Nethereum.Tutorials
 {
     public class Deployment
-    { 
+    {
         [Fact]
         public async Task ShouldBeAbleToDeployAContractUsingPersonalUnlock()
         {
             var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
             var password = "password";
-            var abi = @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
+            var abi =
+                @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
             var byteCode =
                 "0x60606040526040516020806052833950608060405251600081905550602b8060276000396000f3606060405260e060020a60003504631df4f1448114601a575b005b600054600435026060908152602090f3";
 
             var multiplier = 7;
 
             var web3 = new Web3.Web3(new ManagedAccount(senderAddress, password));
-            
+
             var transactionPolling = new TransactionReceiptPollingService(web3);
 
             //assumed client is mining already
-            var contractAddress = await 
+            var contractAddress = await
                 transactionPolling.DeployContractAndGetAddressAsync(
-                        () => web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, new HexBigInteger(900000), multiplier)
-                    );
+                    () =>
+                        web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, new HexBigInteger(900000),
+                            multiplier)
+                );
 
             var contract = web3.Eth.GetContract(abi, contractAddress);
 
@@ -49,7 +53,8 @@ namespace Nethereum.Tutorials
         {
             var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
             var privateKey = "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
-            var abi = @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
+            var abi =
+                @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
             var byteCode =
                 "0x60606040526040516020806052833950608060405251600081905550602b8060276000396000f3606060405260e060020a60003504631df4f1448114601a575b005b600054600435026060908152602090f3";
 
@@ -62,8 +67,10 @@ namespace Nethereum.Tutorials
             //assumed client is mining already
             var contractAddress = await
                 transactionPolling.DeployContractAndGetAddressAsync(
-                        () => web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, new HexBigInteger(900000), multiplier)
-                    );
+                    () =>
+                        web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, new HexBigInteger(900000),
+                            multiplier)
+                );
 
             var contract = web3.Eth.GetContract(abi, contractAddress);
 
@@ -86,9 +93,11 @@ namespace Nethereum.Tutorials
             //Mac: /Library/Ethereum/keystore
             //Windows: %APPDATA%/Ethereum
 
-            var keyStoreEncryptedJson = @"{""crypto"":{""cipher"":""aes-128-ctr"",""ciphertext"":""b4f42e48903879b16239cd5508bc5278e5d3e02307deccbec25b3f5638b85f91"",""cipherparams"":{""iv"":""dc3f37d304047997aa4ef85f044feb45""},""kdf"":""scrypt"",""mac"":""ada930e08702b89c852759bac80533bd71fc4c1ef502291e802232b74bd0081a"",""kdfparams"":{""n"":65536,""r"":1,""p"":8,""dklen"":32,""salt"":""2c39648840b3a59903352b20386f8c41d5146ab88627eaed7c0f2cc8d5d95bd4""}},""id"":""19883438-6d67-4ab8-84b9-76a846ce544b"",""address"":""12890d2cce102216644c59dae5baed380d84830c"",""version"":3}";
+            var keyStoreEncryptedJson =
+                @"{""crypto"":{""cipher"":""aes-128-ctr"",""ciphertext"":""b4f42e48903879b16239cd5508bc5278e5d3e02307deccbec25b3f5638b85f91"",""cipherparams"":{""iv"":""dc3f37d304047997aa4ef85f044feb45""},""kdf"":""scrypt"",""mac"":""ada930e08702b89c852759bac80533bd71fc4c1ef502291e802232b74bd0081a"",""kdfparams"":{""n"":65536,""r"":1,""p"":8,""dklen"":32,""salt"":""2c39648840b3a59903352b20386f8c41d5146ab88627eaed7c0f2cc8d5d95bd4""}},""id"":""19883438-6d67-4ab8-84b9-76a846ce544b"",""address"":""12890d2cce102216644c59dae5baed380d84830c"",""version"":3}";
 
-            var abi = @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
+            var abi =
+                @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
             var byteCode =
                 "0x60606040526040516020806052833950608060405251600081905550602b8060276000396000f3606060405260e060020a60003504631df4f1448114601a575b005b600054600435026060908152602090f3";
 
@@ -98,7 +107,7 @@ namespace Nethereum.Tutorials
 
             var keyStoreService = new KeyStore.KeyStoreService();
             var key = keyStoreService.DecryptKeyStoreFromJson(password, keyStoreEncryptedJson);
-           
+
 
             var web3 = new Web3.Web3(new Account(key));
 
@@ -107,8 +116,10 @@ namespace Nethereum.Tutorials
             //assumed client is mining already
             var contractAddress = await
                 transactionPolling.DeployContractAndGetAddressAsync(
-                        () => web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, new HexBigInteger(900000), multiplier)
-                    );
+                    () =>
+                        web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, new HexBigInteger(900000),
+                            multiplier)
+                );
 
             var contract = web3.Eth.GetContract(abi, contractAddress);
 
@@ -120,3 +131,4 @@ namespace Nethereum.Tutorials
         }
     }
 }
+
