a0f94c026ffa0b9283368fb49216992338a925fb
Juan Blanco <juanfranblanco@hotmail.com>
2017-08-09 10:22:57 +0200

    add contract

--- src/Nethereum.Web3.Tests/Issues/EventAddressIntString.cs
+++ src/Nethereum.Web3.Tests/Issues/EventAddressIntString.cs
@@ -38,6 +38,46 @@ namespace Nethereum.Web3.Tests.Issues
 
         }
 
+        /*
+         pragma solidity ^0.4.14;
+
+contract Coin {
+    // The keyword "public" makes those variables
+    // readable from outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, uint amount, address to );
+
+    // This is the constructor whose code is
+    // run only when the contract is created.
+    function Coin() {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Sent(msg.sender, amount, receiver);
+    }
+
+    event MetadataEvent(address creator, int id, string description, string metadata);
+
+    function RaiseEventMetadata(address creator, int id, string description, string metadata ){
+        MetadataEvent(creator, id, description, metadata);
+    }
+}
+
+        */
+
         public class CoinService
         {
             private readonly Web3 web3;
