6a9d76256ef9851e07c51cbcb6ff291c1ff41649
Juan Blanco <juanfranblanco@hotmail.com>
2017-09-14 20:52:54 +0100

    Clique bat adding account

--- testchain/clique/startgeth.bat
+++ testchain/clique/startgeth.bat
@@ -4,4 +4,4 @@ RD /S /Q %~dp0\devChain\geth\nodes
 del %~dp0\devchain\geth\nodekey
 
 geth  --datadir=devChain init genesis_clique.json
-geth  --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console  
+geth --nodiscover --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --unlock 0x12890d2cce102216644c59daE5baed380d84830c --verbosity 0 console  
