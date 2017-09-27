9f0ddfa6ad1ed95f004bbd8b8c93fa2954f0ca4f
Juan Blanco <juanfranblanco@hotmail.com>
2017-09-27 16:37:13 +0100

    clique sh update

--- testchain/clique/startgeth.sh
+++ testchain/clique/startgeth.sh
@@ -4,4 +4,4 @@ rm -rf devChain/nodes
 rm -rf devchain/nodekey
 
 geth  --datadir=devChain init genesis_clique.json
-geth  --rpc --datadir=devChain  --rpccorsdomain "*" --rpcapi "eth,web3,personal,net,miner,admin,debug" --verbosity 0 console  
+geth --nodiscover --rpc --datadir=devChain  --rpccorsdomain "*" --mine --rpcapi "eth,web3,personal,net,miner,admin,debug" --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt" --verbosity 0 console  
