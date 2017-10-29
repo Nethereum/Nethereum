a0d12a2a32fd712a60ed7d22249e4fef45eb1401
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-10-29 17:43:43 +0700

    Fixed eth conversion

--- src/Nethereum.Util/UnitConversion.cs
+++ src/Nethereum.Util/UnitConversion.cs
@@ -95,51 +95,49 @@ namespace Nethereum.Util
             {
                 case EthUnit.Wei:
                     return BigInteger.Parse("1");
+
                 case EthUnit.Kwei:
-                    return BigInteger.Parse("1000");
-                case EthUnit.Babbage:
-                    return BigInteger.Parse("1000");
+                case EthUnit.Ada:
                 case EthUnit.Femtoether:
                     return BigInteger.Parse("1000");
+
                 case EthUnit.Mwei:
-                    return BigInteger.Parse("1000000");
+                case EthUnit.Babbage:
                 case EthUnit.Picoether:
                     return BigInteger.Parse("1000000");
+
                 case EthUnit.Gwei:
-                    return BigInteger.Parse("1000000000");
                 case EthUnit.Shannon:
-                    return BigInteger.Parse("1000000000");
                 case EthUnit.Nanoether:
-                    return BigInteger.Parse("1000000000");
                 case EthUnit.Nano:
                     return BigInteger.Parse("1000000000");
+
                 case EthUnit.Szabo:
-                    return BigInteger.Parse("1000000000000");
                 case EthUnit.Microether:
-                    return BigInteger.Parse("1000000000000");
                 case EthUnit.Micro:
                     return BigInteger.Parse("1000000000000");
+
                 case EthUnit.Finney:
-                    return BigInteger.Parse("1000000000000000");
                 case EthUnit.Milliether:
-                    return BigInteger.Parse("1000000000000000");
                 case EthUnit.Milli:
                     return BigInteger.Parse("1000000000000000");
+
                 case EthUnit.Ether:
                     return BigInteger.Parse("1000000000000000000");
+
                 case EthUnit.Kether:
-                    return BigInteger.Parse("1000000000000000000000");
                 case EthUnit.Grand:
-                    return BigInteger.Parse("1000000000000000000000");
                 case EthUnit.Einstein:
                     return BigInteger.Parse("1000000000000000000000");
+
                 case EthUnit.Mether:
                     return BigInteger.Parse("1000000000000000000000000");
+
                 case EthUnit.Gether:
                     return BigInteger.Parse("1000000000000000000000000000");
+
                 case EthUnit.Tether:
                     return BigInteger.Parse("1000000000000000000000000000000");
-                                                        
             }
             throw new NotImplementedException();
         }
