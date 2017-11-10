0e634ebb52012e4d56de85d93d092ff8e6c9099d
Juan Blanco <juanfranblanco@hotmail.com>
2017-11-10 18:47:41 +0000

    Upgrading tutorials

--- src/Nethereum.Tutorials/Nethereum.Tutorials.Core/Deployment/Deployment.cs
+++ src/Nethereum.Tutorials/Nethereum.Tutorials.Core/Deployment/Deployment.cs
@@ -17,8 +17,9 @@ namespace Nethereum.Tutorials
         {
           var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
           var password = "password";
-          var abi = @"[{""constant"":false,""inputs"":[{""name"":""a"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""r"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
-          var byteCode = "0x606060405260405160208060ae833981016040528080519060200190919050505b806000600050819055505b5060768060386000396000f360606040526000357c0100000000000000000000000000000000000000000000000000000000900480631df4f144146037576035565b005b604b60048080359060200190919050506061565b6040518082815260200191505060405180910390f35b6000600060005054820290506071565b91905056";
+            var abi = @"[{'constant':false,'inputs':[{'name':'val','type':'int256'}],'name':'multiply','outputs':[{'name':'d','type':'int256'}],'payable':false,'stateMutability':'nonpayable','type':'function'},{'inputs':[{'name':'multiplier','type':'int256'}],'payable':false,'stateMutability':'nonpayable','type':'constructor'}]";
+            var byteCode =
+                "0x6060604052341561000f57600080fd5b6040516020806100f283398101604052808051906020019091905050806000819055505060b1806100416000396000f300606060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680631df4f144146044575b600080fd5b3415604e57600080fd5b606260048080359060200190919050506078565b6040518082815260200191505060405180910390f35b60008054820290509190505600a165627a7a723058200dfd1138ee1b70e240253d56f9253b3c82bc9f1058e3cb18c2cf7a86691d60b60029";
           var multiplier = 7;
           //a managed account uses personal_sendTransanction with the given password, this way we don't need to unlock the account for a certain period of time
           var account = new ManagedAccount(senderAddress, password);
@@ -26,7 +27,7 @@ namespace Nethereum.Tutorials
           //using the specific geth web3 library to allow us manage the mining.
           var web3 = new Geth.Web3Geth(account);
 
-          var transactionHash = await web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, multiplier);
+          var transactionHash = await web3.Eth.DeployContract.SendRequestAsync(abi, byteCode, senderAddress, new HexBigInteger(3000000), null, multiplier);
 
           var miningResult = await web3.Miner.Start.SendRequestAsync(6);
     
--- src/Nethereum.Tutorials/Nethereum.Tutorials/Deployment/Deployment.cs
+++ src/Nethereum.Tutorials/Nethereum.Tutorials/Deployment/Deployment.cs
@@ -5,22 +5,24 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nethereum.RPC.Accounts;
 
 namespace Nethereum.Tutorials
 {
+    [TestClass]
     public class Deployment
     {
-		//[Fact(Skip="Unskip it if want to demo")]
-        [Fact]
+		
+        [TestMethod]
         public async Task ShouldBeAbleToDeployAContract()
         {
             var senderAddress = "0x12890d2cce102216644c59daE5baed380d84830c";
             var password = "password";
-            var abi = @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
+            var abi = @"[{'constant':false,'inputs':[{'name':'val','type':'int256'}],'name':'multiply','outputs':[{'name':'d','type':'int256'}],'payable':false,'stateMutability':'nonpayable','type':'function'},{'inputs':[{'name':'multiplier','type':'int256'}],'payable':false,'stateMutability':'nonpayable','type':'constructor'}]";
             var byteCode =
-                "0x60606040526040516020806052833950608060405251600081905550602b8060276000396000f3606060405260e060020a60003504631df4f1448114601a575b005b600054600435026060908152602090f3";
-
+                "0x6060604052341561000f57600080fd5b6040516020806100f283398101604052808051906020019091905050806000819055505060b1806100416000396000f300606060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680631df4f144146044575b600080fd5b3415604e57600080fd5b606260048080359060200190919050506078565b6040518082815260200191505060405180910390f35b60008054820290509190505600a165627a7a723058200dfd1138ee1b70e240253d56f9253b3c82bc9f1058e3cb18c2cf7a86691d60b60029";
+            
             var multiplier = 7;
             
             //a managed account uses personal_sendTransanction with the given password, this way we don't need to unlock the account for a certain period of time
@@ -36,7 +38,7 @@ namespace Nethereum.Tutorials
                 await web3.Eth.DeployContract.SendRequestAndWaitForReceiptAsync(abi, byteCode, senderAddress, new Hex.HexTypes.HexBigInteger(900000), null, multiplier);
 
             var mineResult = await web3.Miner.Stop.SendRequestAsync();
-            Assert.True(mineResult);
+            Assert.IsTrue(mineResult);
 
             var contractAddress = receipt.ContractAddress;
 
@@ -46,7 +48,7 @@ namespace Nethereum.Tutorials
 
             var result = await multiplyFunction.CallAsync<int>(7);
 
-            Assert.Equal(49, result);
+            Assert.AreEqual(49, result);
 
         }
 
