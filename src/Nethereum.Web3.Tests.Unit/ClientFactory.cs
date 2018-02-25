fc64c9aac42617e73c27f7f45a7691ebc686ffd0
Andres G. Aragoneses <knocte@gmail.com>
2018-02-25 08:53:17 +0800

    Web3.Tests: separate into 2 projects, Unit and Integration
    This way it's easier to check which ones failed
    because you don't have an ETH client running locally
    (or no internet connection).

---
+++ src/Nethereum.Web3.Tests.Unit/ClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Nethereum.JsonRpc.Client;
+
+namespace Nethereum.Web3.Tests.Unit
+{
+    public class ClientFactory
+    {
+        public static IClient GetClient()
+        {
+            return new RpcClient(new Uri("http://not.relevant.for.unit.tests/"));
+        }
+    }
+}
