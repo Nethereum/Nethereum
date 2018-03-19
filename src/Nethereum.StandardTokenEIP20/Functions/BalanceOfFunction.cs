00bbcb5dfb7954c5dc6af915577e8525ab3873ea
Andres G. Aragoneses <knocte@gmail.com>
2018-03-19 18:52:30 +0800

    StandardTokenEIP20: provide BalanceOfFunction
    In the same way we're already providing the
    ERC20's TransferFunction.

---
+++ src/Nethereum.StandardTokenEIP20/Functions/BalanceOfFunction.cs
@@ -0,0 +1,13 @@
+﻿
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts.CQS;
+
+namespace Nethereum.StandardTokenEIP20.Functions
+{
+    [Function("balanceOf", "uint256")]
+    public class BalanceOfFunction : ContractMessage
+    {
+        [Parameter("address", "_owner", 1)]
+        public string Owner { get; set; }
+    }
+}
