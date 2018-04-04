5fee3ec07fdc4c7e1b49acc5c3d9b49e075de018
Juan Blanco <juanfranblanco@hotmail.com>
2018-04-04 08:12:37 +0100

    Constructor ABI mapping without parameters

--- src/Nethereum.Generators/ABI/Model/ConstructorABI.cs
+++ src/Nethereum.Generators/ABI/Model/ConstructorABI.cs
@@ -2,6 +2,10 @@ namespace Nethereum.Generators.Model
 {
     public class ConstructorABI
     {
+        public ConstructorABI()
+        {
+            InputParameters = new ParameterABI[0];
+        }
         public ParameterABI[] InputParameters { get; set; }
     }
 }
