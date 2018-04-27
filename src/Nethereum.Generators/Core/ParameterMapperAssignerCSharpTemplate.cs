124dbe76000991d0f73f798b8b81375a75d27296
Juan Blanco <juanfranblanco@hotmail.com>
2018-04-27 14:36:27 +0100

    Adding Vb as generic and adding destination

--- src/Nethereum.Generators/Core/ParameterMapperAssignerCSharpTemplate.cs
+++ src/Nethereum.Generators/Core/ParameterMapperAssignerCSharpTemplate.cs
@@ -2,19 +2,38 @@
 
 namespace Nethereum.Generators.Core
 {
-    public class ParameterMapperAssignerCSharpTemplate
+
+    public class ParameterMapperAssignerTemplate
     {
-        public string GenerateMappingAssigment<TParameter1, TParameter2>(ParameterMap<TParameter1, TParameter2> map, string variableSourceName)
-            where TParameter1: Parameter
-            where TParameter2: Parameter
+
+        public virtual string GenerateMappingAssigment<TParameter1, TParameter2>(
+            ParameterMap<TParameter1, TParameter2> map, string variableSourceName, string destinationVariableName)
+            where TParameter1 : Parameter
+            where TParameter2 : Parameter
+        {
+            return destinationVariableName + "." + GenerateMappingAssigment(map, variableSourceName);
+        }
+
+        public virtual string GenerateMappingAssigment<TParameter1, TParameter2>(
+            ParameterMap<TParameter1, TParameter2> map, string variableSourceName)
+            where TParameter1 : Parameter
+            where TParameter2 : Parameter
         {
             var modelSource = new ParameterModel<TParameter1>(map.From);
             var modelTo = new ParameterModel<TParameter2>(map.To);
             return $@"{modelTo.GetPropertyName()} = {variableSourceName}.{modelSource.GetPropertyName()}";
         }
 
+        public virtual string GenerateMappingsReturn<TParameter1, TParameter2>(
+            ParameterMap<TParameter1, TParameter2> map, string variableSourceName, string destinationVariableName)
+            where TParameter1 : Parameter
+            where TParameter2 : Parameter
+        {
+            return destinationVariableName + "." + GenerateMappingsReturn(map, variableSourceName);
+        }
 
-        public string GenerateAllMappingsReturns<TParameter1, TParameter2>(ParameterMap<TParameter1, TParameter2> map, string variableSourceName)
+        public virtual string GenerateMappingsReturn<TParameter1, TParameter2>(
+            ParameterMap<TParameter1, TParameter2> map, string variableSourceName)
             where TParameter1 : Parameter
             where TParameter2 : Parameter
         {
@@ -22,5 +41,18 @@ namespace Nethereum.Generators.Core
             var modelTo = new ParameterModel<TParameter2>(map.To);
             return $@"{modelFrom.GetPropertyName()} = {variableSourceName}.{modelTo.GetPropertyName()}";
         }
+
+
+
+    }
+
+    public class ParameterMapperAssignerCSharpTemplate: ParameterMapperAssignerTemplate
+    {
+
+    }
+
+    public class ParameterMapperAssignerVbTemplate : ParameterMapperAssignerTemplate
+    {
+
     }
 }
