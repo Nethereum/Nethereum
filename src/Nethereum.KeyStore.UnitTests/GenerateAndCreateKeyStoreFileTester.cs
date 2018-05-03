312ca85b05c71fa9964a0994feac8ae34668f34d
Juan Blanco <juanfranblanco@hotmail.com>
2018-05-03 10:01:14 +0100

    Adding simple generation and storage test sample

---
+++ src/Nethereum.KeyStore.UnitTests/GenerateAndCreateKeyStoreFileTester.cs
@@ -0,0 +1,41 @@
+﻿using Nethereum.Hex.HexConvertors.Extensions;
+using Xunit;
+
+namespace Nethereum.KeyStore.UnitTests
+{
+    public class GenerateAndCreateKeyStoreFileTester
+    {
+        [Fact]
+        public void ShouldGenerateAccountAndCreateKeyStoreFileScrypt()
+        {
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+            var keyStoreScryptService = new KeyStoreScryptService();
+            var password = "testPassword";
+            var json = keyStoreScryptService.EncryptAndGenerateKeyStoreAsJson(password, ecKey.GetPrivateKeyAsBytes(), ecKey.GetPublicAddress());
+            var key = keyStoreScryptService.DecryptKeyStoreFromJson(password, json);
+            Assert.Equal(ecKey.GetPrivateKey(), key.ToHex(true));
+        }
+
+        [Fact]
+        public void ShouldGenerateAccountAndCreateKeyStoreFilePbkdf2()
+        {
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+            var keyStorePbkdf2Service = new KeyStorePbkdf2Service();
+            var password = "testPassword";
+            var json = keyStorePbkdf2Service.EncryptAndGenerateKeyStoreAsJson(password, ecKey.GetPrivateKeyAsBytes(), ecKey.GetPublicAddress());
+            var key = keyStorePbkdf2Service.DecryptKeyStoreFromJson(password, json);
+            Assert.Equal(ecKey.GetPrivateKey(), key.ToHex(true));
+        }
+
+        [Fact]
+        public void ShouldGenerateAccountAndCreateKeyStoreFileDefaultService()
+        {
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+            var keyStoreService = new KeyStoreService();
+            var password = "testPassword";
+            var json = keyStoreService.EncryptAndGenerateDefaultKeyStoreAsJson(password, ecKey.GetPrivateKeyAsBytes(), ecKey.GetPublicAddress());
+            var key = keyStoreService.DecryptKeyStoreFromJson(password, json);
+            Assert.Equal(ecKey.GetPrivateKey(), key.ToHex(true));
+        }
+    }
+}
--- src/Nethereum.KeyStore.UnitTests/KeyStoreCryptoTester.cs
+++ src/Nethereum.KeyStore.UnitTests/KeyStoreCryptoTester.cs
@@ -1,10 +1,47 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using Nethereum.Hex.HexConvertors.Extensions;
 using Nethereum.KeyStore.Crypto;
 using Xunit;
 
 namespace Nethereum.KeyStore.UnitTests
 {
+    public class GenerateAndCreateKeyStoreFileTester
+    {
+        [Fact]
+        public void ShouldGenerateAccountAndCreateKeyStoreFileScrypt()
+        {
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+            var keyStoreScryptService = new KeyStoreScryptService();
+            var password = "testPassword";
+            var json = keyStoreScryptService.EncryptAndGenerateKeyStoreAsJson(password, ecKey.GetPrivateKeyAsBytes(), ecKey.GetPublicAddress());
+            var key = keyStoreScryptService.DecryptKeyStoreFromJson(password, json);
+            Assert.Equal(ecKey.GetPrivateKey(), key.ToHex(true));
+        }
+
+        [Fact]
+        public void ShouldGenerateAccountAndCreateKeyStoreFilePbkdf2()
+        {
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+            var keyStorePbkdf2Service = new KeyStorePbkdf2Service();
+            var password = "testPassword";
+            var json = keyStorePbkdf2Service.EncryptAndGenerateKeyStoreAsJson(password, ecKey.GetPrivateKeyAsBytes(), ecKey.GetPublicAddress());
+            var key = keyStorePbkdf2Service.DecryptKeyStoreFromJson(password, json);
+            Assert.Equal(ecKey.GetPrivateKey(), key.ToHex(true));
+        }
+
+        [Fact]
+        public void ShouldGenerateAccountAndCreateKeyStoreFileDefaultService()
+        {
+            var ecKey = Nethereum.Signer.EthECKey.GenerateKey();
+            var keyStoreService = new KeyStoreService();
+            var password = "testPassword";
+            var json = keyStoreService.EncryptAndGenerateDefaultKeyStoreAsJson(password, ecKey.GetPrivateKeyAsBytes(), ecKey.GetPublicAddress());
+            var key = keyStoreService.DecryptKeyStoreFromJson(password, json);
+            Assert.Equal(ecKey.GetPrivateKey(), key.ToHex(true));
+        }
+    }
+
     public class KeyStoreCryptoTester
     {
         private readonly KeyStoreCrypto keyStoreCrypto = new KeyStoreCrypto();
@@ -86,6 +123,7 @@ namespace Nethereum.KeyStore.UnitTests
             Assert.Equal(result, derived.ToHex());
         }
 
+       
         [Fact]
         public void ShouldGenerateMac()
         {
