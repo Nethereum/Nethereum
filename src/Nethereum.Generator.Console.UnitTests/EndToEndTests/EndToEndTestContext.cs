d76aa7a02c3e387bafe6890d5e26aed99ae02f94
Dave Whiffin <dave.whiffin@gmail.com>
2018-05-18 12:04:18 +0100

    Cleaned up console end to end unit test.  Created reusable EndToEndTestContext

---
+++ src/Nethereum.Generator.Console.UnitTests/EndToEndTests/EndToEndTestContext.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Nethereum.Generator.Console.UnitTests.EndToEndTests
+{
+    public class EndToEndTestContext
+    {
+        public string TargetProjectFolder { get; }
+        public string GeneratorConsolePath { get; }
+        public const string GeneratorConsoleName = "Nethereum.Generator.Console.dll";
+
+        public EndToEndTestContext(string testClass, string testName)
+        {
+            TargetProjectFolder = Path.Combine(
+                Path.GetTempPath(), 
+                GetType().Assembly.GetName(false).Name, 
+                testClass, 
+                testName);
+
+            GeneratorConsolePath =
+                Path.GetFullPath("../../../../Nethereum.Generator.Console/bin/debug/netcoreapp2.0"); 
+        }
+
+        public string WriteFileToProject(string fileName, string fileContent)
+        {
+            var fullPath = Path.Combine(TargetProjectFolder, fileName);
+            File.WriteAllText(fullPath, fileContent, Encoding.UTF8);
+            return fullPath;
+        }
+
+        public void CreateProject(IEnumerable<Tuple<string, string>> nugetPackages = null)
+        {
+            EmptyTargetFolder();
+
+            Directory.CreateDirectory(TargetProjectFolder);
+            CreateProjectFile();
+            if (nugetPackages != null)
+            {
+                foreach (var nuget in nugetPackages)
+                {
+                    AddNugetPackage(nuget.Item1, nuget.Item2);
+                }
+            }
+        }
+
+        public bool DirectoryExists(string subDirectory)
+        {
+            return Directory.Exists(Path.Combine(TargetProjectFolder, subDirectory));
+        }
+
+        public bool FileExists(string relativeFilePath)
+        {
+            return File.Exists(Path.Combine(TargetProjectFolder, relativeFilePath));
+        }
+
+        public void GenerateCode(string commandName, string commandArgs)
+        {
+            var args =
+                $"{GeneratorConsoleName} {commandName} {commandArgs}";
+
+            DotNet(args, workingFolderOverride: GeneratorConsolePath);
+        }
+
+        public void EmptyTargetFolder()
+        {
+            if (Directory.Exists(TargetProjectFolder))
+                DeleteDirectory(TargetProjectFolder);
+        }
+
+        private static void DeleteDirectory(string path)
+        {
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                DeleteDirectory(directory);
+            }
+
+            try
+            {
+                Directory.Delete(path, true);
+            }
+            catch (IOException) 
+            {
+                Directory.Delete(path, true);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Directory.Delete(path, true);
+            }
+        }
+
+        public void AddNugetPackage(string packageName, string version)
+        {
+            var args = $"add package {packageName} -v {version}";
+            DotNet(args);
+        }
+
+        public void CreateProjectFile()
+        {
+            DotNet("new classLib");
+        }
+
+        private void DotNet(string args, string workingFolderOverride = null)
+        {
+            var process = new Process();
+
+            if (workingFolderOverride == null)
+            {
+                process.StartInfo.WorkingDirectory = TargetProjectFolder;
+            }
+
+            if (workingFolderOverride != null)
+            {
+                process.StartInfo.WorkingDirectory = GeneratorConsolePath;
+            }
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.Arguments = args;
+            process.StartInfo.FileName = "dotnet";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            process.Start();
+            process.WaitForExit();
+            var output = process.StandardOutput?.ReadToEnd();
+            var error = process.StandardError?.ReadToEnd();
+
+            if (!string.IsNullOrEmpty(output))
+            {
+                Debug.Write(output);
+            }
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                Debug.Write(error);
+                throw new Exception($"Error occurred calling dotnet with args '{args}'. {error}");
+            }
+
+            if (process.ExitCode != 0)
+            {
+                throw new Exception($"Error occurred calling dotnet with args '{args}'. ExitCode was not 0. {output}");
+            }
+        }
+
+        public void BuildProject()
+        {
+            DotNet("build");
+        }
+
+        public bool BuildHasSucceeded()
+        {
+            var outputDir = Path.Combine(TargetProjectFolder, @"bin\Debug\netstandard2.0\");
+            return Directory.Exists(outputDir) && Directory.GetFiles(outputDir, "*.dll").Length > 0;
+        }
+
+        public void CleanUp()
+        {
+            DeleteDirectory(TargetProjectFolder);
+        }
+    }
+}
--- src/Nethereum.Generator.Console.UnitTests/EndToEndTests/GenerateFromAbiTests.cs
+++ src/Nethereum.Generator.Console.UnitTests/EndToEndTests/GenerateFromAbiTests.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Diagnostics;
-using System.IO;
-using System.Text;
+using System.Reflection;
 using Xunit;
 
 namespace Nethereum.Generator.Console.UnitTests.EndToEndTests
@@ -12,72 +10,32 @@ namespace Nethereum.Generator.Console.UnitTests.EndToEndTests
         public void GeneratesExpectedFilesAndCompiles()
         {
             //given
-            var targetProjectFolder = Path.Combine(
-                Path.GetTempPath(), "Nethereum.Generator.Console.UnitTests", "GenerateFromAbiTests", "Sample.CodeGenProject");
+            var context = new EndToEndTestContext(
+                this.GetType().Name, MethodBase.GetCurrentMethod().Name);
 
-            EmptyTargetFolder(targetProjectFolder);
+            context.CreateProject(new []
+            {
+                new Tuple<string, string>("Nethereum.Web3", "2.4.0")
+            });
 
-            const string relativeAbiPath = "StandardContract.abi";
-            const string baseNamespace = "Sample.CodeGenProject";
-            const string generateCommand = "gen-fromabi";
-            var fullAbiFilePath = Path.Combine(targetProjectFolder, relativeAbiPath);
-            var pathToConsole =
-                Path.GetFullPath("../../../../Nethereum.Generator.Console/bin/debug/netcoreapp2.0/Nethereum.Generator.Console.dll");
-
-            Directory.CreateDirectory(targetProjectFolder);
-            File.WriteAllText(fullAbiFilePath, TestData.StandardContract.ABI, Encoding.UTF8);
-            CreateProjectFile(targetProjectFolder);
-            AddNugetPackages(targetProjectFolder);
+            var pathToAbi = context.WriteFileToProject("StandardContract.abi", TestData.StandardContract.ABI);
 
             //when
-            GenerateCode(pathToConsole, targetProjectFolder, baseNamespace, generateCommand, fullAbiFilePath);
+            const string baseNamespace = "Sample.CodeGenProject";
+            var args =
+                $"-abi {pathToAbi} -o {context.TargetProjectFolder} -ns {baseNamespace}";
+            context.GenerateCode("gen-fromabi", args);
+            context.BuildProject();
 
             //then
-            Assert.True(Directory.Exists(Path.Combine(targetProjectFolder, "StandardContract")));
-            Assert.True(Directory.Exists(Path.Combine(targetProjectFolder, "StandardContract", "CQS")));
-            Assert.True(Directory.Exists(Path.Combine(targetProjectFolder, "StandardContract", "DTO")));
-            Assert.True(Directory.Exists(Path.Combine(targetProjectFolder, "StandardContract", "Service")));
+            Assert.True(context.DirectoryExists("StandardContract"));
+            Assert.True(context.DirectoryExists("StandardContract\\CQS"));
+            Assert.True(context.DirectoryExists("StandardContract\\DTO"));
+            Assert.True(context.DirectoryExists("StandardContract\\Service"));
+            Assert.True(context.BuildHasSucceeded());
 
-            BuildProject(targetProjectFolder);
-
-            Assert.True(File.Exists(Path.Combine(targetProjectFolder, @"bin\Debug\netstandard2.0\Sample.CodeGenProject.dll")));
-
-            //clean up
-            Directory.Delete(targetProjectFolder, true);
+            context.CleanUp();
         }
 
-        private static void EmptyTargetFolder(string targetProjectFolder)
-        {
-            if (Directory.Exists(targetProjectFolder))
-                Directory.Delete(targetProjectFolder, true);
-        }
-
-        private static void GenerateCode(string pathToConsole, string targetProjectFolder, string baseNamespace, string generateCommand, string fullAbiFilePath)
-        {
-            var args =
-                $"{pathToConsole} {generateCommand} -abi {fullAbiFilePath} -o {targetProjectFolder} -ns {baseNamespace}";
-
-            var startInfo = new ProcessStartInfo("dotnet", args);
-            Process.Start(startInfo).WaitForExit();
-        }
-
-        private static void AddNugetPackages(string targetProjectFolder)
-        {
-            var args = "add package Nethereum.Web3 -v 2.4.0";
-            var startInfo = new ProcessStartInfo("dotnet", args){WorkingDirectory = targetProjectFolder};
-            Process.Start(startInfo).WaitForExit();
-        }
-
-        private static void CreateProjectFile(string targetProjectFolder)
-        {
-            var startInfo = new ProcessStartInfo("dotnet", "new classLib"){WorkingDirectory = targetProjectFolder};
-            Process.Start(startInfo).WaitForExit();
-        }
-
-        private static void BuildProject(string targetProjectFolder)
-        {
-            var startInfo = new ProcessStartInfo("dotnet", "build"){WorkingDirectory = targetProjectFolder};
-            Process.Start(startInfo).WaitForExit();
-        }
     }
 }
