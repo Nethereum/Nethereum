378f8367691a9f0d68f9205c5a77578f64b21e0e
Juan Blanco <juanfranblanco@hotmail.com>
2018-06-27 18:58:52 +0100

    packing dev unity3d dlls

---
+++ src/unitynet35.bat
@@ -0,0 +1,13 @@
+xcopy Nethereum.ABI\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.Contracts\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.Hex\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.JsonRpc.Client\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.KeyStore\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.RLP\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.RPC\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.Signer\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.Unity\bin\Debug\net35\*.dll unitynet35dlls /s /y
+xcopy Nethereum.Util\bin\Debug\net35\*.dll unitynet35dlls /s /y
+
+
+
