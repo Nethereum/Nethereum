Nethereum/Nethereum

commit 516a3c4c2cbae0f90a9e42bc84565cb47522f8bb

Use TimeSpan instead of int, for timeout (fix #390)

src/Nethereum.JsonRpc.IpcClient/IpcClient.cs
@@ -41,7 +41,7 @@ namespace Nethereum.JsonRpc.IpcClient
             }
             catch (TimeoutException ex)
             {
-                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout} milliseconds", ex);
+                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout.TotalMilliseconds} milliseconds", ex);
             }
             catch
             {

src/Nethereum.Accounts.IntegrationTests/NonceTests.cs
@@ -23,7 +23,7 @@ namespace Nethereum.Accounts.IntegrationTests
                 @"[{""constant"":false,""inputs"":[{""name"":""val"",""type"":""int256""}],""name"":""multiply"",""outputs"":[{""name"":""d"",""type"":""int256""}],""type"":""function""},{""inputs"":[{""name"":""multiplier"",""type"":""int256""}],""type"":""constructor""}]";
             var byteCode =
                 "0x60606040526040516020806052833950608060405251600081905550602b8060276000396000f3606060405260e060020a60003504631df4f1448114601a575b005b600054600435026060908152602090f3";
-            JsonRpc.Client.RpcClient.ConnectionTimeout = 30000;
+            JsonRpc.Client.RpcClient.ConnectionTimeout = TimeSpan.FromSeconds(30.0);
             var multiplier = 7;
 
             var client = ClientFactory.GetClient();

src/Nethereum.JsonRpc.Client/ClientBase.cs
@@ -1,4 +1,5 @@
 #if !DOTNET35
+using System;
 using System.Threading.Tasks;
 
 namespace Nethereum.JsonRpc.Client
@@ -6,7 +7,7 @@ namespace Nethereum.JsonRpc.Client
     public abstract class ClientBase : IClient
     {
 
-        public static int ConnectionTimeout { get; set; } = 20000;
+        public static TimeSpan ConnectionTimeout { get; set; } = TimeSpan.FromSeconds(20.0);
 
         public RequestInterceptor OverridingRequestInterceptor { get; set; }
 

src/Nethereum.JsonRpc.IpcClient/UnixIpcClient.cs
@@ -31,7 +31,7 @@ namespace Nethereum.JsonRpc.IpcClient
                     if (!Task.Run(() =>
                         _socket.Connect(endPoint)).Wait(ConnectionTimeout))
                     {
-                        throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout} milliseconds");
+                        throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout.TotalMilliseconds} milliseconds");
                     }
                 }
             }

src/Nethereum.JsonRpc.RpcClient/RpcClient.cs
@@ -89,7 +89,7 @@ namespace Nethereum.JsonRpc.Client
                 var rpcRequestJson = JsonConvert.SerializeObject(request, _jsonSerializerSettings);
                 var httpContent = new StringContent(rpcRequestJson, Encoding.UTF8, "application/json");
                 var cancellationTokenSource = new CancellationTokenSource();
-                cancellationTokenSource.CancelAfter(TimeSpan.FromMilliseconds(ConnectionTimeout));
+                cancellationTokenSource.CancelAfter(ConnectionTimeout);
 
                 logger.LogRequest(rpcRequestJson);
 
@@ -110,7 +110,7 @@ namespace Nethereum.JsonRpc.Client
             }
             catch(TaskCanceledException ex)
             {
-                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout} milliseconds", ex);
+                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout.TotalMilliseconds} milliseconds", ex);
             }
             catch (Exception ex)
             {

src/Nethereum.JsonRpc.RpcClient/SimpleRpcClient.cs
@@ -74,7 +74,7 @@ namespace Nethereum.JsonRpc.Client
                 var httpContent = new StringContent(rpcRequestJson, Encoding.UTF8, "application/json");
 
                 var cancellationTokenSource = new CancellationTokenSource();
-                cancellationTokenSource.CancelAfter(TimeSpan.FromMilliseconds(ConnectionTimeout));
+                cancellationTokenSource.CancelAfter(ConnectionTimeout);
 
                 var httpResponseMessage = await _httpClient.PostAsync(route, httpContent, cancellationTokenSource.Token)
                     .ConfigureAwait(false);
@@ -92,7 +92,7 @@ namespace Nethereum.JsonRpc.Client
             }
             catch (TaskCanceledException ex)
             {
-                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout} milliseconds", ex);
+                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout.TotalMilliseconds} milliseconds", ex);
             }
             catch (Exception ex)
             {

src/Nethereum.JsonRpc.WebSocketClient/WebSocketClient.cs
@@ -98,7 +98,7 @@ namespace Nethereum.JsonRpc.WebSocketClient
             }
             catch (TaskCanceledException ex)
             {
-                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout} milliseconds", ex);
+                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout.TotalMilliseconds} milliseconds", ex);
             }
             catch
             {
@@ -122,7 +122,7 @@ namespace Nethereum.JsonRpc.WebSocketClient
             }
             catch (TaskCanceledException ex)
             {
-                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout} milliseconds", ex);
+                throw new RpcClientTimeoutException($"Rpc timeout afer {ConnectionTimeout.TotalMilliseconds} milliseconds", ex);
             }
         }
 
@@ -161,7 +161,7 @@ namespace Nethereum.JsonRpc.WebSocketClient
                 var requestBytes = new ArraySegment<byte>(Encoding.UTF8.GetBytes(rpcRequestJson));
                 logger.LogRequest(rpcRequestJson);
                 var cancellationTokenSource = new CancellationTokenSource();
-                cancellationTokenSource.CancelAfter(TimeSpan.FromMilliseconds(ConnectionTimeout));
+                cancellationTokenSource.CancelAfter(ConnectionTimeout);
 
                 var webSocket = GetClientWebSocket();
                 await webSocket.SendAsync(requestBytes, WebSocketMessageType.Text, true, cancellationTokenSource.Token)

commit 4ae1149e3db518da7e8298b182390e59be2c5909

JsonRpc.IpcClient: fix build
Broken by 516a3c4c2cbae0f90a9e42bc84565cb47522f8bb oops!

src/Nethereum.JsonRpc.IpcClient/IpcClient.cs
@@ -36,7 +36,7 @@ namespace Nethereum.JsonRpc.IpcClient
                 if (_pipeClient == null || !_pipeClient.IsConnected)
                 {
                     _pipeClient = new NamedPipeClientStream(IpcPath);
-                    _pipeClient.Connect(ConnectionTimeout);
+                    _pipeClient.Connect((int)ConnectionTimeout.TotalMilliseconds);
                 }
             }
             catch (TimeoutException ex)