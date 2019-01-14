b4f57b382fdded9949781ab10a2551299bc1461a
Roy Stoof <contact@protoman.net>
2019-01-14 23:44:58 +0100

    Add general polling extensions

---
+++ src/Nethereum.RPC.Reactive/Polling/PollingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Nethereum.RPC.Reactive.UnitTests")]
+
+namespace Nethereum.RPC.Reactive.Polling
+{
+    public static partial class Polling
+    {
+        public static IObservable<Unit> DefaultPoller = Observable
+            .Timer(TimeSpan.FromMilliseconds(250))
+            .Select(_ => Unit.Default);
+
+        internal static IObservable<T> Poll<T>(this IObservable<T> query, IObservable<Unit> poller) => poller
+            .SelectMany(_ => query)
+            .Repeat();
+
+        internal static IObservable<T> Poll<T>(this IObservable<T> query) => query.Poll(DefaultPoller);
+    }
+}
