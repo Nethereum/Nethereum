15b2b22b5190f6f2f306a5dfa668abd406d09305
Roy Stoof <contact@protoman.net>
2019-01-19 17:01:37 +0100

    Add enumerable extensions

---
+++ src/Nethereum.RPC.Reactive/Polling/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Nethereum.RPC.Reactive.UnitTests")]
+
+namespace Nethereum.RPC.Reactive.Polling
+{
+    internal static class EnumerableExtensions
+    {
+        internal static IEnumerable<BigInteger> Range(BigInteger start, BigInteger count)
+        {
+            if (count < 0) throw new ArgumentOutOfRangeException(nameof(count));
+
+            for (var i = 0; i < count; i++)
+                yield return start + i;
+        }
+    }
+}
