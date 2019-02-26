d8a4dbf7aa4e8cc454ec500d0fbeda8f83a8e793
Juan Blanco <juanfranblanco@hotmail.com>
2019-02-26 12:49:59 +0000

    More feedback on the event

--- src/Nethereum.Contracts/FilterInputNotForEventException.cs
+++ src/Nethereum.Contracts/FilterInputNotForEventException.cs
@@ -4,7 +4,7 @@ namespace Nethereum.Contracts
 {
     public class FilterInputNotForEventException : Exception
     {
-        public FilterInputNotForEventException() : base("Invalid filter input for current event, the event signatures, or contract addresses do not match, please use the method CreateFilterInput from this class")
+        public FilterInputNotForEventException() : base("Invalid filter input for current event, the event signatures (abi types, names), or contract addresses do not match, please use the method CreateFilterInput from this class")
         {
 
         }
