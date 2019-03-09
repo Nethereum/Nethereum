2b8f8a1e4c4c394381fda95e590bcd48d90b17c6
Juan Blanco <juanfranblanco@hotmail.com>
2019-03-09 13:10:08 +0000

    Fix for parity not returning end of message on websockets

--- src/Nethereum.JsonRpc.WebSocketClient/WebSocketClient.cs
+++ src/Nethereum.JsonRpc.WebSocketClient/WebSocketClient.cs
@@ -94,7 +94,7 @@ namespace Nethereum.JsonRpc.WebSocketClient
                 memoryStream.Write(buffer, 0, bytesRead);
                 var lastByte = buffer[bytesRead - 1];
 
-                if (lastByte == 10)  //return signalled with a line feed
+                if (lastByte == 10 || bytesRead < 512)  //return signalled with a line feed / or just less than the full message
                 {
                     bytesRead = 0;
                 }
--- src/Nethereum.Parity.Reactive.ConsoleTest/Program.cs
+++ src/Nethereum.Parity.Reactive.ConsoleTest/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using Nethereum.Hex.HexTypes;
+using Nethereum.JsonRpc.IpcClient;
+using Nethereum.JsonRpc.WebSocketClient;
 using Nethereum.JsonRpc.WebSocketStreamingClient;
 using Nethereum.RPC.Eth;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3.Accounts;
+using Nethereum.Web3.Accounts.Managed;
 
 namespace Nethereum.Parity.Reactive.ConsoleTest
 {
@@ -11,7 +14,19 @@ namespace Nethereum.Parity.Reactive.ConsoleTest
     {
         static void Main(string[] args)
         {
-            
+
+            var account = new ManagedAccount("0x12890d2cce102216644c59daE5baed380d84830c", "password");
+            var clientws = new WebSocketClient("ws://127.0.0.1:8546");
+            var web3ws = new Web3.Web3(account, clientws);
+            var res =  web3ws.Eth.Blocks.GetBlockNumber.SendRequestAsync().Result; //task cancelled exception
+
+
+
+            var clientipc = new IpcClient("jsonrpc.ipc");
+            var web3ipc = new Web3.Web3(account, clientipc);
+            var resIpc = web3ws.Eth.Blocks.GetBlockNumber.SendRequestAsync().Result; 
+
+
             var client = new StreamingWebSocketClient("ws://127.0.0.1:8546");
             var accountBalanceSubscription = new ParityPubSubObservableSubscription<HexBigInteger>(client);
             var ethBalanceRequest = new EthGetBalance().BuildRequest("0x12890d2cce102216644c59daE5baed380d84830c", BlockParameter.CreateLatest());
