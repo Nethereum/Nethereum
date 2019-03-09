996775ed9c2e58db48e040e628a97f938beb53c8
Juan Blanco <juanfranblanco@hotmail.com>
2019-03-09 12:40:20 +0000

    Parity pub / sub test completed

--- src/Nethereum.Parity.Reactive.ConsoleTest/Program.cs
+++ src/Nethereum.Parity.Reactive.ConsoleTest/Program.cs
@@ -3,6 +3,7 @@ using Nethereum.Hex.HexTypes;
 using Nethereum.JsonRpc.WebSocketStreamingClient;
 using Nethereum.RPC.Eth;
 using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Web3.Accounts;
 
 namespace Nethereum.Parity.Reactive.ConsoleTest
 {
@@ -13,15 +14,25 @@ namespace Nethereum.Parity.Reactive.ConsoleTest
             
             var client = new StreamingWebSocketClient("ws://127.0.0.1:8546");
             var accountBalanceSubscription = new ParityPubSubObservableSubscription<HexBigInteger>(client);
-            var ethBalanceRequest = new EthGetBalance().BuildRequest("0x", BlockParameter.CreateLatest());
+            var ethBalanceRequest = new EthGetBalance().BuildRequest("0x12890d2cce102216644c59daE5baed380d84830c", BlockParameter.CreateLatest());
 
             accountBalanceSubscription.GetSubscriptionDataResponsesAsObservable().Subscribe(newBalance =>
                 Console.WriteLine("New Balance: " + newBalance.Value.ToString()));
 
+            client.Start().Wait();
+
             accountBalanceSubscription.SubscribeAsync(ethBalanceRequest).Wait();
             // do transfer 
 
-            
+            var web3 = new Web3.Web3(new Account("0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7"));
+
+            for (int i = 0; i < 10; i++)
+            {
+                web3.Eth.GetEtherTransferService()
+                    .TransferEtherAndWaitForReceiptAsync("0x12890d2cce102216644c59daE5baed380d848306", 10).Wait();
+
+            }
+
         }
     }
 }
