ab1fbc3b54803a32c83ba240f2f4ab774b9e5144
Juan Blanco <juanfranblanco@hotmail.com>
2019-04-01 10:05:27 +0100

    Log changes

---
+++ src/Nethereum.Model/Log.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Nethereum.Model
+{
+    public class Log
+    {
+        public string Address { get; set; }
+        public byte[] Data { get; set; }
+        public List<byte[]> Topics { get; set; } = new List<byte[]>();
+
+        public static Log Create(byte[] data, string address, params byte[][] topics)
+        {
+            var log = new Log(){Data = data, Address = address};
+            log.Topics.AddRange(topics);
+            return log;
+        }
+
+        public static Log Create(string address, params byte[][] topics)
+        {
+            return Create(null, address, topics);
+        }
+    }
+}
