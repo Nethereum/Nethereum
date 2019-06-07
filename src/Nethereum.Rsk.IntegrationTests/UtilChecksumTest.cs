aa658d7eeead9b00a6e738f029d57541a6f079d2
Juan Blanco <juanfranblanco@hotmail.com>
2019-06-07 18:09:17 +0100

    Extra tests

--- src/Nethereum.Rsk.IntegrationTests/UtilChecksumTest.cs
+++ src/Nethereum.Rsk.IntegrationTests/UtilChecksumTest.cs
@@ -10,10 +10,17 @@ namespace Nethereum.Rsk.IntegrationTests
         [InlineData("0xFb6916095cA1Df60bb79ce92cE3EA74c37c5d359", 30, true)]
         [InlineData("0xDBF03B407c01E7CD3cBea99509D93F8Dddc8C6FB", 30, true)]
         [InlineData("0xD1220A0Cf47c7B9BE7a2e6ba89F429762E7B9adB", 30, true)]
+
+        [InlineData("0x5aaeb6053F3E94C9B9A09f33669435E7Ef1BeAed", 1234, false)]
+        [InlineData("0xfb6916095ca1df60bb79Ce92ce3ea74c37c5d359", 1234, false)]
+        [InlineData("0xDBF03B407C01E7CD3CBEA99509D93f8DDDC8C6FB", 1234, false)]
+        [InlineData("0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb", 1234, false)]
+
         [InlineData("0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed", null, true)]
         [InlineData("0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359", null, true)]
         [InlineData("0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB", null, true)]
         [InlineData("0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb", null, true)]
+
         public void ValidateAddress(string address, int? network, bool isValid)
         {
             var result = AddressUtil.Current.IsChecksumAddress(address, network);
