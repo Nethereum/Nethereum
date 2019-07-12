b0aed1a0f12ee9c8ae286292f2a14cdcb0a39d50
Dave Whiffin <dave.whiffin@gmail.com>
2019-07-12 14:41:42 +0100

    Changing Long to BigInteger for ITransactionLogRepository

--- src/Nethereum.BlockchainProcessing/Storage/Repositories/ITransactionLogRepository.cs
+++ src/Nethereum.BlockchainProcessing/Storage/Repositories/ITransactionLogRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Numerics;
+using System.Threading.Tasks;
 using Nethereum.BlockchainProcessing.Storage.Entities;
 using Nethereum.RPC.Eth.DTOs;
 
@@ -7,6 +8,6 @@ namespace Nethereum.BlockchainProcessing.Storage.Repositories
     public interface ITransactionLogRepository
     {
         Task UpsertAsync(FilterLogVO log);
-        Task<ITransactionLogView> FindByTransactionHashAndLogIndexAsync(string hash, long idx);
+        Task<ITransactionLogView> FindByTransactionHashAndLogIndexAsync(string hash, BigInteger logIndex);
     }
 }
