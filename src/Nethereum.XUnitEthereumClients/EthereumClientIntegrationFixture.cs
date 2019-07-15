b35f58009ba3986b8f738f20a4d9474d6c9473b5
Dave Whiffin <dave.whiffin@gmail.com>
2019-07-15 10:55:50 +0100

    geth testchain specifying --allow-insecure-unlock

--- src/Nethereum.XUnitEthereumClients/EthereumClientIntegrationFixture.cs
+++ src/Nethereum.XUnitEthereumClients/EthereumClientIntegrationFixture.cs
@@ -134,7 +134,7 @@ namespace Nethereum.XUnitEthereumClients
                 Thread.Sleep(3000);
 
                 var psi = new ProcessStartInfo(Path.Combine(_exePath, "geth.exe"),
-                    @" --nodiscover --rpc --datadir=devChain  --rpccorsdomain "" * "" --mine --rpcapi ""eth, web3, personal, net, miner, admin, debug"" --rpcaddr ""0.0.0.0"" --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password ""pass.txt"" --verbosity 0 console  ")
+                    @" --nodiscover --rpc --datadir=devChain  --rpccorsdomain "" * "" --mine --rpcapi ""eth, web3, personal, net, miner, admin, debug"" --rpcaddr ""0.0.0.0"" --allow-insecure-unlock --unlock 0x12890d2cce102216644c59daE5baed380d84830c --password ""pass.txt"" --verbosity 0 console  ")
                 {
                     CreateNoWindow = false,
                     WindowStyle = ProcessWindowStyle.Normal,
