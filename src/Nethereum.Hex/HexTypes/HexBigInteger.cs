4f8288ef92aa27d7e5e66b497e4d409caafa2e96
Dave Whiffin <dave.whiffin@gmail.com>
2019-07-22 12:50:54 +0100

    IEquatable HexBigInteger with tests

--- src/Nethereum.Hex/HexTypes/HexBigInteger.cs
+++ src/Nethereum.Hex/HexTypes/HexBigInteger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Numerics;
 using Nethereum.Hex.HexConvertors;
 using Newtonsoft.Json;
@@ -5,7 +6,7 @@ using Newtonsoft.Json;
 namespace Nethereum.Hex.HexTypes
 {
     [JsonConverter(typeof(HexRPCTypeJsonConverter<HexBigInteger, BigInteger>))]
-    public class HexBigInteger : HexRPCType<BigInteger>
+    public class HexBigInteger : HexRPCType<BigInteger>, IEquatable<HexBigInteger>
     {
         public HexBigInteger(string hex) : base(new HexBigIntegerBigEndianConvertor(), hex)
         {
@@ -19,10 +20,38 @@ namespace Nethereum.Hex.HexTypes
         {
             if (obj is HexBigInteger val)
             {
-                return val.Value == Value;
+                return Equals(val);
             }
 
             return false;
+        }
+
+        public bool Equals(HexBigInteger other)
+        {
+            return other == null ? false : other.Value == Value;
+        }
+
+        public static bool operator == (HexBigInteger lhs, HexBigInteger rhs)
+        {
+            // Check for null on left side.
+            if (Object.ReferenceEquals(lhs, null))
+            {
+                if (Object.ReferenceEquals(rhs, null))
+                {
+                    // null == null = true.
+                    return true;
+                }
+
+                // Only the left side is null.
+                return false;
+            }
+            // Equals handles case of null on right side.
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator != (HexBigInteger lhs, HexBigInteger rhs)
+        {
+            return !(lhs == rhs);
         }
 
         public override string ToString()
--- src/Nethereum.Util.UnitTests/HexBigIntegerTests.cs
+++ src/Nethereum.Util.UnitTests/HexBigIntegerTests.cs
@@ -1,5 +1,4 @@
-﻿using Nethereum.Hex.HexConvertors;
-using Nethereum.Hex.HexTypes;
+﻿using Nethereum.Hex.HexTypes;
 using Xunit;
 
 namespace Nethereum.Util.UnitTests
@@ -15,5 +14,50 @@ namespace Nethereum.Util.UnitTests
             Assert.Equal("0", $"{new HexBigInteger(null)}");
             Assert.Equal("1000", $"{new HexBigInteger("3E8")}");
         }
+
+        [Fact]
+        public void TwoHexBigIntegersWithTheSameValueAreEqual()
+        {
+            var val1 = new HexBigInteger(100);
+            var val2 = new HexBigInteger(100);
+
+            Assert.Equal(val1, val2);
+            Assert.True(val1 == val2);
+            Assert.True(val1.Equals(val2));
+        }
+
+        [Fact]
+        public void TwoHexBigIntegersWithDifferingValuesAreNotEqual()
+        {
+            var val1 = new HexBigInteger(100);
+            var val2 = new HexBigInteger(101);
+
+            Assert.NotEqual(val1, val2);
+        }
+
+        [Fact]
+        public void TwoNullHexBigIntegersAreEqual()
+        {
+            HexBigInteger val1 = null;
+            HexBigInteger val2 = null;
+
+            Assert.Equal(val1, val2);
+        }
+
+        [Fact]
+        public void ANonNullHexBigIntegerIsNotEqualToANull()
+        {
+            var nonNull = new HexBigInteger(100);
+            HexBigInteger nullHexBigInteger = null;
+
+            Assert.NotEqual(nonNull, nullHexBigInteger);
+            Assert.NotEqual(nullHexBigInteger, nonNull);
+            Assert.False(nonNull == nullHexBigInteger);
+            Assert.False(nullHexBigInteger == nonNull);
+            Assert.True(nonNull != nullHexBigInteger);
+            Assert.True(nullHexBigInteger != nonNull);
+        }
+
+
     }
 }
