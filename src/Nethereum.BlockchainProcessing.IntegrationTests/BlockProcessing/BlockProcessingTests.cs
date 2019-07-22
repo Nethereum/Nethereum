1e9eb04ffd860a2e43875a878dd28caaa136398f
Dave Whiffin <dave.whiffin@gmail.com>
2019-07-22 12:01:38 +0100

    Block Processing Test WIP

--- src/Nethereum.BlockchainProcessing.IntegrationTests/BlockProcessing/BlockProcessingTests.cs
+++ src/Nethereum.BlockchainProcessing.IntegrationTests/BlockProcessing/BlockProcessingTests.cs
@@ -19,81 +19,138 @@ using Xunit;
 
 namespace Nethereum.BlockchainProcessing.IntegrationTests.BlockProcessing
 {
-    public class BlockProcessingTests
+    public class BlockingProcessingRpcMock
     {
-        public class RpcResponses
+        public BlockingProcessingRpcMock(Web3Mock web3Mock)
         {
-            public List<BlockWithTransactions> Blocks { get;set;} = new List<BlockWithTransactions>();
-
-            public List<TransactionReceipt> Receipts { get;set;} = new List<TransactionReceipt>();
-
-        }
-
-        [Fact]
-        public async Task ShouldCrawlBlocksAndInvokeHandlingSteps()
-        {
-            var web3Mock = new Web3Mock();
-            var processingSteps = new BlockProcessingSteps();
-            var orchestrator = new BlockCrawlOrchestrator(web3Mock.EthApiContractServiceMock.Object, new []{ processingSteps });
-            var progressRepository = new InMemoryBlockchainProgressRepository(lastBlockProcessed: null);
-            var lastConfirmedBlockService = new LastConfirmedBlockNumberService(web3Mock.BlockNumberMock.Object);
-            var blockchainProcessor = new BlockchainProcessor(orchestrator, progressRepository, lastConfirmedBlockService);
-
-            var mockRpcResponses = new RpcResponses();
-
-            var currentBlockOnChain = new HexBigInteger(100);
-
-            // 0x243e72b69141f6af525a9a5fd939668ee9f2b354
-
-            mockRpcResponses.Blocks.Add(new BlockWithTransactions
-            {
-                Number = new HexBigInteger(1),
-                Transactions = new[]
-                {
-                    new Transaction{TransactionHash = "0x10ce02e0b4fdf5cfee0ed21141b38c2d88113c58828c771e813ce2624af127cd", TransactionIndex = new HexBigInteger(1)},
-                    new Transaction{TransactionHash = "0x20ce02e0b4fdf5cfee0ed21141b38c2d88113c58828c771e813ce2624af127cd", TransactionIndex = new HexBigInteger(2)}
-                }
-            });
-
-            mockRpcResponses.Receipts.AddRange(new []
-            {
-                new TransactionReceipt { TransactionHash = "0x10ce02e0b4fdf5cfee0ed21141b38c2d88113c58828c771e813ce2624af127cd", Logs = new []{ new FilterLog() }.ConvertToJArray() },
-                new TransactionReceipt { TransactionHash = "0x20ce02e0b4fdf5cfee0ed21141b38c2d88113c58828c771e813ce2624af127cd", Logs = new []{ new FilterLog() }.ConvertToJArray() } 
-            });
-
-            web3Mock.BlockNumberMock.Setup(m => m.SendRequestAsync(null)).ReturnsAsync(currentBlockOnChain);
 
             web3Mock.GetBlockWithTransactionsByNumberMock.Setup(s => s.SendRequestAsync(It.IsAny<HexBigInteger>(), null))
                 .Returns<HexBigInteger, object>((n, id) =>
                 {
-                    return Task.FromResult(mockRpcResponses.Blocks.FirstOrDefault(b => b.Number == n));
+                    return Task.FromResult(Blocks.FirstOrDefault(b => b.Number == n));
                 });
 
             web3Mock.GetTransactionReceiptMock.Setup(s => s.SendRequestAsync(It.IsAny<string>(), null))
                 .Returns<string, object>((hash, id) =>
                 {
-                    return Task.FromResult(mockRpcResponses.Receipts.FirstOrDefault(b => b.TransactionHash == hash));
+                    return Task.FromResult(Receipts.FirstOrDefault(b => b.TransactionHash == hash));
                 });
+        }
+
+        public List<BlockWithTransactions> Blocks { get; set; } = new List<BlockWithTransactions>();
+
+        public List<TransactionReceipt> Receipts { get; set; } = new List<TransactionReceipt>();
+
+        public void AddTransactionsWithReceipts(int blockNumber, int numberOfTransactions, int logsPerTransaction)
+        {
+            var transactions = new Transaction[numberOfTransactions];
+
+            for (var i = 0; i < numberOfTransactions; i++)
+            {
+                transactions[0] = new Transaction
+                {
+                    TransactionHash = $"0x{blockNumber}{i}ce02e0b4fdf5cfee0ed21141b38c2d88113c58828c771e813ce2624af127cd",
+                    TransactionIndex = new HexBigInteger(i)
+                };
+            }
+
+            Blocks.Add(new BlockWithTransactions
+            {
+                Number = new HexBigInteger(blockNumber),
+                Transactions = transactions
+            });
+
+            foreach (var tx in transactions)
+            {
+                var logs = new FilterLog[logsPerTransaction];
+
+                for (var l = 0; l < logsPerTransaction; l++)
+                {
+                    logs[l] = new FilterLog();
+                }
+
+                Receipts.AddRange(new[] {new TransactionReceipt {
+                        TransactionHash = tx.TransactionHash,
+                        Logs = logs.ConvertToJArray() },
+                    });
+            }
+        }
+
+    }
+
+    public class ProcessedBlockchainData
+    {
+        public List<BlockWithTransactions> Blocks { get; set; } = new List<BlockWithTransactions>();
+        public List<TransactionVO> Transactions { get; set; } = new List<TransactionVO>();
+        public List<TransactionReceiptVO> TransactionsWithReceipt { get; set; } = new List<TransactionReceiptVO>();
+        public List<ContractCreationVO> ContractCreations { get; set; } = new List<ContractCreationVO>();
+        public List<FilterLogVO> FilterLogs { get; set; } = new List<FilterLogVO>();
+    }
+
+    public class BlockProcessingTestBase
+    {
+        protected Web3Mock Web3Mock;
+        protected BlockProcessingSteps processingSteps;
+        protected BlockCrawlOrchestrator orchestrator;
+        protected InMemoryBlockchainProgressRepository progressRepository;
+        protected LastConfirmedBlockNumberService lastConfirmedBlockService;
+        protected BlockchainProcessor blockchainProcessor;
+        protected Queue<HexBigInteger> blockNumberQueue;
+        protected BlockingProcessingRpcMock mockRpcResponses;
+        protected ProcessedBlockchainData processedBlockchainData;
+
+        public BlockProcessingTestBase()
+        {
+            Web3Mock = new Web3Mock();
+            processedBlockchainData = new ProcessedBlockchainData();
+        }
+
+        protected virtual void Initialise()
+        {
+            processingSteps = new BlockProcessingSteps();
+            orchestrator = new BlockCrawlOrchestrator(Web3Mock.EthApiContractServiceMock.Object, new[] { processingSteps });
+            progressRepository = new InMemoryBlockchainProgressRepository(lastBlockProcessed: null);
+            lastConfirmedBlockService = new LastConfirmedBlockNumberService(Web3Mock.BlockNumberMock.Object);
+            blockchainProcessor = new BlockchainProcessor(orchestrator, progressRepository, lastConfirmedBlockService);
+            blockNumberQueue = new Queue<HexBigInteger>();
+
+            Web3Mock
+                .BlockNumberMock
+                .Setup(m => m.SendRequestAsync(null))
+                .Returns(() => Task.FromResult(blockNumberQueue.Dequeue()));
+
+            mockRpcResponses = new BlockingProcessingRpcMock(Web3Mock);
+        }
+
+        protected virtual void MockGetBlockNumber(BigInteger blockNumberToReturn)
+        {
+            blockNumberQueue.Enqueue(new HexBigInteger(blockNumberToReturn));
+        }
+    }
 
 
-            var blocks = new List<BlockWithTransactions>();
-            var transactions = new List<TransactionVO>();
-            var transactionsWithReceipt = new List<TransactionReceiptVO>();
-            var contractCreations = new List<ContractCreationVO>();
-            var filterLogs = new List<FilterLogVO>();
+    public class BlockProcessingTests : BlockProcessingTestBase
+    {
 
-            processingSteps.BlockStep.AddSynchronousProcessorHandler((block) => blocks.Add(block));
-            processingSteps.TransactionStep.AddSynchronousProcessorHandler((tx) => transactions.Add(tx));
-            processingSteps.TransactionReceiptStep.AddSynchronousProcessorHandler((tx) => transactionsWithReceipt.Add(tx));
-            processingSteps.ContractCreationStep.AddSynchronousProcessorHandler((c) => contractCreations.Add(c));
-            processingSteps.FilterLogStep.AddSynchronousProcessorHandler((filterLog) => filterLogs.Add(filterLog));
+        [Fact]
+        public async Task ShouldCrawlBlocksAndInvokeHandlingSteps()
+        {
+            Initialise();
+            MockGetBlockNumber(100);
 
-            var fromBlock = new BigInteger(1);
-            var toBlock = fromBlock;
+            mockRpcResponses.AddTransactionsWithReceipts(blockNumber: 1, numberOfTransactions: 2, logsPerTransaction: 2);
+
+            processingSteps.BlockStep.AddSynchronousProcessorHandler((block) => processedBlockchainData.Blocks.Add(block));
+            processingSteps.TransactionStep.AddSynchronousProcessorHandler((tx) => processedBlockchainData.Transactions.Add(tx));
+            processingSteps.TransactionReceiptStep.AddSynchronousProcessorHandler((tx) => processedBlockchainData.TransactionsWithReceipt.Add(tx));
+            processingSteps.ContractCreationStep.AddSynchronousProcessorHandler((c) => processedBlockchainData.ContractCreations.Add(c));
+            processingSteps.FilterLogStep.AddSynchronousProcessorHandler((filterLog) => processedBlockchainData.FilterLogs.Add(filterLog));
+
+            var blockToCrawl = new BigInteger(1);
 
             var cancellationTokenSource = new CancellationTokenSource();
 
-            var progress =  await blockchainProcessor.ExecuteAsync(toBlock, cancellationTokenSource.Token, fromBlock);
+            await blockchainProcessor.ExecuteAsync(blockToCrawl, cancellationTokenSource.Token, blockToCrawl);
 
 
         }
