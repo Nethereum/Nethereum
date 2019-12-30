75155d6f648f20c940d08f49183e14f0ff96d62a
binhsignkeys <56526928+binhsignkeys@users.noreply.github.com>
2019-12-30 08:13:34 +0700

    test for shh symkey

---
+++ src/Nethereum.RPC.IntegrationTests/Testers/ShhNewSymKeyTester.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nethereum.JsonRpc.Client;
+using Nethereum.RPC.Shh.KeyPair;
+using Nethereum.RPC.Shh.SymKey;
+using Xunit;
+
+namespace Nethereum.RPC.Tests.Testers
+{
+    public class ShhNewSymKeyTester : RPCRequestTester<string>, IRPCRequestTester
+    {
+        [Fact]
+        public async void ShouldReturnTheSymKey()
+        {
+            var result = await ExecuteAsync();
+            Assert.NotNull(result);
+        }
+
+        public override async Task<string> ExecuteAsync(IClient client)
+        {
+            var shhNewKeyPair = new ShhNewSymKey(client);
+            return await shhNewKeyPair.SendRequestAsync();
+        }
+
+        public override Type GetRequestType()
+        {
+            return typeof(ShhNewKeyPair);
+        }
+    }
+}
---
+++ src/Nethereum.RPC.IntegrationTests/Testers/ShhSymKeyTester.cs
@@ -0,0 +1,45 @@
+﻿using Nethereum.JsonRpc.Client;
+using Nethereum.RPC.Shh.KeyPair;
+using Nethereum.RPC.Shh.SymKey;
+using Nethereum.RPC.Tests.Testers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nethereum.RPC.Tests.Testers
+{
+    public class ShhSymKeyTester : RPCRequestTester<bool>, IRPCRequestTester
+    {
+        [Fact]
+        public async void ShouldReturnResult()
+        {
+            var addSymKey = new ShhAddSymKey(Client);
+            var hasSymKey = new ShhHasSymKey(Client);
+            var getSymKey = new ShhGetSymKey(Client); 
+            var deleteSymKey = new ShhDeleteSymKey(Client);
+
+            var addResult = await addSymKey.SendRequestAsync(Settings.GetDefaultPrivateKey());
+            var hasSymKeyResult = await hasSymKey.SendRequestAsync(addResult);
+            var getSymKeyResult = await getSymKey.SendRequestAsync(addResult); 
+            var deleteSymKeyResult = await deleteSymKey.SendRequestAsync(addResult);
+
+            Assert.NotNull(addResult);
+            Assert.True(hasSymKeyResult);
+            Assert.Equal(Settings.GetDefaultPrivateKey(), getSymKeyResult); 
+            Assert.True(deleteSymKeyResult);
+        }
+
+        public override Task<bool> ExecuteAsync(IClient client)
+        {
+            var shhAddPrivateKey = new ShhDeleteKeyPair(client);
+            return shhAddPrivateKey.SendRequestAsync(Settings.GetDefaultPrivateKey());
+        }
+
+        public override Type GetRequestType()
+        {
+            return typeof(ShhAddPrivateKey);
+        }
+    }
+}
