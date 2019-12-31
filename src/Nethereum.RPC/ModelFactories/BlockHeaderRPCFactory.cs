2ffe4deec319bd9dcd81d3dd4a5bb47b38f472ae
Juan Blanco <juanfranblanco@hotmail.com>
2019-12-31 08:50:45 +0000

    Support for parity clique mixhash and nonce in SealFields

--- src/Nethereum.RPC/ModelFactories/BlockHeaderRPCFactory.cs
+++ src/Nethereum.RPC/ModelFactories/BlockHeaderRPCFactory.cs
@@ -6,7 +6,7 @@ namespace Nethereum.RPC.ModelFactories
 {
     public class BlockHeaderRPCFactory
     {
-        public static BlockHeader FromRPC(Block rpcBlock)
+        public static BlockHeader FromRPC(Block rpcBlock, bool mixHasAndhNonceInSealFields = false)
         {
             var blockHeader = new BlockHeader();
             blockHeader.BlockNumber = rpcBlock.Number;
@@ -16,14 +16,23 @@ namespace Nethereum.RPC.ModelFactories
             blockHeader.GasLimit = (long)rpcBlock.GasLimit.Value;
             blockHeader.GasUsed = (long)rpcBlock.GasUsed.Value;
             blockHeader.LogsBloom = rpcBlock.LogsBloom.HexToByteArray();
-            blockHeader.MixHash = rpcBlock.MixHash.HexToByteArray();
-            blockHeader.Nonce = rpcBlock.Nonce.HexToByteArray();
             blockHeader.ParentHash = rpcBlock.ParentHash.HexToByteArray();
             blockHeader.ReceiptHash = rpcBlock.ReceiptsRoot.HexToByteArray();
             blockHeader.StateRoot = rpcBlock.StateRoot.HexToByteArray();
             blockHeader.Timestamp = (long)rpcBlock.Timestamp.Value;
             blockHeader.TransactionsHash = rpcBlock.TransactionsRoot.HexToByteArray();
             blockHeader.UnclesHash = rpcBlock.Sha3Uncles.HexToByteArray();
+
+            if (mixHasAndhNonceInSealFields && rpcBlock.SealFields != null && rpcBlock.SealFields.Length >= 2)
+            {
+                blockHeader.MixHash = rpcBlock.SealFields[0].HexToByteArray();
+                blockHeader.Nonce = rpcBlock.SealFields[1].HexToByteArray();
+            }
+            else
+            {
+                blockHeader.MixHash = rpcBlock.MixHash.HexToByteArray();
+                blockHeader.Nonce = rpcBlock.Nonce.HexToByteArray();
+            }
             return blockHeader;
         }
     }
