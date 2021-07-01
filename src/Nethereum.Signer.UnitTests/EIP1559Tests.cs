c4e7faf406913d85f3b5245b6125aff64ce4310e
Juan Blanco <juanfranblanco@hotmail.com>
2021-07-01 11:16:14 +0100

    Refactoring 1559 signing tests

--- src/Nethereum.Signer.UnitTests/EIP1559Tests.cs
+++ src/Nethereum.Signer.UnitTests/EIP1559Tests.cs
@@ -1,23 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
-using System.Numerics;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using Nethereum.Hex.HexConvertors.Extensions;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.RPC.TransactionReceipts;
 using Nethereum.Util;
-using Newtonsoft.Json;
-using Org.BouncyCastle.Asn1.Mozilla;
 using Xunit;
 
 namespace Nethereum.Signer.UnitTests
 {
     public class EIP1559Test
     {
-
 
         [Theory]
         [InlineData(1559, 2, 3, 4, 3500, "0x1ad91ee08f21be3de0ba2ba6918e714da6b45836", 10, "")]
@@ -149,90 +142,5 @@ namespace Nethereum.Signer.UnitTests
             Assert.Equal(transaction1559.Signature.R.ToHex(), decodedTransaction1559.Signature.R.ToHex());
             Assert.Equal(transaction1559.Signature.S.ToHex(), decodedTransaction1559.Signature.S.ToHex());
         }
-
-        //INTEGRATION TESTING
-
-        [Fact]
-        public async void ShouldSendTransactionWithAccessLists()
-        {
-            var chainId = 444444444500;
-
-            var accessLists = new List<AccessListItem>();
-            accessLists.Add(new AccessListItem("0x527306090abaB3A6e1400e9345bC60c78a8BEf57",
-                new List<byte[]>
-                {
-                    "0x3fd54831f488a22b28398de0c567a3b064b937f54f81739ae9bd545967f3abab".HexToByteArray(),
-                    "0x3fd54831f488a22b28398de0c567a3b064b937f54f81739ae9bd545967f3abac".HexToByteArray()
-                }
-            ));
-            accessLists.Add(new AccessListItem("0x427306090abaB3A6e1400e9345bC60c78a8BEf5c",
-                new List<byte[]>
-                {
-                    "0x3fd54831f488a22b28398de0c567a3b064b937f54f81739ae9bd545967f3abaa".HexToByteArray(),
-                    "0x3fd54831f488a22b28398de0c567a3b064b937f54f81739ae9bd545967f3abad".HexToByteArray()
-                }
-            ));
-
-            var web3 = new Web3.Web3();
-            var nonce = await web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(
-                "0x12890D2cce102216644c59daE5baed380d84830c");
-            var lastBlock =
-                await web3.Eth.Blocks.GetBlockWithTransactionsHashesByNumber.SendRequestAsync(
-                    BlockParameter.CreateLatest());
-            var baseFee = lastBlock.BaseFeePerGas;
-            var maxPriorityFeePerGas = 2000000000;
-            var maxFeePerGas = baseFee.Value * 2 + 2000000000;
-
-            var transaction1559 = new Transaction1559(chainId, nonce.Value, maxPriorityFeePerGas, maxFeePerGas, 45000, "0x1ad91ee08f21be3de0ba2ba6918e714da6b45836", 10, "", accessLists);
-            transaction1559.Sign(new EthECKey("0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7"));
-
-
-            var txnHash = await web3.Eth.Transactions.SendRawTransaction.SendRequestAsync(transaction1559.GetRLPEncoded().ToHex());
-            // create recover signature
-            var txn = await web3.Eth.Transactions.GetTransactionByHash.SendRequestAsync(txnHash);
-
-            Assert.True(txn.To.IsTheSameAddress("0x1ad91ee08f21be3de0ba2ba6918e714da6b45836"));
-            Assert.Equal(10, txn.Value.Value);
-
-        }
-
-
-        [Fact]
-        public async void ShouldSendTransactionCalculatingTheDefaultFees()
-        {
-            var chainId = 444444444500;
-            var web3 = new Web3.Web3();
-            var nonce = await web3.Eth.Transactions.GetTransactionCount.SendRequestAsync(
-                "0x12890D2cce102216644c59daE5baed380d84830c");
-            var lastBlock =
-                await web3.Eth.Blocks.GetBlockWithTransactionsHashesByNumber.SendRequestAsync(
-                    BlockParameter.CreateLatest());
-            var baseFee = lastBlock.BaseFeePerGas;
-            var maxPriorityFeePerGas = 2000000000;
-            var maxFeePerGas = baseFee.Value * 2 + 2000000000;
-
-            var transaction1559 = new Transaction1559(chainId, nonce.Value, maxPriorityFeePerGas, maxFeePerGas, 45000, "0x1ad91ee08f21be3de0ba2ba6918e714da6b45836", 10, "", null);
-            transaction1559.Sign(new EthECKey("0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7"));
-           
-            
-            var txnHash = await web3.Eth.Transactions.SendRawTransaction.SendRequestAsync(transaction1559.GetRLPEncoded().ToHex());
-            // create recover signature
-            var txn = await web3.Eth.Transactions.GetTransactionByHash.SendRequestAsync(txnHash);
-            //what I want is to get the right Transaction checking the type or chainid etc and do a recovery
-            Assert.True(txn.To.IsTheSameAddress("0x1ad91ee08f21be3de0ba2ba6918e714da6b45836"));
-            Assert.Equal(10, txn.Value.Value);
-
-            var transaction1559FromChain = TransactionFactory.CreateTransaction(chainId, (byte)txn.Type.Value, txn.Nonce, txn.MaxPriorityFeePerGas,
-                txn.MaxFeePerGas, null, txn.Gas, txn.To, txn.Value, txn.Input, null, txn.R, txn.S, txn.V);
-
-            Assert.True(transaction1559FromChain.GetSenderAddress().IsTheSameAddress("0x12890D2cce102216644c59daE5baed380d84830c"));
-
-            var transactionReceipt =
-                await new TransactionReceiptPollingService(web3.TransactionManager).PollForReceiptAsync(txnHash, new CancellationTokenSource());
-
-        }
-
-
-
     }
 }
