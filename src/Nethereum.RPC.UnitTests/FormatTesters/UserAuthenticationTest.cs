302d99346cc005b2a5e5a52d2132509795438e85
Juan Blanco <juanfranblanco@hotmail.com>
2021-07-19 15:23:13 +0100

    Refactoring user authentication tests

--- src/Nethereum.RPC.UnitTests/FormatTesters/UserAuthenticationTest.cs
+++ src/Nethereum.RPC.UnitTests/FormatTesters/UserAuthenticationTest.cs
@@ -9,31 +9,16 @@ namespace Nethereum.RPC.UnitTests.FormatTesters
         [Fact]
         public void ShouldDecodeUri()
         {
-            var userAuthentication = UserAuthentication.FromUrl("https://test:123@localhost:8545");
+            var userAuthentication = UserAuthentication.GetBasicAuthenticationUserInfoFromUri(new Uri("https://test:123@localhost:8545"));
             Assert.NotNull(userAuthentication);
             Assert.Equal("test", userAuthentication.UserName);
             Assert.Equal("123", userAuthentication.Password);
-            var header = userAuthentication.GetBasicAuthenticationHeaderValue();
+
+            var header = BasicAuthenticationHeaderHelper.GetBasicAuthenticationHeaderValueFromUri(new Uri("https://test:123@localhost:8545"));
 
             Assert.Equal("dGVzdDoxMjM=", header.Parameter);
             Assert.Equal("Basic", header.Scheme);
 
         }
-
-        //[Fact]
-        //public void ShouldBeAbleToAuthenticateUsingBasicScheme()
-        //{
-        //        IClient client = new RpcClient(new Uri(""));
-        //        var ethGetBlockNumber = new Eth.Blocks.EthBlockNumber(client);
-        //        var number = ethGetBlockNumber.SendRequestAsync().Result;
-        //}
-
-        //[Fact]
-        //public void ShouldBeAbleToAuthenticateUsingBasicSchemeNoUserNamePassword()
-        //{
-        //    IClient client = new RpcClient(new Uri("https://mainnet.infura.io"));
-        //    var ethGetBlockNumber = new Eth.Blocks.EthBlockNumber(client);
-        //    var number = ethGetBlockNumber.SendRequestAsync().Result;
-        //}
     }
 }
