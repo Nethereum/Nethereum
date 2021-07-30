a2ff0150c2a4d60654492b65a9ba553e62e31827
Juan Blanco <juanfranblanco@hotmail.com>
2021-07-30 15:42:43 +0100

    Adding more transactions tests

--- tests/Nethereum.Accounts.IntegrationTests/TransactionTests.cs
+++ tests/Nethereum.Accounts.IntegrationTests/TransactionTests.cs
@@ -23,9 +23,14 @@ namespace Nethereum.Accounts.IntegrationTests
             var toAddress = "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe";
             var receipt = await web3.Eth.GetEtherTransferService()
                 .TransferEtherAndWaitForReceiptAsync(toAddress, 1.11m, gasPriceGwei: 2).ConfigureAwait(false);
-            var transaction = await web3.Eth.Transactions.GetTransactionByHash.SendRequestAsync(receipt.TransactionHash);
-            Assert.NotNull(transaction.TransactionHash);
-
+            var tran = await web3.Eth.Transactions.GetTransactionByHash.SendRequestAsync(receipt.TransactionHash);
+            Assert.NotNull(tran.TransactionHash);
+            var blockWithTransactions =
+                await web3.Eth.Blocks.GetBlockWithTransactionsByNumber.SendRequestAsync(receipt.BlockNumber);
+            foreach (var transaction in blockWithTransactions.Transactions)
+            {
+                        Assert.NotNull(transaction.TransactionHash);
+            }
         }
 
     }
