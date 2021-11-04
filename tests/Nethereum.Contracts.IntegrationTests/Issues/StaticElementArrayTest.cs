ddfa487f757748ecec7ec42904abf16876033148
Juan Blanco <juanfranblanco@hotmail.com>
2021-11-04 11:28:40 +0000

    Adding more tests with issue on array decoding

--- tests/Nethereum.Contracts.IntegrationTests/Issues/StaticElementArrayTest.cs
+++ tests/Nethereum.Contracts.IntegrationTests/Issues/StaticElementArrayTest.cs
@@ -32,5 +32,28 @@ namespace Nethereum.Contracts.IntegrationTests.Issues
             var data = "0x7ff36ab500000000000000000000000000000000000000000000000000000000000f42400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000251366392987f8915badae03c77933bc72682b9300000000000000000000000000000000000000000000000000000000617951f80000000000000000000000000000000000000000000000000000000000000002000000000000000000000000bb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c0000000000000000000000000bc89aa98ad94e6798ec822d0814d934ccd0c0ceffffffffffffffffffffffffffff";
             var decoded = new SwapExactETHForTokensFunction().DecodeInput(data);
         }
+
+        public partial class UnoswapFunction : UnoswapFunctionBase { }
+
+        [Function("unoswap", "uint256")]
+        public class UnoswapFunctionBase : FunctionMessage
+        {
+            [Parameter("address", "srcToken", 1)]
+            public virtual string SrcToken { get; set; }
+            [Parameter("uint256", "amount", 2)]
+            public virtual BigInteger Amount { get; set; }
+            [Parameter("uint256", "minReturn", 3)]
+            public virtual BigInteger MinReturn { get; set; }
+            [Parameter("bytes32[]", "", 4)]
+            public virtual List<byte[]> ReturnValue4 { get; set; }
+        }
+
+        [Fact]
+        public void ShouldDecode()
+        {
+            var data = "0x2e95b6c8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000366ffbc80d1bd500000000000000000000000000000000000000000000000000db85235d983ed80000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000180000000000000003b6d03407ee2d59972dd251f4212cfb69e0414cb33962650e26b9977";
+            var decoded = new UnoswapFunction().DecodeInput(data.Substring(10));
+        }
+
     }
 }
