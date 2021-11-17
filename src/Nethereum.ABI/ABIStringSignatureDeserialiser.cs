699709306b633872f582dddce4cd30376a59654b
Juan Blanco <juanfranblanco@hotmail.com>
2021-11-17 22:19:05 +0000

    #736 Support Human readable abi signatures, parsers for contract, function, events, tuples, errors and arrays

---
+++ src/Nethereum.ABI/ABIStringSignatureDeserialiser.cs
@@ -0,0 +1,186 @@
+﻿using Nethereum.ABI.Model;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace Nethereum.ABI.UnitTests
+{
+    public class ABIStringSignatureDeserialiser
+    {
+        //https://stackoverflow.com/questions/546433/regular-expression-to-match-balanced-parentheses
+        //balanced parameters to support tuples
+        private const string paramsRegPattern = @"\((?<params>(?>\((?<c>)|[^()]+|\)(?<-c>))*(?(c)(?!)))?\)";
+     
+        public List<Parameter> ExtractParameters(string parameters, bool canIndex = false)
+        {
+
+            var parametersArray = Regex.Split(parameters, @",(?![^(]*\))"); 
+            var returnParameters = new List<Parameter>();
+            var parameterOrder = 0;
+            foreach(var parameter in parametersArray)
+            {
+                parameterOrder = parameterOrder + 1;
+                bool isIndexed = false;
+                string name = null;
+                var nameTypeIndex = Regex.Split(parameter.Trim(),@"\s+(?![^(]*\))");
+                string type = GetType(nameTypeIndex[0]);
+                if (canIndex && nameTypeIndex.Length == 3 && parameter.IndexOf("indexed") > -1)
+                {
+                    name = nameTypeIndex[2];
+                    isIndexed = true;
+                    
+                }
+                else if (nameTypeIndex.Length == 2)
+                {
+                    name = nameTypeIndex[1];
+                    type = GetType(nameTypeIndex[0]);
+                }
+                
+                var returnParameter = new Parameter(type, name, parameterOrder);
+                var tupleType = returnParameter.ABIType as TupleType;
+                if(tupleType != null)
+                {
+                    var match = Regex.Match(parameter, paramsRegPattern);
+                    tupleType.SetComponents(ExtractParameters(match.Groups["params"].Value).ToArray());
+                }
+
+                var arrayType = returnParameter.ABIType as ArrayType;
+
+                while (arrayType != null)
+                {
+
+                    if (arrayType.ElementType is TupleType arrayTupleType)
+                    {
+                        var match = Regex.Match(parameter, paramsRegPattern);
+                        arrayTupleType.SetComponents(ExtractParameters(match.Groups["params"].Value).ToArray());
+                        arrayType = null;
+                    }
+                    else
+                    {
+                        arrayType = arrayType.ElementType as ArrayType;
+                    }
+                }
+
+                returnParameter.Indexed = isIndexed;
+                returnParameters.Add(returnParameter);
+
+                
+            }
+            return returnParameters;
+        }
+
+        private string GetType(string type)
+        {
+            type = type.Trim();
+            if (type.StartsWith("tuple"))
+            {
+                if (type.EndsWith("]"))
+                {
+                    var index = type.LastIndexOf("[");
+                    return "tuple" + type.Substring(index, type.Length - index);
+                }
+                else
+                {
+                    return "tuple";
+                }
+            }
+            return type;
+        }
+
+        public ErrorABI ExtractErrorABI(string signature, string name, string parameters)
+        {
+            var errorABI = new ErrorABI(name);
+            if (!string.IsNullOrEmpty(parameters))
+            {
+                errorABI.InputParameters = ExtractParameters(parameters).ToArray();
+            }
+            return errorABI;
+        }
+
+        public EventABI ExtractEventABI(string signature, string name, string parameters)
+        {
+            var eventAbi = new EventABI(name, false);
+            if (!string.IsNullOrEmpty(parameters))
+            {
+                eventAbi.InputParameters = ExtractParameters(parameters, true).ToArray();
+            }
+            return eventAbi;
+        }
+
+        public ConstructorABI ExtractConstructorABI(string signature, string name, string parameters)
+        {
+            var constructor = new ConstructorABI();
+            if (!string.IsNullOrEmpty(parameters))
+            {
+                constructor.InputParameters = ExtractParameters(parameters).ToArray();
+            }
+            return constructor;
+        }
+
+        public FunctionABI ExtractFunctionABI(string signature, string name, string parameters)
+        {
+            var matchReturns = Regex.Match(signature, @"returns\s*" + paramsRegPattern);
+            var parameterReturns = matchReturns.Groups["params"].Value;
+            var matchModifier = Regex.Match(signature, @"\)\s*\w*\s+(?<modifier>view|pure|constant)");
+            var modifier = matchModifier.Groups["modifier"].Value;
+            var function = new FunctionABI(name, !string.IsNullOrEmpty(modifier));
+            if (!string.IsNullOrEmpty(parameterReturns))
+            {
+                function.OutputParameters = ExtractParameters(parameterReturns).ToArray();
+            }
+
+            if (!string.IsNullOrEmpty(parameters))
+            {
+                function.InputParameters = ExtractParameters(parameters).ToArray();
+            }
+            
+            return function;
+        }
+
+       
+        public ContractABI ExtractContractABI(params string[] signatures)
+        {
+            var contractABI = new ContractABI();
+            var functions = new List<FunctionABI>();
+            var events = new List<EventABI>();
+            var errors = new List<ErrorABI>();
+            
+            foreach(var signature in signatures)
+            {
+                
+                var match = Regex.Match(signature, @"(?<scope>\w*)\s*(?<name>\w+)\s*" + paramsRegPattern);
+                var scope = match.Groups["scope"].Value;
+                var name = match.Groups["name"].Value;
+                var parameters = match.Groups["params"].Value;
+ 
+                if (scope == "function")
+                {
+                    functions.Add(ExtractFunctionABI(signature, name, parameters));
+                }
+
+                if (scope == "event")
+                {
+                    events.Add(ExtractEventABI(signature, name, parameters));
+                }
+
+                if (scope == "error")
+                {
+                    errors.Add(ExtractErrorABI(signature, name, parameters));
+                }
+
+                if (name == "constructor")
+                {
+                    contractABI.Constructor = ExtractConstructorABI(signature, name, parameters);
+                }
+            }
+
+            contractABI.Functions = functions.ToArray();
+            contractABI.Events = events.ToArray();
+            contractABI.Errors = errors.ToArray();
+            return contractABI;
+        }
+
+       
+    }
+}
---
+++ tests/Nethereum.ABI.UnitTests/ABIStringSignatureDeserialiserTests.cs
@@ -0,0 +1,64 @@
+﻿using Nethereum.ABI.Model;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Nethereum.ABI.UnitTests
+{
+    public class ABIStringSignatureDeserialiserTests
+    {
+        [Fact]
+        public void ShouldExtractSignatures()
+        {
+            //string fullSignature = "function addPerson(tuple(string name, uint16 age) person)";
+            //string fullSignature = "function addPeople(tuple(string name, uint16 age)[] person)";
+            //string fullSignature = @" function balanceOf ( address owner, address thing ) public view returns (uint)
+            //                     function setCompleted(uint completed) public restricted 
+            //                     constructor(address erc20)
+            //                     event FeePercentUpdated(uint256 indexed newFeePercent)
+            //                     event HarvestDelayUpdated(uint64 newHarvestDelay)";
+            var contractAbi = new ABIStringSignatureDeserialiser().ExtractContractABI(fullSignature);
+        }
+
+
+        [Fact]
+        public void ShouldExtractFunctionWithTuple()
+        {
+            string signature = "function getTuplePerson(uint id) view returns (tuple(string name, string surname, uint16 age))";
+            var contractAbi = new ABIStringSignatureDeserialiser().ExtractContractABI(signature);
+            var function = contractAbi.Functions[0];
+            Assert.Equal("getTuplePerson", function.Name);
+            Assert.Equal("id", function.InputParameters[0].Name);
+            Assert.Equal("uint", function.InputParameters[0].Type);
+            Assert.Equal("tuple", function.OutputParameters[0].Type);
+            
+            var tupleType = function.OutputParameters[0].ABIType as TupleType;
+            Assert.Equal("name", tupleType.Components[0].Name);
+            Assert.Equal("string", tupleType.Components[0].Type);
+            Assert.Equal("surname", tupleType.Components[1].Name);
+            Assert.Equal("string", tupleType.Components[1].Type);
+            Assert.Equal("age", tupleType.Components[2].Name);
+            Assert.Equal("uint16", tupleType.Components[2].Type);
+            
+        }
+
+
+        [Fact]
+        public void ShouldExtractFunction()
+        {
+            string signature = " function balanceOf ( address owner, address thing ) public view returns (uint)";
+            var contractAbi = new ABIStringSignatureDeserialiser().ExtractContractABI(signature);
+            var function = contractAbi.Functions[0];
+            Assert.Equal("balanceOf", function.Name);
+            Assert.True(function.Constant);
+            Assert.Equal("owner", function.InputParameters[0].Name);
+            Assert.Equal("address", function.InputParameters[0].Type);
+            Assert.Equal("thing", function.InputParameters[1].Name);
+            Assert.Equal("address", function.InputParameters[1].Type);
+            Assert.Equal("uint", function.OutputParameters[0].Type);
+
+        }
+
+    }
+}
