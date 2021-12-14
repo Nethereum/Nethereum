bc94103cce7e4208c458bcf8638615d9b081856d
Juan Blanco <juanfranblanco@hotmail.com>
2021-12-14 18:17:28 +0000

    Moving gnosis safe tests

--- tests/Nethereum.GnosisSafe.IntegrationTests/GnosisSafeTests.cs
+++ tests/Nethereum.GnosisSafe.IntegrationTests/GnosisSafeTests.cs
@@ -30,44 +30,7 @@ namespace Nethereum.GnosisSafe.Contracts.Testing
             _ethereumClientIntegrationFixture = ethereumClientIntegrationFixture;
         }
 
-        [Fact]
-        public async void ShouldBeAbleToEncodeTheSameAsTheSmartContract()
-        {
-            var web3 = _ethereumClientIntegrationFixture.GetInfuraWeb3(InfuraNetwork.Rinkeby);
-            var signer = new Eip712TypedDataSigner();
-            var gnosisSafeAddress = "0xa9C09412C1d93DAc6eE9254A51E97454588D3B88";
-            var chainId = (int)Chain.Rinkeby;
-            var service = new GnosisSafeService(web3,  gnosisSafeAddress);
-            var param = new EncodeTransactionDataFunction
-            {
-                To = "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D",
-                Value = 0,
-                Data = "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D".HexToByteArray(),
-                Operation = (byte)ContractOperationType.Call,
-                SafeTxGas = 0,
-                BaseGas = 0,
-                GasPrice = 0,
-                GasToken = AddressUtil.AddressEmptyAsHex,
-                RefundReceiver = AddressUtil.AddressEmptyAsHex,
-                Nonce = 1
-            };
-            var encoded = await service.EncodeTransactionDataQueryAsync(param);
-
-            var domain = new Domain
-            {
-                VerifyingContract = gnosisSafeAddress,
-                ChainId = chainId
-            };
-
-            var encodedMessage = signer.EncodeTypedData(param, domain, "SafeTx");
-            Assert.Equal(encoded.ToHex(), encodedMessage.ToHex());
-
-        }
-
-   
-
-      
-
+        
         //[Fact] //replace private keys to avoid issues
         public async void ShouldDoMultiTransferTokensUsingARelayer()
         {
---
+++ tests/Nethereum.GnosisSafe.IntegrationTests/SafeFunctionalTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nethereum.GnosisSafe.IntegrationTests
+{
+    internal class SafeFunctionalTests
+    {
+    }
+}
