5d060b1ed81284a536997feee782c8556722701b
Juan Blanco <juanfranblanco@hotmail.com>
2021-12-20 11:05:26 +0000

    Siwe adding more tests

---
+++ src/Nethereum.Siwe.Core/SiweMessageStringBuilder.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Nethereum.Util;
+
+namespace Nethereum.Siwe.Core;
+
+public class SiweMessageStringBuilder
+{
+    public const string DOMAIN = "{0} wants you to sign in with your Ethereum account:";
+    public const string ADDRESS = "\n{0}\n\n";
+    public const string STATEMENT = "{0}\n";
+    public const string URI_LINE = "\nURI: {0}";
+    public const string VERSION = "\nVersion: {0}";
+    public const string CHAIN_ID = "\nChain ID: {0}";
+    public const string NONCE = "\nNonce: {0}";
+    public const string ISSUED_AT = "\nIssued At: {0}";
+    public const string EXPIRATION_TIME = "\nExpiration Time: {0}";
+    public const string NOT_BEFORE = "\nNot Before: {0}";
+    public const string REQUEST_ID = "\nRequest ID: {0}";
+    public const string RESOURCES = "\nResources:";
+    public const string RESOURCE = "\n- {0}";
+
+    private static string GetDomain(SiweMessage message)
+    {
+        if (string.IsNullOrEmpty(message.Domain)) throw new ArgumentException("Domain cannot be null or empty");
+        return string.Format(DOMAIN, message.Domain);
+    }
+
+    private static string GetAddress(SiweMessage message)
+    {
+        //should this be a checksum address?
+        //if (message.Address.IsEthereumChecksumAddress())
+        if(message.Address.IsValidEthereumAddressHexFormat())
+        {
+            return string.Format(ADDRESS, message.Address);
+        }
+
+        throw new FormatException("Invalid address format");
+    }
+
+    private static string GetStatement(SiweMessage message)
+    {
+        if (!string.IsNullOrEmpty(message.Statement))
+        {
+            return string.Format(STATEMENT, message.Statement);
+        }
+            
+        return string.Empty;
+    }
+
+    private static string GetUriLine(SiweMessage message)
+    {
+        return string.Format(URI_LINE, message.Uri);
+    }
+
+    private static string GetVersion(SiweMessage message)
+    {
+        if (string.IsNullOrEmpty(message.Version)) throw new ArgumentException("Version cannot be null or empty");
+        return string.Format(VERSION, message.Version);
+    }
+
+    private static string GetChainId(SiweMessage message)
+    {
+        if (string.IsNullOrEmpty(message.ChainId)) throw new ArgumentException("ChainId cannot be null or empty");
+        return string.Format(CHAIN_ID, message.ChainId);
+    }
+
+    private static string GetNonce(SiweMessage message)
+    {
+        if (string.IsNullOrEmpty(message.Nonce)) throw new ArgumentException("Nonce cannot be null or empty");
+        if (message.Nonce.Length < 8) throw new ArgumentException("Nonce has to be bigger or equal to 8 characters");
+        return string.Format(NONCE, message.Nonce);
+    }
+
+    private static string GetIssuedAt(SiweMessage message)
+    {
+        if (string.IsNullOrEmpty(message.IssuedAt)) throw new ArgumentException("IssuedAt cannot be null or empty");
+        return string.Format(ISSUED_AT, message.IssuedAt);
+    }
+
+    private static string GetExpirationTime(SiweMessage message)
+    {
+        if (!string.IsNullOrEmpty(message.ExpirationTime))
+        {
+            return string.Format(EXPIRATION_TIME, message.ExpirationTime);
+        }
+
+        return string.Empty;
+    }
+
+    private static string GetNotBefore(SiweMessage message)
+    {
+        if (!string.IsNullOrEmpty(message.NotBefore))
+        {
+            return string.Format(NOT_BEFORE, message.NotBefore);
+        }
+
+        return string.Empty;
+    }
+
+
+    private static string GetRequestId(SiweMessage message)
+    {
+        if (!string.IsNullOrEmpty(message.RequestId))
+        {
+            return string.Format(REQUEST_ID, message.RequestId);
+        }
+
+        return string.Empty;
+    }
+
+    private static string GetResources(SiweMessage message)
+    {
+        if (message.Resources != null && message.Resources.Count > 0)
+        {
+            var returnString = RESOURCES;
+            foreach (var resource in message.Resources)
+            {
+                returnString += string.Format(RESOURCE, resource);
+            }
+
+            return returnString;
+        }
+
+        return string.Empty;
+    }
+
+    public static string BuildMessage(SiweMessage message)
+    {
+        return GetDomain(message) +
+               GetAddress(message) +
+               GetStatement(message) +
+               GetUriLine(message) +
+               GetVersion(message) +
+               GetChainId(message) +
+               GetNonce(message) +
+               GetIssuedAt(message) +
+               GetExpirationTime(message) +
+               GetNotBefore(message) +
+               GetRequestId(message) +
+               GetResources(message);
+
+    }
+}
---
+++ tests/Nethereum.Siwe.UnitTests/ParserBuilderTests.cs
@@ -0,0 +1,323 @@
+﻿using System.Collections.Generic;
+using Nethereum.Siwe.Core;
+using Xunit;
+
+namespace Nethereum.Siwe.UnitTests
+{
+    public class ParserBuilderTests
+    {
+        [Fact]
+        public void ShouldParseAndBuildWith2OptionalFields()
+        {
+            var message = "service.org wants you to sign in with your Ethereum account:\n0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2\n\nI accept the ServiceOrg Terms of Service: https://service.org/tos\n\nURI: https://service.org/login\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z\nResources:\n- ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu\n- https://example.com/my-web2-claim.json";
+            var domain = "service.org";
+            var address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            var statement = "I accept the ServiceOrg Terms of Service: https://service.org/tos";
+            var uri = "https://service.org/login";
+            var version = "1";
+            var chainId = "1";
+            var nonce = "32891757";
+            var issuedAt = "2021-09-30T16:25:24.000Z";
+            var resource1 = "ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu";
+            var resource2 = "https://example.com/my-web2-claim.json";
+
+
+            var decodedMessage = SiweMessageParser.Parse(message);
+            
+            Assert.Equal(domain, decodedMessage.Domain);
+            Assert.Equal(address, decodedMessage.Address);
+            Assert.Equal(statement, decodedMessage.Statement);
+            Assert.Equal(uri, decodedMessage.Uri);
+            Assert.Equal(version, decodedMessage.Version);
+            Assert.Equal(chainId, decodedMessage.ChainId);
+            Assert.Equal(nonce, decodedMessage.Nonce);
+            Assert.Equal(issuedAt, decodedMessage.IssuedAt);
+            Assert.Equal(resource1, decodedMessage.Resources[0]);
+            Assert.Equal(resource2, decodedMessage.Resources[1]);
+
+            var decodedMessage2 = SiweMessageParser.ParseUsingAbnf(message);
+
+            Assert.Equal(domain, decodedMessage2.Domain);
+            Assert.Equal(address, decodedMessage2.Address);
+            Assert.Equal(statement, decodedMessage2.Statement);
+            Assert.Equal(uri, decodedMessage2.Uri);
+            Assert.Equal(version, decodedMessage2.Version);
+            Assert.Equal(chainId, decodedMessage2.ChainId);
+            Assert.Equal(nonce, decodedMessage2.Nonce);
+            Assert.Equal(issuedAt, decodedMessage2.IssuedAt);
+            Assert.Equal(resource1, decodedMessage2.Resources[0]);
+            Assert.Equal(resource2, decodedMessage2.Resources[1]);
+
+            var builtMessage = SiweMessageStringBuilder.BuildMessage(decodedMessage2);
+            Assert.Equal(message, builtMessage);
+        }
+
+
+        [Fact]
+        public void ShouldParseAndBuildWithNoOptionalFields()
+        {
+            var message = "service.org wants you to sign in with your Ethereum account:\n0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2\n\nI accept the ServiceOrg Terms of Service: https://service.org/tos\n\nURI: https://service.org/login\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z";
+            var domain = "service.org";
+            var address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            var statement = "I accept the ServiceOrg Terms of Service: https://service.org/tos";
+            var uri = "https://service.org/login";
+            var version = "1";
+            var chainId = "1";
+            var nonce = "32891757";
+            var issuedAt = "2021-09-30T16:25:24.000Z";
+
+
+            var decodedMessage = SiweMessageParser.Parse(message);
+
+            Assert.Equal(domain, decodedMessage.Domain);
+            Assert.Equal(address, decodedMessage.Address);
+            Assert.Equal(statement, decodedMessage.Statement);
+            Assert.Equal(uri, decodedMessage.Uri);
+            Assert.Equal(version, decodedMessage.Version);
+            Assert.Equal(chainId, decodedMessage.ChainId);
+            Assert.Equal(nonce, decodedMessage.Nonce);
+            Assert.Equal(issuedAt, decodedMessage.IssuedAt);
+
+
+            var decodedMessage2 = SiweMessageParser.ParseUsingAbnf(message);
+
+            Assert.Equal(domain, decodedMessage2.Domain);
+            Assert.Equal(address, decodedMessage2.Address);
+            Assert.Equal(statement, decodedMessage2.Statement);
+            Assert.Equal(uri, decodedMessage2.Uri);
+            Assert.Equal(version, decodedMessage2.Version);
+            Assert.Equal(chainId, decodedMessage2.ChainId);
+            Assert.Equal(nonce, decodedMessage2.Nonce);
+            Assert.Equal(issuedAt, decodedMessage2.IssuedAt);
+ 
+
+            var builtMessage = SiweMessageStringBuilder.BuildMessage(decodedMessage2);
+            Assert.Equal(message, builtMessage);
+        }
+
+
+        [Fact]
+        public void ShouldParseAndBuildTimestampWithoutMicroseconds()
+        {
+            var message = "service.org wants you to sign in with your Ethereum account:\n0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2\n\nI accept the ServiceOrg Terms of Service: https://service.org/tos\n\nURI: https://service.org/login\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24Z";
+            var domain = "service.org";
+            var address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            var statement = "I accept the ServiceOrg Terms of Service: https://service.org/tos";
+            var uri = "https://service.org/login";
+            var version = "1";
+            var chainId = "1";
+            var nonce = "32891757";
+            var issuedAt = "2021-09-30T16:25:24Z";
+
+
+            var decodedMessage = SiweMessageParser.Parse(message);
+
+            Assert.Equal(domain, decodedMessage.Domain);
+            Assert.Equal(address, decodedMessage.Address);
+            Assert.Equal(statement, decodedMessage.Statement);
+            Assert.Equal(uri, decodedMessage.Uri);
+            Assert.Equal(version, decodedMessage.Version);
+            Assert.Equal(chainId, decodedMessage.ChainId);
+            Assert.Equal(nonce, decodedMessage.Nonce);
+            Assert.Equal(issuedAt, decodedMessage.IssuedAt);
+
+
+            var decodedMessage2 = SiweMessageParser.ParseUsingAbnf(message);
+
+            Assert.Equal(domain, decodedMessage2.Domain);
+            Assert.Equal(address, decodedMessage2.Address);
+            Assert.Equal(statement, decodedMessage2.Statement);
+            Assert.Equal(uri, decodedMessage2.Uri);
+            Assert.Equal(version, decodedMessage2.Version);
+            Assert.Equal(chainId, decodedMessage2.ChainId);
+            Assert.Equal(nonce, decodedMessage2.Nonce);
+            Assert.Equal(issuedAt, decodedMessage2.IssuedAt);
+
+
+            var builtMessage = SiweMessageStringBuilder.BuildMessage(decodedMessage2);
+            Assert.Equal(message, builtMessage);
+        }
+
+        [Fact]
+        public void ShouldParseAndBuildWithDomainRFC3986AndUserInfo()
+        {
+            var message = "test@127.0.0.1 wants you to sign in with your Ethereum account:\n0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2\n\nI accept the ServiceOrg Terms of Service: https://service.org/tos\n\nURI: https://service.org/login\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z";
+            var domain = "test@127.0.0.1";
+            var address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            var statement = "I accept the ServiceOrg Terms of Service: https://service.org/tos";
+            var uri = "https://service.org/login";
+            var version = "1";
+            var chainId = "1";
+            var nonce = "32891757";
+            var issuedAt = "2021-09-30T16:25:24.000Z";
+
+
+            var decodedMessage = SiweMessageParser.Parse(message);
+
+            Assert.Equal(domain, decodedMessage.Domain);
+            Assert.Equal(address, decodedMessage.Address);
+            Assert.Equal(statement, decodedMessage.Statement);
+            Assert.Equal(uri, decodedMessage.Uri);
+            Assert.Equal(version, decodedMessage.Version);
+            Assert.Equal(chainId, decodedMessage.ChainId);
+            Assert.Equal(nonce, decodedMessage.Nonce);
+            Assert.Equal(issuedAt, decodedMessage.IssuedAt);
+
+
+            var decodedMessage2 = SiweMessageParser.ParseUsingAbnf(message);
+
+            Assert.Equal(domain, decodedMessage2.Domain);
+            Assert.Equal(address, decodedMessage2.Address);
+            Assert.Equal(statement, decodedMessage2.Statement);
+            Assert.Equal(uri, decodedMessage2.Uri);
+            Assert.Equal(version, decodedMessage2.Version);
+            Assert.Equal(chainId, decodedMessage2.ChainId);
+            Assert.Equal(nonce, decodedMessage2.Nonce);
+            Assert.Equal(issuedAt, decodedMessage2.IssuedAt);
+
+
+            var builtMessage = SiweMessageStringBuilder.BuildMessage(decodedMessage2);
+            Assert.Equal(message, builtMessage);
+        }
+
+        [Fact]
+        public void ShouldParseAndBuildWithDomainRFC3986AndPort()
+        {
+            var message = "127.0.0.1:8080 wants you to sign in with your Ethereum account:\n0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2\n\nI accept the ServiceOrg Terms of Service: https://service.org/tos\n\nURI: https://service.org/login\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z";
+            var domain = "127.0.0.1:8080";
+            var address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            var statement = "I accept the ServiceOrg Terms of Service: https://service.org/tos";
+            var uri = "https://service.org/login";
+            var version = "1";
+            var chainId = "1";
+            var nonce = "32891757";
+            var issuedAt = "2021-09-30T16:25:24.000Z";
+
+
+            var decodedMessage = SiweMessageParser.Parse(message);
+
+            Assert.Equal(domain, decodedMessage.Domain);
+            Assert.Equal(address, decodedMessage.Address);
+            Assert.Equal(statement, decodedMessage.Statement);
+            Assert.Equal(uri, decodedMessage.Uri);
+            Assert.Equal(version, decodedMessage.Version);
+            Assert.Equal(chainId, decodedMessage.ChainId);
+            Assert.Equal(nonce, decodedMessage.Nonce);
+            Assert.Equal(issuedAt, decodedMessage.IssuedAt);
+
+
+            var decodedMessage2 = SiweMessageParser.ParseUsingAbnf(message);
+
+            Assert.Equal(domain, decodedMessage2.Domain);
+            Assert.Equal(address, decodedMessage2.Address);
+            Assert.Equal(statement, decodedMessage2.Statement);
+            Assert.Equal(uri, decodedMessage2.Uri);
+            Assert.Equal(version, decodedMessage2.Version);
+            Assert.Equal(chainId, decodedMessage2.ChainId);
+            Assert.Equal(nonce, decodedMessage2.Nonce);
+            Assert.Equal(issuedAt, decodedMessage2.IssuedAt);
+
+
+            var builtMessage = SiweMessageStringBuilder.BuildMessage(decodedMessage2);
+            Assert.Equal(message, builtMessage);
+        }
+
+        [Fact]
+        public void ShouldParseAndBuildWithDomainRFC3986WithUserInfoAndPort()
+        {
+            var message = "test@127.0.0.1:8080 wants you to sign in with your Ethereum account:\n0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2\n\nI accept the ServiceOrg Terms of Service: https://service.org/tos\n\nURI: https://service.org/login\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z";
+            var domain = "test@127.0.0.1:8080";
+            var address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            var statement = "I accept the ServiceOrg Terms of Service: https://service.org/tos";
+            var uri = "https://service.org/login";
+            var version = "1";
+            var chainId = "1";
+            var nonce = "32891757";
+            var issuedAt = "2021-09-30T16:25:24.000Z";
+
+
+            var decodedMessage = SiweMessageParser.Parse(message);
+
+            Assert.Equal(domain, decodedMessage.Domain);
+            Assert.Equal(address, decodedMessage.Address);
+            Assert.Equal(statement, decodedMessage.Statement);
+            Assert.Equal(uri, decodedMessage.Uri);
+            Assert.Equal(version, decodedMessage.Version);
+            Assert.Equal(chainId, decodedMessage.ChainId);
+            Assert.Equal(nonce, decodedMessage.Nonce);
+            Assert.Equal(issuedAt, decodedMessage.IssuedAt);
+
+
+            var decodedMessage2 = SiweMessageParser.ParseUsingAbnf(message);
+
+            Assert.Equal(domain, decodedMessage2.Domain);
+            Assert.Equal(address, decodedMessage2.Address);
+            Assert.Equal(statement, decodedMessage2.Statement);
+            Assert.Equal(uri, decodedMessage2.Uri);
+            Assert.Equal(version, decodedMessage2.Version);
+            Assert.Equal(chainId, decodedMessage2.ChainId);
+            Assert.Equal(nonce, decodedMessage2.Nonce);
+            Assert.Equal(issuedAt, decodedMessage2.IssuedAt);
+
+
+            var builtMessage = SiweMessageStringBuilder.BuildMessage(decodedMessage2);
+            Assert.Equal(message, builtMessage);
+        }
+
+        [Fact]
+        public void ShouldParseAndBuildWithAllOptionalFields()
+        {
+            var message = "test@127.0.0.1:8080 wants you to sign in with your Ethereum account:\n0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2\n\nI accept the ServiceOrg Terms of Service: https://service.org/tos\n\nURI: https://service.org/login\nVersion: 1\nChain ID: 1\nNonce: 32891757\nIssued At: 2021-09-30T16:25:24.000Z\nExpiration Time: 2021-09-30T16:25:24.000Z\nNot Before: 2021-09-30T16:25:24.000Z\nRequest ID: 200\nResources:\n- ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu\n- https://example.com/my-web2-claim.json";
+            var domain = "test@127.0.0.1:8080";
+            var address = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+            var statement = "I accept the ServiceOrg Terms of Service: https://service.org/tos";
+            var uri = "https://service.org/login";
+            var version = "1";
+            var chainId = "1";
+            var nonce = "32891757";
+            var issuedAt = "2021-09-30T16:25:24.000Z";
+            var expirationTime = "2021-09-30T16:25:24.000Z";
+            var notBefore = "2021-09-30T16:25:24.000Z";
+            var requestId = "200";
+            var resource1 = "ipfs://Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu";
+            var resource2 = "https://example.com/my-web2-claim.json";
+
+            var decodedMessage = SiweMessageParser.Parse(message);
+            Assert.Equal(domain, decodedMessage.Domain);
+            Assert.Equal(address, decodedMessage.Address);
+            Assert.Equal(statement, decodedMessage.Statement);
+            Assert.Equal(uri, decodedMessage.Uri);
+            Assert.Equal(version, decodedMessage.Version);
+            Assert.Equal(chainId, decodedMessage.ChainId);
+            Assert.Equal(nonce, decodedMessage.Nonce);
+            Assert.Equal(issuedAt, decodedMessage.IssuedAt);
+            Assert.Equal(expirationTime, decodedMessage.ExpirationTime);
+            Assert.Equal(notBefore, decodedMessage.NotBefore);
+            Assert.Equal(requestId, decodedMessage.RequestId);
+
+            Assert.Equal(resource1, decodedMessage.Resources[0]);
+            Assert.Equal(resource2, decodedMessage.Resources[1]);
+
+            var decodedMessage2 = SiweMessageParser.ParseUsingAbnf(message);
+
+            Assert.Equal(domain, decodedMessage2.Domain);
+            Assert.Equal(address, decodedMessage2.Address);
+            Assert.Equal(statement, decodedMessage2.Statement);
+            Assert.Equal(uri, decodedMessage2.Uri);
+            Assert.Equal(version, decodedMessage2.Version);
+            Assert.Equal(chainId, decodedMessage2.ChainId);
+            Assert.Equal(nonce, decodedMessage2.Nonce);
+            Assert.Equal(issuedAt, decodedMessage2.IssuedAt);
+            Assert.Equal(expirationTime, decodedMessage2.ExpirationTime);
+            Assert.Equal(notBefore, decodedMessage2.NotBefore);
+            Assert.Equal(requestId, decodedMessage2.RequestId);
+            Assert.Equal(resource1, decodedMessage2.Resources[0]);
+            Assert.Equal(resource2, decodedMessage2.Resources[1]);
+
+
+            var builtMessage = SiweMessageStringBuilder.BuildMessage(decodedMessage2);
+            Assert.Equal(message, builtMessage);
+        }
+
+    }
+}
