2b37fbcab219e41e1442a2b14adc177c78588e9d
Juan Blanco <juanfranblanco@hotmail.com>
2022-04-08 15:23:19 +0100

    Adding token list to standard contracts

---
+++ src/Nethereum.Contracts/Standards/ERC20/TokenList/TokenListService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+#if NETSTANDARD1_0_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+#endif
+using Newtonsoft.Json;
+
+namespace Nethereum.Contracts.Standards.ERC20
+{
+    /// <summary>
+    /// Helper service to get tokens from https://tokenlists.org/
+    /// </summary>
+    public class TokenListService
+    {
+        public List<Token> DeserialiseFromJson(string json)
+        {
+            var root = JsonConvert.DeserializeObject<Root>(json);
+            return root.Tokens;
+        }
+
+#if NETSTANDARD1_0_OR_GREATER || NETCOREAPP2_1_OR_GREATER
+        public async  Task<List<Token>> LoadFromUrl(string url)
+        {
+            var client = new HttpClient();
+            var json = await client.GetStringAsync(url);
+            return DeserialiseFromJson(json);
+        }
+#endif
+    }
+}
