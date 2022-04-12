8345dfbca5ce51ddd9e235a9fbaf7bdd640ac0c8
Juan Blanco <juanfranblanco@hotmail.com>
2022-04-12 15:56:07 +0100

    ERC20 tests using Contracts service

--- tests/Nethereum.Contracts.IntegrationTests/SmartContracts/Standards/ERC20Tests.cs
+++ tests/Nethereum.Contracts.IntegrationTests/SmartContracts/Standards/ERC20Tests.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Numerics;
 using System.Threading.Tasks;
+using Nethereum.ABI.Decoders;
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts.IntegrationTests.FiltersEvents;
 using Nethereum.Contracts.Standards.ERC20;
+using Nethereum.Contracts.Standards.ERC20.ContractDefinition;
 using Nethereum.Contracts.Standards.ERC20.TokenList;
 using Nethereum.RPC.Eth.DTOs;
+using Nethereum.RPC.Eth.Services;
 using Nethereum.XUnitEthereumClients;
 using Newtonsoft.Json;
 using Xunit;
@@ -21,6 +27,125 @@ namespace Nethereum.Contracts.IntegrationTests.SmartContracts.Standards
         public ERC20Tests(EthereumClientIntegrationFixture ethereumClientIntegrationFixture)
         {
             _ethereumClientIntegrationFixture = ethereumClientIntegrationFixture;
+        }
+
+        [Fact]
+        public async void ShouldGetTheDaiFromMainnet()
+        {
+            var web3 = _ethereumClientIntegrationFixture.GetInfuraWeb3(InfuraNetwork.Mainnet);
+            var contractHandler = web3.Eth.GetContractHandler("0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359");
+            var stringBytes32Decoder = new StringBytes32Decoder();
+            var symbol = await contractHandler.QueryRawAsync<SymbolFunction, StringBytes32Decoder, string>();
+            var token = await contractHandler.QueryRawAsync<NameFunction, StringBytes32Decoder, string>();
+
+        }
+
+        [Fact]
+        public async void ShouldReturnData()
+        {
+            var web3 = _ethereumClientIntegrationFixture.GetWeb3();
+            var deploymentHandler = web3.Eth.GetContractDeploymentHandler<EIP20Deployment>();
+            var receipt = await deploymentHandler.SendRequestAndWaitForReceiptAsync(new EIP20Deployment()
+            {
+                DecimalUnits = 18,
+                InitialAmount = BigInteger.Parse("10000000000000000000000000"),
+                TokenSymbol = "XST",
+                TokenName = "XomeStandardToken"
+            });
+
+            var contractHandler = web3.Eth.GetContractHandler(receipt.ContractAddress);
+            var symbol = await contractHandler.QueryRawAsync<SymbolFunction, StringBytes32Decoder, string>();
+            var token = await contractHandler.QueryRawAsync<NameFunction, StringBytes32Decoder, string>();
+
+            Assert.Equal("XST", symbol);
+            Assert.Equal("XomeStandardToken", token);
+        }
+
+        [Fact]
+        public async void Test()
+        {
+            var addressOwner = EthereumClientIntegrationFixture.AccountAddress;
+            var web3 = _ethereumClientIntegrationFixture.GetWeb3();
+
+            ulong totalSupply = 1000000;
+            var newAddress = "0x12890d2cce102216644c59daE5baed380d84830e";
+
+            var deploymentContract = new EIP20Deployment()
+            {
+                InitialAmount = totalSupply,
+                TokenName = "TestToken",
+                TokenSymbol = "TST"
+            };
+
+            var deploymentHandler = web3.Eth.GetContractDeploymentHandler<EIP20Deployment>();
+            var receipt = await deploymentHandler.SendRequestAndWaitForReceiptAsync(deploymentContract);
+            var tokenService = web3.Eth.ERC20.GetERC20ContractService(receipt.ContractAddress);
+            
+            var transfersEvent = tokenService.GetTransferEvent();
+
+            var totalSupplyDeployed = await tokenService.TotalSupplyQueryAsync();
+            Assert.Equal(totalSupply, totalSupplyDeployed);
+
+            var tokenName = await tokenService.NameQueryAsync();
+            Assert.Equal("TestToken", tokenName);
+
+            var tokenSymbol = await tokenService.SymbolQueryAsync();
+            Assert.Equal("TST", tokenSymbol);
+
+            var ownerBalance = await tokenService.BalanceOfQueryAsync(addressOwner);
+            Assert.Equal(totalSupply, ownerBalance);
+
+            var transferReceipt =
+                await tokenService.TransferRequestAndWaitForReceiptAsync(newAddress, 1000);
+
+            ownerBalance = await tokenService.BalanceOfQueryAsync(addressOwner);
+            Assert.Equal(totalSupply - 1000, ownerBalance);
+
+            var newAddressBalance = await tokenService.BalanceOfQueryAsync(newAddress);
+            Assert.Equal(1000, newAddressBalance);
+
+            var allTransfersFilter =
+                await transfersEvent.CreateFilterAsync(new BlockParameter(transferReceipt.BlockNumber));
+            var eventLogsAll = await transfersEvent.GetAllChangesAsync(allTransfersFilter);
+            Assert.Single(eventLogsAll);
+            var transferLog = eventLogsAll.First();
+            Assert.Equal(transferLog.Log.TransactionIndex.HexValue, transferReceipt.TransactionIndex.HexValue);
+            Assert.Equal(transferLog.Log.BlockNumber.HexValue, transferReceipt.BlockNumber.HexValue);
+            Assert.Equal(transferLog.Event.To.ToLower(), newAddress.ToLower());
+            Assert.Equal(transferLog.Event.Value, (ulong)1000);
+
+            var approveTransactionReceipt = await tokenService.ApproveRequestAndWaitForReceiptAsync(newAddress, 1000);
+            var allowanceAmount = await tokenService.AllowanceQueryAsync(addressOwner, newAddress);
+            Assert.Equal(1000, allowanceAmount);
+        }
+
+        public class EIP20Deployment : ContractDeploymentMessage
+        {
+#if !BYTECODELITE
+            public static string BYTECODE =
+                "608060405234801561001057600080fd5b506040516107843803806107848339810160409081528151602080840151838501516060860151336000908152808552959095208490556002849055908501805193959094919391019161006991600391860190610096565b506004805460ff191660ff8416179055805161008c906005906020840190610096565b5050505050610131565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f106100d757805160ff1916838001178555610104565b82800160010185558215610104579182015b828111156101045782518255916020019190600101906100e9565b50610110929150610114565b5090565b61012e91905b80821115610110576000815560010161011a565b90565b610644806101406000396000f3006080604052600436106100ae5763ffffffff7c010000000000000000000000000000000000000000000000000000000060003504166306fdde0381146100b3578063095ea7b31461013d57806318160ddd1461017557806323b872dd1461019c57806327e235e3146101c6578063313ce567146101e75780635c6581651461021257806370a082311461023957806395d89b411461025a578063a9059cbb1461026f578063dd62ed3e14610293575b600080fd5b3480156100bf57600080fd5b506100c86102ba565b6040805160208082528351818301528351919283929083019185019080838360005b838110156101025781810151838201526020016100ea565b50505050905090810190601f16801561012f5780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b34801561014957600080fd5b50610161600160a060020a0360043516602435610348565b604080519115158252519081900360200190f35b34801561018157600080fd5b5061018a6103ae565b60408051918252519081900360200190f35b3480156101a857600080fd5b50610161600160a060020a03600435811690602435166044356103b4565b3480156101d257600080fd5b5061018a600160a060020a03600435166104b7565b3480156101f357600080fd5b506101fc6104c9565b6040805160ff9092168252519081900360200190f35b34801561021e57600080fd5b5061018a600160a060020a03600435811690602435166104d2565b34801561024557600080fd5b5061018a600160a060020a03600435166104ef565b34801561026657600080fd5b506100c861050a565b34801561027b57600080fd5b50610161600160a060020a0360043516602435610565565b34801561029f57600080fd5b5061018a600160a060020a03600435811690602435166105ed565b6003805460408051602060026001851615610100026000190190941693909304601f810184900484028201840190925281815292918301828280156103405780601f1061031557610100808354040283529160200191610340565b820191906000526020600020905b81548152906001019060200180831161032357829003601f168201915b505050505081565b336000818152600160209081526040808320600160a060020a038716808552908352818420869055815186815291519394909390927f8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925928290030190a350600192915050565b60025481565b600160a060020a03831660008181526001602090815260408083203384528252808320549383529082905281205490919083118015906103f45750828110155b15156103ff57600080fd5b600160a060020a038085166000908152602081905260408082208054870190559187168152208054849003905560001981101561046157600160a060020a03851660009081526001602090815260408083203384529091529020805484900390555b83600160a060020a031685600160a060020a03167fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef856040518082815260200191505060405180910390a3506001949350505050565b60006020819052908152604090205481565b60045460ff1681565b600160209081526000928352604080842090915290825290205481565b600160a060020a031660009081526020819052604090205490565b6005805460408051602060026001851615610100026000190190941693909304601f810184900484028201840190925281815292918301828280156103405780601f1061031557610100808354040283529160200191610340565b3360009081526020819052604081205482111561058157600080fd5b3360008181526020818152604080832080548790039055600160a060020a03871680845292819020805487019055805186815290519293927fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef929181900390910190a350600192915050565b600160a060020a039182166000908152600160209081526040808320939094168252919091522054905600a165627a7a72305820a364c08a705d8b29603263ebff0569de6c90b2d665d056a3c77729e2eda923ef0029";
+#else
+        public static string BYTECODE = "";
+#endif
+            public EIP20Deployment() : base(BYTECODE)
+            {
+            }
+
+            public EIP20Deployment(string byteCode) : base(byteCode)
+            {
+            }
+
+            [Parameter("uint256", "_initialAmount", 1)]
+            public virtual BigInteger InitialAmount { get; set; }
+
+            [Parameter("string", "_tokenName", 2)]
+            public virtual string TokenName { get; set; }
+
+            [Parameter("uint8", "_decimalUnits", 3)]
+            public virtual byte DecimalUnits { get; set; }
+
+            [Parameter("string", "_tokenSymbol", 4)]
+            public virtual string TokenSymbol { get; set; }
         }
 
         [Fact] //e2e test on multicall and tokenLists
