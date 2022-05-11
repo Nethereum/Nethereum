a54c8f334987ba79f21031c8f0c99b7c33f01fa4
Matthew Scerri <matthew.scerri@gmail.com>
2022-05-11 14:02:47 +0200

    Added new Log/Blockchain Processor tests

--- tests/Nethereum.BlockchainProcessing.UnitTests/LogProcessing/LogOrchestratorTests.cs
+++ tests/Nethereum.BlockchainProcessing.UnitTests/LogProcessing/LogOrchestratorTests.cs
@@ -7,6 +7,7 @@ using Nethereum.RPC.Eth.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -74,6 +75,36 @@ namespace Nethereum.BlockchainProcessing.UnitTests.LogProcessing
 
 
             var progress = await _logOrchestrator.ProcessAsync(fromBlock, toBlock);
+
+            Assert.NotNull(progress);
+            Assert.Equal(toBlock, progress.BlockNumberProcessTo);
+            Assert.Null(progress.Exception);
+            Assert.Equal(logsRetrieved, _logsHandled);
+        }
+
+        [Fact]
+        public async Task Should_Return_Current_Block_When_Cancellation_Occurs_During_Processing()
+        {
+            var fromBlock = new BigInteger(10);
+            var toBlock = new BigInteger(20);
+
+            var logsRetrieved = new List<FilterLog>();
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+            _web3Mock.GetLogsMock
+                .Setup(s => s.SendRequestAsync(It.IsAny<NewFilterInput>(), null))
+                .Returns<NewFilterInput, object>((filter, id) =>
+                {
+                    var logs = new[] { new FilterLog() };
+                    logsRetrieved.AddRange(logs);
+
+                    cancellationTokenSource.Cancel();
+
+                    return Task.FromResult(logs);
+                });
+
+            var progress = await _logOrchestrator.ProcessAsync(fromBlock, toBlock, cancellationTokenSource.Token);
 
             Assert.NotNull(progress);
             Assert.Equal(toBlock, progress.BlockNumberProcessTo);
