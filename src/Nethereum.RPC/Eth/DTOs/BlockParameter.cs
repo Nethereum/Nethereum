c24083b836d1e2d2c75e0592b213351438ee968d
Juan Blanco <juanfranblanco@hotmail.com>
2022-05-13 15:48:29 +0100

    Adding rpc changes on block Parameter https://github.com/Nethereum/Nethereum/issues/815

--- src/Nethereum.RPC/Eth/DTOs/BlockParameter.cs
+++ src/Nethereum.RPC/Eth/DTOs/BlockParameter.cs
@@ -5,6 +5,10 @@ using Newtonsoft.Json;
 
 namespace Nethereum.RPC.Eth.DTOs
 {
+    /// <summary>
+    /// "`earliest`: The lowest numbered block the client has available; `finalized`: The most recent crypto-economically secure block, cannot be re-orged outside of manual intervention driven by community coordination; `safe`: The most recent block that is safe from re-orgs under honest majority and certain synchronicity assumptions; `latest`: The most recent block in the canonical chain observed by the client, this block may be re-orged out of the canonical chain even under healthy/normal conditions; `pending`: A sample next block built by the client on top of `latest` and containing the set of transactions usually taken from local mempool.
+    /// Before the merge transition is finalized, any call querying for `finalized` or `safe` block MUST be responded to with `-39001: Unknown block` error"
+    /// </summary>
     [JsonConverter(typeof (BlockParameterJsonConverter))]
     public class BlockParameter
     {
@@ -13,6 +17,8 @@ namespace Nethereum.RPC.Eth.DTOs
             latest,
             earliest,
             pending,
+            finalized,
+            safe,
             blockNumber
         }
 
