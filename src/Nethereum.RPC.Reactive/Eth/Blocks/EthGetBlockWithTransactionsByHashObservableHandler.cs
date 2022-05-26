1dfe394419311f5dee4a166fa194ce4781e90904
Juan Blanco <juanfranblanco@hotmail.com>
2022-05-26 09:16:00 +0100

    Websockets streaming adding normal rpc methods for GetTransactionByHash and GetBlockWithTransactions

---
+++ src/Nethereum.RPC.Reactive/Eth/Blocks/EthGetBlockWithTransactionsByHashObservableHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nethereum.JsonRpc.Client.Streaming;
+using Nethereum.RPC.Eth.Blocks;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.RPC.Eth.Transactions;
+using Nethereum.RPC.Reactive.RpcStreaming;
+using Nethereum.Hex.HexConvertors.Extensions;
+
+namespace Nethereum.RPC.Reactive.Eth.Blocks
+{
+    public class EthGetBlockWithTransactionsByHashObservableHandler : RpcStreamingResponseParamsObservableHandler<BlockWithTransactions, EthGetBlockWithTransactionsByHash>
+    {
+        public EthGetBlockWithTransactionsByHashObservableHandler(IStreamingClient streamingClient) : base(streamingClient, new EthGetBlockWithTransactionsByHash(null))
+        {
+
+        }
+
+        public Task SendRequestAsync(string blockHash, object id = null)
+        {
+            if (id == null) id = Guid.NewGuid().ToString();
+            var request = RpcRequestResponseHandler.BuildRequest(blockHash.EnsureHexPrefix(), id);
+            return SendRequestAsync(request);
+        }
+    }
+}
---
+++ src/Nethereum.RPC.Reactive/Eth/Transactions/EthGetTransactionByHashObservableHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nethereum.JsonRpc.Client.Streaming;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.RPC.Eth.Transactions;
+using Nethereum.RPC.Reactive.RpcStreaming;
+using Nethereum.Hex.HexConvertors.Extensions;
+
+namespace Nethereum.RPC.Reactive.Eth.Transactions
+{
+    public class EthGetTransactionByHashObservableHandler : RpcStreamingResponseParamsObservableHandler<Transaction, EthGetTransactionByHash>
+    {
+        public EthGetTransactionByHashObservableHandler(IStreamingClient streamingClient) : base(streamingClient, new EthGetTransactionByHash(null))
+        {
+
+        }
+
+        public Task SendRequestAsync(string hashTransaction, object id = null)
+        {
+            if (id == null) id = Guid.NewGuid().ToString();
+            var request = RpcRequestResponseHandler.BuildRequest(hashTransaction.EnsureHexPrefix(), id);
+            return SendRequestAsync(request);
+        }
+    }
+}
