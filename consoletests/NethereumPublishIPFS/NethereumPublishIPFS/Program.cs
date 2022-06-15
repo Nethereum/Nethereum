5fa322327b8a306a07210047f4d15cc262b8faec
Juan Blanco <juanfranblanco@hotmail.com>
2022-06-15 20:05:00 +0100

    Ipfs Publish 4.5.0 and core

--- consoletests/NethereumPublishIPFS/NethereumPublishIPFS/Program.cs
+++ consoletests/NethereumPublishIPFS/NethereumPublishIPFS/Program.cs
@@ -5,35 +5,49 @@ using System.Reflection.Metadata.Ecma335;
 using System.Text.Json.Nodes;
 using Newtonsoft.Json;
 
-var files = Directory.GetFiles("../../../Staging");
-var ipfsService = new Nethereum.Web3.IpfsHttpService("https://ipfs.infura.io:5001", "id", "id");
-var metadataList = new List<AssemblyMetadata>();
-foreach (var file in files)
-{
-    Thread.SpinWait(2000);
-    var absolutePath = Path.GetFullPath(file);
-    var assembly =  Assembly.LoadFile(absolutePath);
-    var fullName = assembly.FullName;
-    var name = assembly.GetName().Name;
-    var ipfsHash = await ipfsService.AddFileAsync(absolutePath);
-    var assemblyMetadata = new AssemblyMetadata();
-    assemblyMetadata.FullName = fullName;
-    assemblyMetadata.Name = name;
-    assemblyMetadata.Url = "ipfs://" + ipfsHash.Hash;
-    metadataList.Add(assemblyMetadata);
-}
-using (StreamWriter file = File.CreateText(@"../../../Libraries/assemblies.json"))
-{
-    JsonSerializer serializer = new JsonSerializer();
-    serializer.Serialize(file, metadataList);
-}
+await AssemblyPublisher.PublishAssemblies(@"../../../Staging", @"../../../Libraries/assemblies.json");
+//await AssemblyPublisher.PublishAssemblies(@"../../../NetCore", @"../../../Libraries/core-assemblies.json");
+//await AssemblyPublisher.PublishAssemblies(@"../../../NetDapps", @"../../../Libraries/net-dapps.json");
 
 public class AssemblyMetadata
 {
-    public string Name { get; set; }
+    [JsonProperty("fullName")]
     public string FullName { get; set; }
+    [JsonProperty("name")]
+    public string Name { get; set; }
+    [JsonProperty("url")]
     public string Url { get; set; }
 }
 
+
+public class AssemblyPublisher
+{
+    public static async Task PublishAssemblies(string path, string fileOuput)
+    {
+
+        var files = Directory.GetFiles(path);
+        var ipfsService = new Nethereum.Web3.IpfsHttpService("https://ipfs.infura.io:5001", "", "");
+        var metadataList = new List<AssemblyMetadata>();
+        foreach (var file in files)
+        {
+            Thread.SpinWait(2000);
+            var absolutePath = Path.GetFullPath(file);
+            var assembly = Assembly.LoadFile(absolutePath);
+            var fullName = assembly.FullName;
+            var name = assembly.GetName().Name;
+            var ipfsHash = await ipfsService.AddFileAsync(absolutePath);
+            var assemblyMetadata = new AssemblyMetadata();
+            assemblyMetadata.FullName = fullName;
+            assemblyMetadata.Name = name;
+            assemblyMetadata.Url = "ipfs://" + ipfsHash.Hash;
+            metadataList.Add(assemblyMetadata);
+        }
+        using (StreamWriter file = File.CreateText(fileOuput))
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Serialize(file, metadataList);
+        }
+    }
+}
 
 
