0ac7332d0543e2f9162606508babed9cf58dd3a6
Juan Blanco <juanfranblanco@hotmail.com>
2022-07-06 14:57:21 +0100

    IAdminApiService update to match AdminApiService methods

--- src/Nethereum.Geth/IAdminApiService.cs
+++ src/Nethereum.Geth/IAdminApiService.cs
@@ -4,13 +4,20 @@ namespace Nethereum.Geth
 {
     public interface IAdminApiService
     {
-        IAdminAddPeer AddPeer { get; }
-        IAdminDatadir Datadir { get; }
-        IAdminNodeInfo NodeInfo { get; }
-        IAdminPeers Peers { get; }
-        IAdminStartRPC StartRPC { get; }
-        IAdminStartWS StartWS { get; }
-        IAdminStopRPC StopRPC { get; }
-        IAdminStopWS StopWS { get; }
+         IAdminAddPeer AddPeer { get; }
+         IAdminRemovePeer RemovePeer { get; }
+         IAdminAddTrustedPeer AddTrustedPeer { get; }
+         IAdminRemoveTrustedPeer RemoveTrustedPeer { get; }
+         IAdminStartHTTP StartHttp { get; }
+         IAdminStopHTTP StopHttp { get; }
+         IAdminExportChain ExportChain { get; }
+         IAdminImportChain ImportChain { get; }
+         IAdminDatadir Datadir { get; }
+         IAdminNodeInfo NodeInfo { get; }
+         IAdminStartRPC StartRPC { get; }
+         IAdminStartWS StartWS { get; }
+         IAdminStopRPC StopRPC { get; }
+         IAdminStopWS StopWS { get; }
+         IAdminPeers Peers { get; }
     }
 }
