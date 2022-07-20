8f0e800d1cd0c9819c2471444f4e3120a542c20b
Juan Blanco <juanfranblanco@hotmail.com>
2022-07-20 16:57:49 +0100

    Signing forces now a chainId

--- tests/Nethereum.Accounts.IntegrationTests/SignOfflineTransactionTest.cs
+++ tests/Nethereum.Accounts.IntegrationTests/SignOfflineTransactionTest.cs
@@ -24,7 +24,7 @@ namespace Nethereum.Accounts.IntegrationTests
         [Fact]
         public async Task ShouldSignOfflineTransaction()
         {
-            var account = new Account("0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7");
+            var account = new Account("0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7", 1);
             var web3 = new Web3.Web3(account);
             var transfer = new TransferFunction()
             {
@@ -40,7 +40,7 @@ namespace Nethereum.Accounts.IntegrationTests
             var signedMessage = await web3.Eth.GetContractHandler("0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe")
                 .SignTransactionAsync(transfer).ConfigureAwait(false);
 
-            Assert.Equal("f8a201646494de0b295669a9fd93d5f28d9ec85e40f4cb697bae80b844a9059cbb00000000000000000000000012890d2cce102216644c59dae5baed380d84830c000000000000000000000000000000000000000000000000000000000000000a1ca0a928719a67ff346732bfacd82d8c3d5f50490f57a9edd0c92438714bd6815cd4a0713e0577939049551bf0d4f66bacd2cf4ac371daa16f904f57d804101dcc6ee7", signedMessage);
+            Assert.Equal("f8a201646494de0b295669a9fd93d5f28d9ec85e40f4cb697bae80b844a9059cbb00000000000000000000000012890d2cce102216644c59dae5baed380d84830c000000000000000000000000000000000000000000000000000000000000000a25a0a0800bb95232492a24594b9c45ba3a77823e4c136b04187d9354038cd5bee76da00f54518f9a2d370b791a524a655cc16c68d5a676d93c504c1e6fc304712811f1", signedMessage);
         }
     }
 }
