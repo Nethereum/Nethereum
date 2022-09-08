5b6dd7ca8565581a5a8de56f3d9941ee0356ad4b
Juan Blanco <juanfranblanco@hotmail.com>
2022-09-08 17:56:43 +0100

    Code generator VB service can instantiated with IWeb3

--- generators/Nethereum.Generators/Service/Vb/ServiceVbTemplate.cs
+++ generators/Nethereum.Generators/Service/Vb/ServiceVbTemplate.cs
@@ -23,11 +23,16 @@ namespace Nethereum.Generators.Service
 {SpaceUtils.OneTab}
 {_deploymentServiceMethodsVbTemplate.GenerateMethods()}
 {SpaceUtils.OneTab}
-{SpaceUtils.TwoTabs}Protected Property Web3 As Nethereum.Web3.Web3
+{SpaceUtils.TwoTabs}Protected Property Web3 As Nethereum.Web3.IWeb3
 {SpaceUtils.TwoTabs}
 {SpaceUtils.TwoTabs}Public Property ContractHandler As ContractHandler
 {SpaceUtils.TwoTabs}
 {SpaceUtils.TwoTabs}Public Sub New(ByVal web3 As Nethereum.Web3.Web3, ByVal contractAddress As String)
+{SpaceUtils.ThreeTabs}Web3 = web3
+{SpaceUtils.ThreeTabs}ContractHandler = web3.Eth.GetContractHandler(contractAddress)
+{SpaceUtils.TwoTabs}End Sub
+{SpaceUtils.OneTab}
+{SpaceUtils.TwoTabs}Public Sub New(ByVal web3 As Nethereum.Web3.IWeb3, ByVal contractAddress As String)
 {SpaceUtils.ThreeTabs}Web3 = web3
 {SpaceUtils.ThreeTabs}ContractHandler = web3.Eth.GetContractHandler(contractAddress)
 {SpaceUtils.TwoTabs}End Sub
