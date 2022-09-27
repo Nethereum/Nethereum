2489cbb405ff957cc9421ca7e06d69889da4f0b2
Juan Blanco <juanfranblanco@hotmail.com>
2022-09-27 10:52:45 +0100

    Nethereum host provider and Address / Hex Validation attributes

---
+++ src/Nethereum.UI/NethereumHostProvider.cs
@@ -0,0 +1,107 @@
+﻿using Nethereum.Signer;
+using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using System;
+using System.Threading.Tasks;
+
+namespace Nethereum.UI
+{
+    public class NethereumHostProvider : IEthereumHostProvider
+    {
+        public string Name => "Nethereum Host Provider";
+
+        public bool Available => true;
+
+        protected Account Account { get; private set; }
+
+        protected string Url { get; set; }
+
+        public string SelectedAccount { get { return Account == null ? null : Account.Address; } }
+
+        public bool Enabled => Account != null && !string.IsNullOrEmpty(Url);
+
+        public long SelectedNetworkChainId { get; private set; }
+
+        public event Func<string, Task> SelectedAccountChanged;
+        public event Func<long, Task> NetworkChanged;
+        public event Func<bool, Task> AvailabilityChanged;
+        public event Func<bool, Task> EnabledChanged;
+
+        public Task<bool> CheckProviderAvailabilityAsync()
+        {
+            return Task.FromResult(true);
+        }
+
+        public Task<string> EnableProviderAsync()
+        {
+            return Task.FromResult(SelectedAccount);
+        }
+
+        public Task<string> GetProviderSelectedAccountAsync()
+        {
+            return Task.FromResult(SelectedAccount);
+        }
+
+        public Task<long> GetProviderSelectedNetworkAsync()
+        {
+            return Task.FromResult(SelectedNetworkChainId);
+        }
+
+        public Task<string> SignMessageAsync(string message)
+        {
+            var signer = new EthereumMessageSigner();
+            var signedMessage = signer.EncodeUTF8AndSign(message, new EthECKey(((Account)Account).PrivateKey));
+            return Task.FromResult(signedMessage);
+        }
+
+        public void SetSelectedAccount(string privateKey)
+        { 
+            SetSelectedAccount(new Account(privateKey, SelectedNetworkChainId));
+        }
+
+        public void SetSelectedAccount(Account account)
+        {
+            Account = account;
+
+            if (SelectedAccountChanged != null)
+            {
+                if (Account != null)
+                {
+
+                    SelectedAccountChanged(Account.Address);
+                }
+                else
+                {
+                    SelectedAccountChanged(null);
+                }
+            }
+        }
+
+        public async Task<bool> SetUrl(string url)
+        {
+            var web3 = new Web3.Web3(url);
+            try
+            {
+                var chainId = await web3.Eth.ChainId.SendRequestAsync();
+                Url = url;
+                SelectedNetworkChainId = (long)chainId.Value;
+
+                if (NetworkChanged != null)
+                {
+                    await NetworkChanged(SelectedNetworkChainId);
+                }
+                return true;
+            }
+            catch 
+            {
+                Url = null;
+                return false;
+            }
+        }
+
+        public Task<IWeb3> GetWeb3Async()
+        {
+            return Task.FromResult((IWeb3)new Web3.Web3(new Account(Account.PrivateKey, Account.ChainId), Url));
+        }
+    }
+}
---
+++ src/Nethereum.UI/Validation/Attributes/EthereumAddressAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Util;
+#if NETCOREAPP3_1_OR_GREATER
+using System.ComponentModel.DataAnnotations;
+
+namespace Nethereum.UI.Validation.Attributes
+{
+    public sealed class EthereumAddressAttribute : ValidationAttribute
+    {
+        public EthereumAddressAttribute()
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null) return new ValidationResult("Address cannot be null");
+            if (value.ToString().IsValidEthereumAddressHexFormat()) return ValidationResult.Success;
+            return new ValidationResult("Invalid Ethereum address");
+        }
+    }
+
+}
+#endif
