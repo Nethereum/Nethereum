38347dba5ac248d0bc0256153e74dfe914d9b2b8
Juan Blanco <juanfranblanco@hotmail.com>
2023-04-20 13:28:25 +0100

    Geth upgrade for integration testing.. include --miner.etherbase to match the nethereum account

--- tests/Nethereum.Signer.IntegrationTests/TimePreferenceSuggestionStrategy1559Tests.cs
+++ tests/Nethereum.Signer.IntegrationTests/TimePreferenceSuggestionStrategy1559Tests.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using System.Threading;
 using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth.DTOs;
 using Nethereum.RPC.Fee1559Suggestions;
+using Nethereum.Web3.Accounts;
 using Nethereum.XUnitEthereumClients;
 using Xunit;
 // ReSharper disable ConsiderUsingConfigureAwait
@@ -24,8 +26,9 @@ namespace Nethereum.Signer.IntegrationTests
             if (_ethereumClientIntegrationFixture.EthereumClient == EthereumClient.Geth)
             {
                 var receiveAddress = "0x13f022d72158410433cbd66f5dd8bf6d2d129924";
-
+                var listTransactionHashes = new List<string>();
                 var web3 = _ethereumClientIntegrationFixture.GetWeb3();
+                web3.TransactionReceiptPolling.SetPollingRetryIntervalInMilliseconds(2000);
 #if NETCOREAPP3_1_OR_GREATER || NET50
                 EthECKey.SignRecoverable = true;
 #endif
@@ -33,7 +36,7 @@ namespace Nethereum.Signer.IntegrationTests
                 for (var x = 0; x < 10; x++)
                 {
                     Thread.Sleep(500);
-                    var fee = await feeStrategy.SuggestFeesAsync().ConfigureAwait(false);
+                    var fee = await feeStrategy.SuggestFeeAsync().ConfigureAwait(false);
                     for (int i = 0; i < 50; i++)
                     {
                         var encoded = await web3.TransactionManager.SendTransactionAsync(
@@ -41,12 +44,20 @@ namespace Nethereum.Signer.IntegrationTests
                             {
                                 Type = new HexBigInteger(2),
                                 From = web3.TransactionManager.Account.Address,
-                                MaxFeePerGas = new HexBigInteger(fee[0].MaxFeePerGas.Value),
-                                MaxPriorityFeePerGas = new HexBigInteger(fee[0].MaxPriorityFeePerGas.Value),
+                                MaxFeePerGas = new HexBigInteger(fee.MaxFeePerGas.Value),
+                                MaxPriorityFeePerGas = new HexBigInteger(fee.MaxPriorityFeePerGas.Value),
                                 To = receiveAddress,
                                 Value = new HexBigInteger(10)
                             }).ConfigureAwait(false);
-                    }
+                        listTransactionHashes.Add(encoded);
+                   }
+                }
+
+                foreach (var tx in listTransactionHashes)
+                {   
+                    
+                    var receipt = await web3.TransactionReceiptPolling.PollForReceiptAsync(tx);
+                    Assert.True(receipt.Succeeded());
                 }
             }
         }
