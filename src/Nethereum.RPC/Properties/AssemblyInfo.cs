d045b019c3e387a3a2da5439b2e8778dad7bf4bc
Juan Blanco <juanfranblanco@hotmail.com>
2023-06-30 13:47:13 +0100

    Rpc assemblyinfo fixes

--- src/Nethereum.RPC/Properties/AssemblyInfo.cs
+++ src/Nethereum.RPC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Ethereum.RPC")]
+[assembly: AssemblyTitle("Nethereum.RPC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ethereum.RPC")]
+[assembly: AssemblyProduct("Nethereum.RPC")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
