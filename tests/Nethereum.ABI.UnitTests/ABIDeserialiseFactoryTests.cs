23770135ab8226fc925424cea270be2364a72f6d
Juan Blanco <juanfranblanco@hotmail.com>
2023-07-27 20:25:18 +0200

    Nethereum.Abi Compilation metadata parsing and objects

--- tests/Nethereum.ABI.UnitTests/ABIDeserialiseFactoryTests.cs
+++ tests/Nethereum.ABI.UnitTests/ABIDeserialiseFactoryTests.cs
@@ -4,6 +4,169 @@ using Xunit;
 
 namespace Nethereum.ABI.UnitTests
 {
+    public class CompilationOuputTests
+    {
+        string jsonOuput = @"{
+
+  ""compiler"": {
+
+    ""keccak256"": ""0x123..."",
+
+    ""version"": ""0.8.2+commit.661d1103""
+  },
+
+  ""language"": ""Solidity"",
+
+  ""output"": {
+
+    ""abi"": [],
+
+    ""devdoc"": {
+
+      ""author"": ""John Doe"",
+
+      ""details"": ""Interface of the ERC20 standard as defined in the EIP. See https://eips.ethereum.org/EIPS/eip-20 for details"",
+      ""errors"": {
+        ""MintToZeroAddress()"" : {
+          ""details"": ""Cannot mint to zero address""
+        }
+      },
+      ""events"": {
+        ""Transfer(address,address,uint256)"": {
+          ""details"": ""Emitted when `value` tokens are moved from one account (`from`) toanother (`to`)."",
+          ""params"": {
+            ""from"": ""The sender address"",
+            ""to"": ""The receiver address"",
+            ""value"": ""The token amount""
+          }
+        }
+      },
+      ""kind"": ""dev"",
+      ""methods"": {
+        ""transfer(address,uint256)"": {
+
+          ""details"": ""Returns a boolean value indicating whether the operation succeeded. Must be called by the token holder address"",
+
+          ""params"": {
+            ""_value"": ""The amount tokens to be transferred"",
+            ""_to"": ""The receiver address""
+          },
+
+          ""returns"": {
+
+            ""success"": ""a boolean value indicating whether the operation succeeded""
+          }
+        }
+      },
+      ""stateVariables"": {
+        ""owner"": {
+
+          ""details"": ""Must be set during contract creation. Can then only be changed by the owner""
+        }
+      },
+
+      ""title"": ""MyERC20: an example ERC20"",
+      ""version"": 1 
+    },
+
+    ""userdoc"": {
+      ""errors"": {
+        ""ApprovalCallerNotOwnerNorApproved()"": 
+          {
+            ""notice"": ""The caller must own the token or be an approved operator.""
+          }
+        
+      },
+      ""events"": {
+        ""Transfer(address,address,uint256)"": {
+          ""notice"": ""`_value` tokens have been moved from `from` to `to`""
+        }
+      },
+      ""kind"": ""user"",
+      ""methods"": {
+        ""transfer(address,uint256)"": {
+          ""notice"": ""Transfers `_value` tokens to address `_to`""
+        }
+      },
+      ""version"": 1 
+    }
+  },
+
+  ""settings"": {
+
+    ""compilationTarget"": {
+      ""myDirectory/myFile.sol"": ""MyContract""
+    },
+
+    ""evmVersion"": ""london"",
+
+    ""libraries"": {
+      ""MyLib"": ""0x123123...""
+    },
+    ""metadata"": {
+
+      ""appendCBOR"": true,
+
+      ""bytecodeHash"": ""ipfs"",
+
+      ""useLiteralContent"": true
+    },
+
+    ""optimizer"": {
+      ""details"": {
+        ""constantOptimizer"": false,
+        ""cse"": false,
+        ""deduplicate"": false,
+
+        ""inliner"": false,
+
+        ""jumpdestRemover"": true,
+        ""orderLiterals"": false,
+
+        ""peephole"": true,
+        ""yul"": true,
+
+        ""yulDetails"": {
+          ""optimizerSteps"": ""dhfoDgvulfnTUtnIf..."",
+          ""stackAllocation"": false
+        }
+      },
+      ""enabled"": true,
+      ""runs"": 500
+    },
+
+    ""remappings"": [ "":g=/dir"" ]
+  },
+
+  ""sources"": {
+    ""destructible"": {
+
+      ""content"": ""contract destructible is owned { function destroy() { if (msg.sender == owner) selfdestruct(owner); } }"",
+
+      ""keccak256"": ""0x234...""
+    },
+    ""myDirectory/myFile.sol"": {
+
+      ""keccak256"": ""0x123..."",
+
+      ""license"": ""MIT"",
+
+      ""urls"": [ ""bzz-raw://7d7a..."", ""dweb:/ipfs/QmN..."" ]
+    }
+  },
+
+  ""version"": 1
+}";
+
+
+        [Fact]
+        public void ShouldDeserialiseJsonCompilationOutput() 
+        {
+            var result =  CompilationMetadata.CompilationMetadataDeserialiser.DeserialiseCompilationMetadata(jsonOuput);
+        }
+    }
+
+
     public class ABIDeserialiseFactoryTests
     {
         [Fact]
