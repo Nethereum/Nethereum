50087e7c8dceb0c62a578f06c2fa4e61b2ebab82
Artem Kurianov <artemkuryanov@gmail.com>
2023-10-13 11:32:40 +0300

    updated text records enum according to ENS docs

--- src/Nethereum.ENS/TextDataKey.cs
+++ src/Nethereum.ENS/TextDataKey.cs
@@ -1,5 +1,8 @@
 ﻿namespace Nethereum.ENS
 {
+    /// <summary>
+    /// List of default text records from ENS docs <see href="https://docs.ens.domains/ens-improvement-proposals/ensip-5-text-records"/> 
+    /// </summary>
     public enum TextDataKey
     {
         email,
@@ -9,6 +12,19 @@
         notice,
         keywords,
         vnd_twitter,
-        vnd_github
+        vnd_github,
+        com_twitter,
+        com_github,
+        com_discord,
+        com_reddit,
+        org_telegram,
+        eth_ens_delegate,
+        display,
+        mail,
+        location,
+        phone,
+        com_peepeth,
+        com_linkedin,
+        io_keybase
     }
 }
