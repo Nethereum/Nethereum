9bf29d8c471a72c593467a0d74b602e2ab1e6a32
Juan Blanco <juanfranblanco@hotmail.com>
2024-02-05 16:47:03 +0000

    Features extended

--- src/Nethereum.RPC/Chain/ChainDefaultFeaturesServicesRepository.cs
+++ src/Nethereum.RPC/Chain/ChainDefaultFeaturesServicesRepository.cs
@@ -1,4 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
+using Nethereum.RPC.HostWallet;
 
 namespace Nethereum.RPC.Chain
 {
@@ -8,9 +12,49 @@ namespace Nethereum.RPC.Chain
         {
             return new List<ChainFeature>()
             {
-                new ChainFeature(){ChainId = 1, ChainName="Mainnet", SupportEIP155 = true, SupportEIP1559 = true},
-            };
+                new ChainFeature(){
+                                   ChainId = new BigInteger((int)Signer.Chain.MainNet),
+                                   ChainName="Ethereum Mainnet",
+                                   NativeCurrency = new NativeCurrency(){Name = "Ether", Symbol = "ETH", Decimals = 18},
+                                   HttpRpcs = new List<string>(){ "https://mainnet.infura.io/v3/{INFURA_API_KEY}",
+                                                                  "https://api.mycryptoapi.com/eth",
+                                                                  "https://ethereum.publicnode.com"},
+                                   WsRpcs = new List<string>(){ "wss://mainnet.infura.io/ws/v3/{INFURA_API_KEY}",
+                                                                 "wss://ethereum.publicnode.com"},
+                                   Explorers = new List<string>(){"https://etherscan.io/", "https://eth.blockscout.com"},
+                                   SupportEIP155 = true,
+                                   SupportEIP1559 = true},
+
+               new ChainFeature()
+                    {
+                        ChainId = new BigInteger((int)Signer.Chain.Optimism),
+                        ChainName = "OP Mainnet",
+                        NativeCurrency = new NativeCurrency()
+                        {
+                            Name = "Ether",
+                            Symbol = "ETH",
+                            Decimals = 18
+                        },
+                        HttpRpcs = new List<string>()
+                        {
+                            "https://mainnet.optimism.io",
+                            "https://optimism.publicnode.com",
+
+                        },
+                        WsRpcs = new List<string>()
+                        {
+                            "wss://optimism.publicnode.com",
+
+                        },
+                        Explorers = new List<string>()
+                        {
+                            "https://optimistic.etherscan.io",
+                            "https://optimism.blockscout.com",
+                        },
+                        SupportEIP155 = true
+
+                    }
+               };
         }
     }
-
 }
--- src/Nethereum.RPC/Chain/ChainFeature.cs
+++ src/Nethereum.RPC/Chain/ChainFeature.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Nethereum.RPC.Chain
 {
@@ -7,7 +8,18 @@ namespace Nethereum.RPC.Chain
         public BigInteger ChainId { get; set; }
         public bool SupportEIP1559 { get; set; } = true;
         public bool SupportEIP155 { get; set; } = true;
+        public NativeCurrency NativeCurrency { get; set; } = new NativeCurrency();
         public string ChainName { get; set; }
+        public List<string> HttpRpcs { get; set; } = new List<string>();
+        public List<string> WsRpcs { get; set; } = new List<string>();
+        public List<string> Explorers { get; set; } = new List<string>();
+    }
+
+    public class NativeCurrency
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; } 
+        public int Decimals { get; set; }
     }
 
 }
