44018af9d3011541858e9bb3909a0adf1f9a292e
Juan Blanco <juanfranblanco@hotmail.com>
2024-05-03 16:15:37 +0100

    Mapping extensions (SchemaEncoded to RegisterTable)

---
+++ src/Nethereum.Mud.Contracts/MappingExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Nethereum.Mud.Contracts.RegistrationSystem.ContractDefinition;
+using Nethereum.Mud.EncodingDecoding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nethereum.Mud.Contracts
+{
+    public static class MappingExtensions
+    {
+
+        public static RegisterTableFunction ToRegisterTableFunction(this SchemaEncoded schemaEncoded)
+        {
+            return new RegisterTableFunction()
+            {
+                KeySchema = schemaEncoded.KeySchema,
+                ValueSchema = schemaEncoded.ValueSchema,
+                TableId = schemaEncoded.TableId,
+                FieldLayout = schemaEncoded.FieldLayout,
+                FieldNames = schemaEncoded.FieldNames,
+                KeyNames = schemaEncoded.KeyNames,
+
+            };
+        }
+       
+    }
+}
