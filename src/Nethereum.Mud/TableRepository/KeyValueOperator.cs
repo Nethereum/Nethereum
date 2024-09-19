4ddb6d4e475862f40bd24f8c124a3a01f6c9e27f
Juan Blanco <juanfranblanco@hotmail.com>
2024-09-19 14:28:04 +0100

    Key value operator of the predicate query

---
+++ src/Nethereum.Mud/TableRepository/KeyValueOperator.cs
@@ -0,0 +1,19 @@
+﻿namespace Nethereum.Mud.TableRepository
+{
+    public class KeyValueOperator
+    {
+        public string Key { get; set; }
+        public string AbiType { get; set; }
+        public string HexValue { get; set; }
+        public string PropertyName { get; set; }
+        public int Order { get; set; }
+        public string ComparisonOperator { get; set; }
+        public string Name { get; set; }
+        public string TableId { get; set; }
+        public string Address { get; set; }
+
+        public string UnionOperator { get; set; }
+
+    }
+
+}
