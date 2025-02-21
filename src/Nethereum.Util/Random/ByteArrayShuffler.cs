cfe1ff47ac859649f00e8c5b0d49fd3db05a8c4a
Juan Blanco <juanfranblanco@hotmail.com>
2025-02-21 15:18:00 +0000

    Shuffler

---
+++ src/Nethereum.Util/Random/ByteArrayShuffler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nethereum.Util.Random
+{
+    public class ByteArrayShuffler
+    {
+        public GenericShuffler<byte> Shuffler { get; }
+
+        public ByteArrayShuffler()
+        {
+            Shuffler = new GenericShuffler<byte>();
+        }
+
+        public byte[] ShuffleRandomTimes(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length < 3)
+                throw new ArgumentException("Array must have at least 3 items to shuffle.");
+            List<byte> byteList = bytes.ToList();
+            byteList = Shuffler.ShuffleRandomTimes(byteList);
+            return byteList.ToArray();
+        }
+
+        public byte[] ShuffleMultipleTimes(byte[] bytes, int times = 7)
+        {
+            if (bytes == null || bytes.Length < 3)
+                throw new ArgumentException("Array must have at least 3 items to shuffle.");
+            List<byte> byteList = bytes.ToList();
+            byteList = Shuffler.ShuffleMultipleTimes(byteList, times);
+            return byteList.ToArray();
+        }
+
+        public byte[] ShuffleOnce(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length < 3)
+                throw new ArgumentException("Array must have at least 3 items to shuffle.");
+            List<byte> byteList = bytes.ToList();
+            byteList = Shuffler.ShuffleOnce(byteList);
+            return byteList.ToArray();
+        }
+    }
+}
---
+++ src/Nethereum.Util/Random/GenericShuffler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nethereum.Util.Random
+{
+
+    public class GenericShuffler<T>
+    {
+        private readonly System.Random _random = new System.Random();
+
+        public List<T> ShuffleOnce(List<T> items)
+        {
+            if (items == null || items.Count < 3)
+                throw new ArgumentException("List must have at least 3 items to shuffle.");
+
+            int countToShuffle = items.Count / 3;
+            List<T> selectedItems = items.OrderBy(_ => _random.Next()).Take(countToShuffle).ToList();
+            List<T> remainingItems = items.Except(selectedItems).ToList();
+
+            foreach (var item in selectedItems.OrderBy(_ => _random.Next()))
+            {
+                int insertIndex = (_random.Next(1, 10) * _random.Next(1, 10)) % (remainingItems.Count + 1);
+                remainingItems.Insert(insertIndex, item);
+            }
+
+            return remainingItems;
+        }
+
+        public List<T> ShuffleMultipleTimes(List<T> items, int times)
+        {
+            var shuffledList = new List<T>(items);
+            for (int i = 0; i < times; i++)
+            {
+                shuffledList = ShuffleOnce(shuffledList);
+            }
+            return shuffledList;
+        }
+
+        public List<T> ShuffleRandomTimes(List<T> items)
+        {
+            int times = (_random.Next(1, 6) * _random.Next(1, 4)) / 2; // Shuffle between 1 and 10 times, roughly
+            return ShuffleMultipleTimes(items, times);
+        }
+    }
+}
