4433d11caf52feebb727817ebe9819632c65d883
Juan Blanco <juanfranblanco@hotmail.com>
2025-05-09 17:16:54 +0100

    Nethereum.MudBlazorComponents New project
    New project providing different Blazor components in (MudBlazor) to provide simple data input for smart contracts and Mud (Mud.Dev) Tables.
    + QueryFunctionComponent you can query any smart contract by providing the FunctionMessageType, SmartContractAddress and the Output, or a service type and the query method.
    + TransactionFunctionComponent you can submit any transaction in the same way as the QueryFunctionComponent, the service will be used to decode all the events output after the transaction submission, you can also provide a set of event types for decoding
    + ContractDeploymentComponent as per the Transactions but to deploy smart contracts.
    
    + MudDevTableComponent using a TableServiceType it can be used to both query by key a table and upsert a value.
    + MudDevTableServicesComponent scans an assembly for all the TableServiceTypes and creates a user interface (collection) of MudTableComponents per each table found.

---
+++ src/Nethereum.MudBlazorComponents/DynamicRouteService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using MudBlazor;
+using System.Reflection;
+
+namespace Nethereum.MudBlazorComponents
+{
+    public class DynamicRouteService
+    {
+        //.gen is converted to _gen as the type name
+        public List<NavItem> GetGeneratedRoutes(string suffixFilter = "_gen")
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a =>
+                {
+                    try { return a.GetExportedTypes(); } catch { return Enumerable.Empty<Type>(); }
+                })
+               .Where(t => typeof(ComponentBase).IsAssignableFrom(t))
+               .Where(t => {
+                       // Console.WriteLine(t.Name);
+                        return t.Name.EndsWith(suffixFilter, StringComparison.OrdinalIgnoreCase);
+                    })
+                .SelectMany(t => t.GetCustomAttributes<RouteAttribute>().Select(r => new { t.Name, r.Template }))
+                .Where(x => !string.IsNullOrWhiteSpace(x.Template))
+                .Select(r => new NavItem
+                {
+                    Title = r.Name.Replace(suffixFilter, "", StringComparison.OrdinalIgnoreCase),
+                    Href = r.Template,
+                    Icon = Icons.Material.Filled.List
+                })
+               .ToList();
+        }
+    }
+
+    public class NavItem
+    {
+        public string Title { get; set; }
+        public string Href { get; set; }
+        public string Icon { get; set; }
+        public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
+    }
+}
