62b06359322c986fa21a8308985e61c4d3c793f9
Juan Blanco <juanfranblanco@hotmail.com>
2025-05-14 18:23:07 +0100

    Fixing geth dev (forcing dev mining 1 second)

--- testchain/clique/startgeth.bat
+++ testchain/clique/startgeth.bat
@@ -1,3 +1,3 @@
 RD /S /Q %~dp0\devChain\geth
 geth --dev --datadir=devChain init genesis_clique.json
-geth --dev --nodiscover  --datadir=devChain --http --http.corsdomain "*" --mine  --ws --http.api "eth,web3,personal,net,miner,admin,debug" --http.addr "0.0.0.0" --allow-insecure-unlock --unlock 0x12890d2cce102216644c59daE5baed380d84830c --miner.etherbase 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt" --verbosity 0 console
+geth --dev --nodiscover --mine --dev.period 1 --datadir=devChain --keystore devChain/keystore  --http --http.corsdomain "*" --ws --http.api "eth,web3,personal,net,miner,admin,debug" --http.addr "0.0.0.0" --unlock 0x12890d2cce102216644c59daE5baed380d84830c --miner.etherbase 0x12890d2cce102216644c59daE5baed380d84830c --password "pass.txt" --verbosity 5 console
