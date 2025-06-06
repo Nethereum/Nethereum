97f7c19c0445bcaab347dc946ef1612dd956f2d1
Juan Blanco <juanfranblanco@hotmail.com>
2025-06-06 10:11:36 +0100

    New Json utils to to convert hex to byte array for both newtonsoft and stj

---
+++ src/Nethereum.Util/Json/HexToByteArrayConverter .cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Nethereum.Util.Json
+{
+    using Nethereum.Hex.HexConvertors.Extensions;
+    using System.Numerics;
+#if NET6_0_OR_GREATER
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+
+    public class HexToByteArrayConverter : JsonConverter<byte[]>
+    {
+        public override byte[] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            => reader.GetString().HexToByteArray();
+
+        public override void Write(Utf8JsonWriter writer, byte[] value, JsonSerializerOptions options)
+            => writer.WriteStringValue(value.ToHex(true));
+    }
+
+    public class BigIntegerJsonConverter : System.Text.Json.Serialization.JsonConverter<BigInteger>
+    {
+        public override BigInteger Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return reader.TokenType switch
+            {
+                JsonTokenType.String => BigInteger.Parse(reader.GetString()),
+                JsonTokenType.Number => reader.TryGetInt64(out var l) ? new BigInteger(l) : BigInteger.Parse(reader.GetDouble().ToString()),
+                _ => throw new JsonException($"Unexpected token parsing BigInteger: {reader.TokenType}")
+            };
+        }
+
+        public override void Write(Utf8JsonWriter writer, BigInteger value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+}
+#endif
+
+
+}
---
+++ src/Nethereum.Util/Json/NewtonsoftHexToByteArrayConverter .cs
@@ -0,0 +1,19 @@
+﻿using Nethereum.Hex.HexConvertors.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nethereum.Util.Json
+{
+    public class NewtonsoftHexToByteArrayConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(byte[]);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    => ((string)reader.Value).HexToByteArray();
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            => writer.WriteValue(((byte[])value).ToHex(true));
+    }
+}
