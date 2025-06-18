5c990a576b6c27201b063d9abaa5afd4cc0c192c
Juan Blanco <juanfranblanco@hotmail.com>
2025-06-18 15:14:02 +0100

    TypedDataRaw extension GetChainIdFromDomain

--- src/Nethereum.ABI/EIP712/TypedDataRaw.cs
+++ src/Nethereum.ABI/EIP712/TypedDataRaw.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Nethereum.ABI.EIP712
 {
@@ -13,5 +14,29 @@ namespace Nethereum.ABI.EIP712
         public string PrimaryType { get; set; }
         public MemberValue[] Message { get; set; }
         public MemberValue[] DomainRawValues { get; set; }
+
+    }
+
+    public static class TypedDataRawExtensions
+    {
+        public static BigInteger? GetChainIdFromDomain(this TypedDataRaw typedData)
+        {
+            if (!typedData.Types.TryGetValue("EIP712Domain", out var domainMembers))
+                return null;
+
+            for (int i = 0; i < domainMembers.Length; i++)
+            {
+                var member = domainMembers[i];
+                if (member.Type == "uint256" && member.Name == "chainId")
+                {
+                    if (i < typedData.DomainRawValues.Length && typedData.DomainRawValues[i].Value is BigInteger value)
+                    {
+                        return value;
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
