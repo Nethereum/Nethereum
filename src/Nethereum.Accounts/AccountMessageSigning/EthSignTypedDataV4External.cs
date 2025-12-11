6b7bd150be98f8ffad8f0cf5d7d5543c33254425
Juan Blanco <juanfranblanco@hotmail.com>
2025-12-11 18:51:52 +0000

    v4 external signer update

--- src/Nethereum.Accounts/AccountMessageSigning/EthSignTypedDataV4External.cs
+++ src/Nethereum.Accounts/AccountMessageSigning/EthSignTypedDataV4External.cs
@@ -11,20 +11,16 @@ namespace Nethereum.Accounts.AccountMessageSigning
     public class EthSignTypedDataV4External : IEthSignTypedDataV4
     {
         private readonly IEthExternalSigner _ethExternalSigner;
-        private Eip712TypedDataSigner _typedDataSigner;
 
         public EthSignTypedDataV4External(IEthExternalSigner ethExternalSigner)
         {
             _ethExternalSigner = ethExternalSigner;
-            _typedDataSigner = new Eip712TypedDataSigner();
         }
 
 
         public async Task<string> SendRequestAsync(string jsonMessage, object id = null)
         {
-            var encodedData = _typedDataSigner.EncodeTypedData(jsonMessage);
-            var signature = await _ethExternalSigner.SignAsync(Sha3Keccack.Current.CalculateHash(encodedData)).ConfigureAwait(false);
-            return EthECDSASignature.CreateStringSignature(signature);
+            return await _ethExternalSigner.SignTypedDataJsonAsync(jsonMessage).ConfigureAwait(false);
         }
 
         public RpcRequest BuildRequest(string message, object id = null)
