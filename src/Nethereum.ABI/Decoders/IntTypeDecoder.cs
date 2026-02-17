1065393dc7cbff1ff4941dc077016ff9f49e8884
0xInnominatus <259166792+0xInnominatus@users.noreply.github.com>
2026-02-17 14:12:34 -0300

    Direct decoding of primitive integer types instead of decoding via BigInteger

--- src/Nethereum.ABI/Decoders/IntTypeDecoder.cs
+++ src/Nethereum.ABI/Decoders/IntTypeDecoder.cs
@@ -22,20 +22,57 @@ namespace Nethereum.ABI.Decoders
 
         public override object Decode(byte[] encoded, Type type)
         {
+            object result = null;
+            int size = 32;
+
+            Byte expectedByteValue;
+            if (_signed && encoded.First() > 0) // Check that number is negative
+                expectedByteValue = 0xFF; // Expected leadings bytes for negative numbers
+            else
+                expectedByteValue = 0x00; // Expected leading bytes for positive numbers
+
+
             if (type == typeof(byte))
-                return DecodeByte(encoded);
+            {
+                result = DecodeByte(encoded);
+                size = sizeof(byte);
+            }
 
             if (type == typeof(sbyte))
-                return DecodeSbyte(encoded);
+            {
+                var value = DecodeSbyte(encoded);
+                size = sizeof(sbyte);
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type == typeof(short))
-                return DecodeShort(encoded);
+            {
+                var value = DecodeShort(encoded);
+                size = sizeof(short);
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type == typeof(ushort))
-                return DecodeUShort(encoded);
+            {
+                result = DecodeUShort(encoded);
+                size = sizeof(ushort);
+            }
 
             if (type == typeof(int))
-                return DecodeInt(encoded);
+            {
+                var value = DecodeInt(encoded);
+                size = sizeof(int);
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type.GetTypeInfo().IsEnum)
             {
@@ -44,25 +81,57 @@ namespace Nethereum.ABI.Decoders
             }
 
             if (type == typeof(uint))
-                return DecodeUInt(encoded);
+            {
+                result = DecodeUInt(encoded);
+                size = sizeof(uint);
+            }
 
             if (type == typeof(long))
-                return DecodeLong(encoded);
+            {
+                var value = DecodeLong(encoded);
+                size = sizeof(long);
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type == typeof(ulong))
-                return DecodeULong(encoded);
+            {
+                result = DecodeULong(encoded);
+                size = sizeof(ulong);
+            }
 
 #if NET7_0_OR_GREATER
             if (type == typeof(UInt128))
-                return DecodeUInt128(encoded);
+            {
+                result = DecodeUInt128(encoded);
+                size = 16;
+            }
 
             if (type == typeof(Int128))
-                return DecodeInt128(encoded);
+            {
+                var value = DecodeInt128(encoded);
+                size = 16;
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 #endif
-			if (type == typeof(BigInteger) || type == typeof(object))
+            if (type == typeof(BigInteger) || type == typeof(object))
                 return DecodeBigInteger(encoded);
 
-            throw new NotSupportedException(type + " is not a supported decoding type for IntType");
+            for (int i = 0; i < encoded.Length - size; i++)
+            {
+                if (encoded[i] != expectedByteValue)
+                    throw new OverflowException();
+            }
+
+            if (result is null)
+                throw new NotSupportedException(type + " is not a supported decoding type for IntType");
+            else
+                return result;
         }
 
         public BigInteger DecodeBigInteger(string hexString)
@@ -96,58 +165,61 @@ namespace Nethereum.ABI.Decoders
             return new BigInteger(encoded);
         }
 
-        public byte DecodeByte(byte[] encoded)
-        {
-            return (byte) DecodeBigInteger(encoded);
-        }
+        public byte DecodeByte(byte[] encoded) => encoded.Last();
 
-        public sbyte DecodeSbyte(byte[] encoded)
-        {
-            return (sbyte) DecodeBigInteger(encoded);
-        }
+        public sbyte DecodeSbyte(byte[] encoded) => (sbyte) encoded.Last();
 
         public short DecodeShort(byte[] encoded)
         {
-            return (short) DecodeBigInteger(encoded);
+            var result = GetIntFromBytesBE(encoded, encoded.Length - 4);
+            return (short) result;
         }
 
         public ushort DecodeUShort(byte[] encoded)
         {
-            return (ushort) DecodeBigInteger(encoded);
+            var result = GetIntFromBytesBE(encoded, encoded.Length - 4);
+            return (ushort) result;
         }
 
         public int DecodeInt(byte[] encoded)
         {
-            return (int) DecodeBigInteger(encoded);
+            return GetIntFromBytesBE(encoded, encoded.Length - 4);
         }
 
         public long DecodeLong(byte[] encoded)
         {
-            return (long) DecodeBigInteger(encoded);
+            return (long) DecodeULong(encoded);
         }
 
         public uint DecodeUInt(byte[] encoded)
         {
-            return (uint) DecodeBigInteger(encoded);
+            return (uint) GetIntFromBytesBE(encoded, encoded.Length - 4);
         }
 
         public ulong DecodeULong(byte[] encoded)
         {
-            return (ulong) DecodeBigInteger(encoded);
+            uint high = (uint)GetIntFromBytesBE(encoded, encoded.Length - 8);
+            uint low = (uint)GetIntFromBytesBE(encoded, encoded.Length - 4);
+            ulong result = ((ulong)high << 32) | low;
+            return result;
         }
 
 #if NET7_0_OR_GREATER
         public UInt128 DecodeUInt128(byte[] encoded)
         {
-            return (UInt128) DecodeBigInteger(encoded);
+            UInt128 low = (UInt128) DecodeULong(encoded);
+            uint highHigh = (uint) GetIntFromBytesBE(encoded, encoded.Length - 16);
+            uint highLow = (uint) GetIntFromBytesBE(encoded, encoded.Length - 12);
+            UInt128 highPart = ((UInt128) highHigh << (64 + 32)) | ((UInt128) highLow << 64);
+            return highPart | low;
         }
 
         public Int128 DecodeInt128(byte[] encoded)
         {
-            return (Int128) DecodeBigInteger(encoded);
+            return (Int128) DecodeUInt128(encoded);
         }
 #endif
-		public override Type GetDefaultDecodingType()
+        public override Type GetDefaultDecodingType()
         {
             return typeof(BigInteger);
         }
@@ -162,12 +234,30 @@ namespace Nethereum.ABI.Decoders
 #if NET7_0_OR_GREATER
                    type == typeof(UInt128) || type == typeof(Int128) ||
 #endif
-				   type.GetTypeInfo().IsEnum;
+                   type.GetTypeInfo().IsEnum;
         }
 
         public override object DecodePacked(byte[] encoded, Type type)
         { 
            return Decode(encoded, type);
+        }
+
+        /// <summary>
+        /// Converts four consecutive bytes from the specified array, starting at the given index, into a 32-bit signed
+        /// integer using big-endian byte order.
+        /// </summary>
+        /// <remarks>The method does not perform bounds checking. If there are fewer than four bytes
+        /// available from the specified index, an exception may be thrown.</remarks>
+        /// <param name="encoded">The byte array containing the bytes to convert.</param>
+        /// <param name="startIndex">The zero-based index in the array at which to begin reading the four bytes. Must be less than or equal to
+        /// the length of the array minus four.</param>
+        /// <returns>A 32-bit signed integer representing the value of the four bytes interpreted in big-endian order.</returns>
+        internal int GetIntFromBytesBE(byte[] encoded, int startIndex)
+        {
+            int result = encoded[startIndex] << 24;
+            result |= encoded[startIndex + 1] << 16;
+            result |= encoded[startIndex + 2] << 8;
+            return result | encoded[startIndex + 3];
         }
     }
 }
