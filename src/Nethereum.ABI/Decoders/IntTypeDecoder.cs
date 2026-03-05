6e476ac7a8b909b61b067ebbdaf2117c2ab02711
0xInnominatus <259166792+0xInnominatus@users.noreply.github.com>
2026-03-05 19:37:34 -0300

    Direct decoding of primitive integer types with BinaryPrimitives instead of decoding via BigInteger

--- src/Nethereum.ABI/Decoders/IntTypeDecoder.cs
+++ src/Nethereum.ABI/Decoders/IntTypeDecoder.cs
@@ -4,6 +4,9 @@ using System.Numerics;
 using System.Reflection;
 using Nethereum.Hex.HexConvertors.Extensions;
 
+#if NETCOREAPP || NETSTANDARD2_1_OR_GREATER
+using System.Buffers.Binary;
+#endif
 
 namespace Nethereum.ABI.Decoders
 {
@@ -22,20 +25,56 @@ namespace Nethereum.ABI.Decoders
 
         public override object Decode(byte[] encoded, Type type)
         {
+            object result = null;
+            int size = 32;
+
+            Byte expectedByteValue;
+            if (_signed && encoded.First() > 0) // Check that number is negative
+                expectedByteValue = 0xFF; // Expected leadings bytes for negative numbers
+            else
+                expectedByteValue = 0x00; // Expected leading bytes for positive numbers
+
             if (type == typeof(byte))
-                return DecodeByte(encoded);
+            {
+                result = DecodeByte(encoded);
+                size = 1;
+            }
 
             if (type == typeof(sbyte))
-                return DecodeSbyte(encoded);
+            {
+                var value = DecodeSbyte(encoded);
+                size = 1;
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type == typeof(short))
-                return DecodeShort(encoded);
+            {
+                var value = DecodeShort(encoded);
+                size = 2;
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type == typeof(ushort))
-                return DecodeUShort(encoded);
+            {
+                result = DecodeUShort(encoded);
+                size = 2;
+            }
 
             if (type == typeof(int))
-                return DecodeInt(encoded);
+            {
+                var value = DecodeInt(encoded);
+                size = 4;
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type.GetTypeInfo().IsEnum)
             {
@@ -44,25 +83,57 @@ namespace Nethereum.ABI.Decoders
             }
 
             if (type == typeof(uint))
-                return DecodeUInt(encoded);
+            {
+                result = DecodeUInt(encoded);
+                size = 4;
+            }
 
             if (type == typeof(long))
-                return DecodeLong(encoded);
+            {
+                var value = DecodeLong(encoded);
+                size = 8;
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 
             if (type == typeof(ulong))
-                return DecodeULong(encoded);
+            {
+                result = DecodeULong(encoded);
+                size = 8;
+            }
 
 #if NET7_0_OR_GREATER
             if (type == typeof(UInt128))
-                return DecodeUInt128(encoded);
+            {
+                result = DecodeUInt128(encoded);
+                size = 16;
+            }
 
             if (type == typeof(Int128))
-                return DecodeInt128(encoded);
+            {
+                var value = DecodeInt128(encoded);
+                size = 16;
+                if (expectedByteValue == 0xFF && value >= 0
+                    || expectedByteValue == 0x00 && value < 0)
+                    throw new OverflowException();
+                result = value;
+            }
 #endif
-			if (type == typeof(BigInteger) || type == typeof(object))
+            if (type == typeof(BigInteger) || type == typeof(object))
                 return DecodeBigInteger(encoded);
 
-            throw new NotSupportedException(type + " is not a supported decoding type for IntType");
+            for (int i = 0; i < encoded.Length - size; i++)
+            {
+                if (encoded[i] != expectedByteValue)
+                    throw new OverflowException();
+            }
+
+            if (result is null)
+                throw new NotSupportedException(type + " is not a supported decoding type for IntType");
+            else
+                return result;
         }
 
         public BigInteger DecodeBigInteger(string hexString)
@@ -96,58 +167,76 @@ namespace Nethereum.ABI.Decoders
             return new BigInteger(encoded);
         }
 
-        public byte DecodeByte(byte[] encoded)
-        {
-            return (byte) DecodeBigInteger(encoded);
-        }
+        public byte DecodeByte(byte[] encoded) => encoded.Last();
 
-        public sbyte DecodeSbyte(byte[] encoded)
-        {
-            return (sbyte) DecodeBigInteger(encoded);
-        }
+        public sbyte DecodeSbyte(byte[] encoded) => (sbyte) encoded.Last();
 
         public short DecodeShort(byte[] encoded)
         {
+#if NETCOREAPP || NETSTANDARD2_1_OR_GREATER
+            return BinaryPrimitives.ReadInt16BigEndian(encoded[^2..]);
+#else
             return (short) DecodeBigInteger(encoded);
+#endif
         }
 
         public ushort DecodeUShort(byte[] encoded)
         {
+#if NETCOREAPP || NETSTANDARD2_1_OR_GREATER
+            return BinaryPrimitives.ReadUInt16BigEndian(encoded[^2..]);
+#else
             return (ushort) DecodeBigInteger(encoded);
+#endif
         }
 
         public int DecodeInt(byte[] encoded)
         {
+#if NETCOREAPP || NETSTANDARD2_1_OR_GREATER
+            return BinaryPrimitives.ReadInt32BigEndian(encoded[^4..]);
+#else
             return (int) DecodeBigInteger(encoded);
+#endif
         }
 
         public long DecodeLong(byte[] encoded)
         {
+#if NETCOREAPP || NETSTANDARD2_1_OR_GREATER
+            return BinaryPrimitives.ReadInt64BigEndian(encoded[^8..]);
+#else
             return (long) DecodeBigInteger(encoded);
+#endif
         }
 
         public uint DecodeUInt(byte[] encoded)
         {
+#if NETCOREAPP || NETSTANDARD2_1_OR_GREATER
+            return BinaryPrimitives.ReadUInt32BigEndian(encoded[^4..]);
+#else
             return (uint) DecodeBigInteger(encoded);
+#endif
         }
 
         public ulong DecodeULong(byte[] encoded)
         {
+#if NETCOREAPP || NETSTANDARD2_1_OR_GREATER
+            return BinaryPrimitives.ReadUInt64BigEndian(encoded[^8..]);
+#else
             return (ulong) DecodeBigInteger(encoded);
+#endif
         }
 
 #if NET7_0_OR_GREATER
         public UInt128 DecodeUInt128(byte[] encoded)
         {
-            return (UInt128) DecodeBigInteger(encoded);
+            return BinaryPrimitives.ReadUInt128BigEndian(encoded[^16..]);
         }
 
         public Int128 DecodeInt128(byte[] encoded)
         {
-            return (Int128) DecodeBigInteger(encoded);
+            return BinaryPrimitives.ReadInt128BigEndian(encoded[^16..]);
         }
 #endif
-		public override Type GetDefaultDecodingType()
+        public override Type GetDefaultDecodingType()
         {
             return typeof(BigInteger);
         }
@@ -162,7 +251,7 @@ namespace Nethereum.ABI.Decoders
 #if NET7_0_OR_GREATER
                    type == typeof(UInt128) || type == typeof(Int128) ||
 #endif
-				   type.GetTypeInfo().IsEnum;
+                   type.GetTypeInfo().IsEnum;
         }
 
         public override object DecodePacked(byte[] encoded, Type type)
